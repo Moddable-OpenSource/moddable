3d0b6b36a37df753eebdb145418f60c57617dc5b
Peter Hoddie <peter@moddable.tech>
2017-10-24 10:14:11 -0700

    adapt commodetto examples to mix-in style manifest. get them working on simlator.

--- examples/commodetto/sprite/main.js
+++ examples/commodetto/sprite/main.js
@@ -19,7 +19,7 @@ import parseBMP from "commodetto/parseBMP";
 import Poco from "commodetto/Poco";
 import Resource from "Resource";
 
-let render = new Poco(new (require(config.screen))({}));
+let render = new Poco(screen ? screen : new (require(config.screen))({}));
 
 let backgroundColor = render.makeColor(0, 0, 0);
 
