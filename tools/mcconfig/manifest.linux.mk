a4a76d98df7b3161750b1a0466bef1fb6c18e642
Patrick Soquet <ps@moddable.tech>
2017-10-25 18:27:12 +0200

    XS linker: use first chararcter of url as separator

--- tools/mcconfig/manifest.linux.mk
+++ tools/mcconfig/manifest.linux.mk
@@ -166,7 +166,7 @@ $(TMP_DIR)/mc.xs.o: $(TMP_DIR)/mc.xs.c
 
 $(TMP_DIR)/mc.xs.c: $(MODULES) $(MANIFEST)
 	@echo "# xsl modules"
-	$(XSL) -u "./" -b $(MODULES_DIR) -o $(TMP_DIR) $(PRELOADS) $(STRIPS) $(CREATION) $(MODULES)
+	$(XSL) -u / -b $(MODULES_DIR) -o $(TMP_DIR) $(PRELOADS) $(STRIPS) $(CREATION) $(MODULES)
 
 $(TMP_DIR)/mc.resources.o: $(TMP_DIR)/mc.resources.c
 	@echo "# cc" $(<F)
