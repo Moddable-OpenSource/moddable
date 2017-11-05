bd6dafb3b612c5adf932d2c830dc3a4d51de73f7
Patrick Soquet <ps@moddable.tech>
2017-11-05 14:03:10 +0100

    tools: cleanup

--- tools/tool.js
+++ tools/tool.js
@@ -18,17 +18,31 @@
  *
  */
 
-import * as FS from "fs";
-
 const inline = "args.txt";
 
-export default class Tool {
+export class FILE @ "FILE_prototype_destructor" {
+	constructor(path, flags) @ "FILE_prototype_constructor";
+	close() @ "FILE_prototype_close";
+	dump(path) @ "FILE_prototype_dump";
+	line(...strings) {
+		let c = arguments.length; 
+		for (let i = 0; i < c; i++)
+			this.writeString(arguments[i]);
+		this.writeString("\n");
+	}
+	write() @ "FILE_prototype_write";
+	writeBuffer(buffer) @ "FILE_prototype_writeBuffer";
+	writeByte(byte) @ "FILE_prototype_writeByte";
+	writeString(string) @ "FILE_prototype_writeString";
+}
+
+export class TOOL {
 	constructor(argv) {
 		this.errorCount = 0;
 		this.warningCount = 0;
 		if ((argv.length == 2) && (argv[1] == inline)) {
 			let path = this.resolveFilePath(inline);
-			let args = FS.readFileSync(path);
+			let args = this.readFileString(path);
 			args = args.trim().split(/\s+/);
 			argv.length = 1;
 			args.forEach(arg => argv.push(arg));
@@ -38,10 +52,17 @@ export default class Tool {
 	get currentDirectory() @ "Tool_prototype_get_currentDirectory";
 	set currentDirectory(it) @ "Tool_prototype_set_currentDirectory";
 	get currentPlatform() @ "Tool_prototype_get_currentPlatform";
+	deleteDirectory(path) @ "Tool_prototype_deleteDirectory";
+	deleteFile(path) @ "Tool_prototype_deleteFile";
+	createDirectory(path) @ "Tool_prototype_createDirectory";
+	enumerateDirectory(path) @ "Tool_prototype_enumerateDirectory";
 	execute(command) @ "Tool_prototype_execute";
 	getenv(name) @ "Tool_prototype_getenv";
 	hash(d, string) @  "Tool_prototype_fsvhash";
+	isDirectoryOrFile(path) @ "Tool_prototype_isDirectoryOrFile"; // -1: directory, 0: none, 1: file
 	joinPath(parts) @ "Tool_prototype_joinPath";
+	readFileString(path) @ "Tool_prototype_readFileString";
+	readFileBuffer(path) @ "Tool_prototype_readFileBuffer";
 	report(message) @ "Tool_prototype_report";
 	reportError(message) @ "Tool_prototype_reportError";
 	reportWarning(message) @ "Tool_prototype_reportWarning";
@@ -51,4 +72,6 @@ export default class Tool {
 	splitPath(path) @ "Tool_prototype_splitPath";
 	strlen(string) @  "Tool_prototype_strlen";
 	then() @  "Tool_prototype_then";
+	writeFileString(path, string) @ "Tool_prototype_writeFileString";
+	writeFileBuffer(path, buffer) @ "Tool_prototype_writeFileBuffer";
 }
