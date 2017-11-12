80fb4deb72bc236cc45bce72fa10e8356c98cee0
Peter Hoddie <peter@moddable.tech>
2017-11-11 17:29:22 -0800

    one more file for the fmod change

--- tools/mcconfig/make.esp.mk
+++ tools/mcconfig/make.esp.mk
@@ -109,6 +109,7 @@ SDK_SRC = \
 	$(ARDUINO_ESP8266)/uart.c \
 	$(ARDUINO_ESP8266)/umm_malloc/umm_malloc.c \
 	$(ARDUINO_ESP8266)/WString.cpp \
+	$(PLATFORM_DIR)/lib/fmod/e_fmod.c \
 	$(PLATFORM_DIR)/lib/rtc/rtctime.c \
 	$(PLATFORM_DIR)/lib/tinyprintf/tinyprintf.c
 SDK_SRC_SKIPPED = \
