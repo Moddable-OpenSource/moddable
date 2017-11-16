30f6d745c0e5b66634e8443aaabd322f60526ce7
Patrick Soquet <ps@moddable.tech>
2017-11-16 10:00:07 +0100

    examples piu drag: borders

--- examples/piu/drag/main.js
+++ examples/piu/drag/main.js
@@ -28,6 +28,8 @@ import config from "mc/config";
 const desktopTexture = new Texture("desktop.png");
 const desktopSkin = new Skin({ texture:desktopTexture, x:0, y:0, width:40, height:40, tiles:{ left:0, right:0, top:0, bottom:0 } });
 
+const bordersSkin = new Skin({ stroke:"black", left:2, right:2, top:2, bottom:2 });
+
 const buttonTexture = new Texture("button.png");
 const buttonSkin = new Skin({ texture:buttonTexture, x:0, y:0, width:60, height:40, states:40, tiles:{ left:20, right:20 } });
 const buttonStyle = new Style({ font:"600 28px Open Sans", color:["black", "white"], });
@@ -60,12 +62,17 @@ let DragApplication = Application.template($ => ({
 	skin:desktopSkin, style:buttonStyle,
 	contents: [
 		Container($, {
-			left:20, right:20, top:20, bottom:20, clip:true,
+			left:18, right:18, top:18, bottom:18, skin:bordersSkin,
 			contents: [
-				Label($, { left:10, top:10, width:120, height:40, skin:buttonSkin, string:"Drag 1", active:true, Behavior:DragBehavior }),
-				Label($, { right:10, top:10, width:120, height:40, skin:buttonSkin, string:"Drag 2 ", active:true, Behavior:DragBehavior }),
-				Label($, { right:10, bottom:10, width:120, height:40, skin:buttonSkin, string:"Drag 3", active:true, Behavior:DragBehavior }),
-				Label($, { left:10, bottom:10, width:120, height:40, skin:buttonSkin, string:"Drag 4", active:true, Behavior:DragBehavior }),
+				Container($, {
+					left:2, right:2, top:2, bottom:2, clip:true,
+					contents: [
+						Label($, { left:10, top:10, width:120, height:40, skin:buttonSkin, string:"Drag 1", active:true, Behavior:DragBehavior }),
+						Label($, { right:10, top:10, width:120, height:40, skin:buttonSkin, string:"Drag 2 ", active:true, Behavior:DragBehavior }),
+						Label($, { right:10, bottom:10, width:120, height:40, skin:buttonSkin, string:"Drag 3", active:true, Behavior:DragBehavior }),
+						Label($, { left:10, bottom:10, width:120, height:40, skin:buttonSkin, string:"Drag 4", active:true, Behavior:DragBehavior }),
+					],
+				}),
 			],
 		}),
 	]
