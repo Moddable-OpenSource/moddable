b31e11bd7ffa3cb58e609c8df8aa2f7dfc9131e4
Peter Hoddie <peter@moddable.tech>
2017-11-15 20:03:17 -0800

    XS6->XS straggler

--- modules/base/instrumentation/modInstrumentation.h
+++ modules/base/instrumentation/modInstrumentation.h
@@ -55,7 +55,7 @@ enum {
 	/* system */
 	kModInstrumentationSystemFreeMemory,
 
-	/* XS6 */
+	/* XS */
 	kModInstrumentationSlotHeapSize,
 	kModInstrumentationChunkHeapSize,
 	kModInstrumentationKeysUsed,
