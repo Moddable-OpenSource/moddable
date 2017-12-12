e31c35434dd2ed9e4ef762963a7a1c013d847dca
Patrick Soquet <ps@moddable.tech>
2017-12-12 15:59:03 +0100

    serial2xsbug muxer Linux/macOS/Windows REBUILD ALL

--- tools/serial2xsbug/lin/serial2xsbug.c
+++ tools/serial2xsbug/lin/serial2xsbug.c
@@ -39,13 +39,22 @@
 #define mxSerialBufferSize 1024
 #define mxBufferSize 32 * 1024
 
+typedef struct txSerialMachineStruct txSerialMachineRecord, *txSerialMachine;
 typedef struct txSerialToolStruct txSerialToolRecord, *txSerialTool;
+
+struct txSerialMachineStruct {
+	txSerialTool tool;
+	int index;
+	int networkConnection;
+	char networkBuffer[mxNetworkBufferSize];
+};
 
 struct txSerialToolStruct {
 	jmp_buf _jmp_buf;
 	char* file;
 	int line;
 	int error;
+	struct pollfd fds[11];
 	
 	char* path;
 	int baud;
@@ -58,8 +67,8 @@ struct txSerialToolStruct {
 	
 	char* host;
 	int port;
-	int networkConnection;
-	char networkBuffer[mxNetworkBufferSize];
+	txSerialMachine machines[10];
+	txSerialMachine currentMachine;
 	
 	int index;
 	int state;
@@ -67,17 +76,30 @@ struct txSerialToolStruct {
 	char buffer[mxBufferSize];
 };
 
-static void fxCloseNetwork(txSerialTool self);
+static void fxCloseNetwork(txSerialTool self, int index);
 static void fxCloseSerial(txSerialTool self);
-static void fxOpenNetwork(txSerialTool self);
+static txSerialMachine fxOpenNetwork(txSerialTool self, int index);
 static void fxOpenSerial(txSerialTool self);
-static void fxReadNetwork(txSerialTool self);
+static void fxReadNetwork(txSerialMachine machine);
 static void fxReadSerial(txSerialTool self);
 static void fxWriteNetwork(txSerialTool self, char* buffer, int size);
 static void fxWriteSerial(txSerialTool self, char* buffer, int size);
 
 #define mxThrowElse(_ASSERTION) { if (!(_ASSERTION)) { self->file=__FILE__; self->line=__LINE__; self->error = errno; longjmp(self->_jmp_buf, 1); } }
 #define mxThrowError(_ERROR) { self->file=__FILE__; self->line=__LINE__; self->error = _ERROR; longjmp(self->_jmp_buf, 1); } 
+
+static char* gxMachineTags[10] = {
+	"?xs0?>\r\n<",
+	"?xs1?>\r\n<",
+	"?xs2?>\r\n<",
+	"?xs3?>\r\n<",
+	"?xs4?>\r\n<",
+	"?xs5?>\r\n<",
+	"?xs6?>\r\n<",
+	"?xs7?>\r\n<",
+	"?xs8?>\r\n<",
+	"?xs9?>\r\n<"
+};
 
 #define BOTHER 0010000
 struct termios2 {
@@ -93,10 +115,18 @@ struct termios2 {
 #define TCGETS2 _IOR('T', 0x2A, struct termios2)
 #define TCSETS2 _IOW('T', 0x2B, struct termios2)
 
-void fxCloseNetwork(txSerialTool self)
+void fxCloseNetwork(txSerialTool self, int index)
 {
-	if (self->networkConnection >= 0)
-		close(self->networkConnection);
+	txSerialMachine machine = self->machines[index];
+	if (machine) {
+		self->fds[index].fd = -1 - index;
+		if (machine->networkConnection >= 0)
+			close(machine->networkConnection);
+		free(machine);
+		self->machines[index] = NULL;
+		if (self->currentMachine == machine)
+			self->currentMachine = NULL;
+	}
 }
 
 void fxCloseSerial(txSerialTool self)
@@ -106,25 +136,34 @@ void fxCloseSerial(txSerialTool self)
 	}
 }
 
-void fxOpenNetwork(txSerialTool self)
+txSerialMachine fxOpenNetwork(txSerialTool self, int index)
 {
-	struct hostent *host;
-	struct sockaddr_in address;
-	host = gethostbyname(self->host);
-	mxThrowElse(host != NULL);
-	memset(&address, 0, sizeof(address));
-	address.sin_family = AF_INET;
-	memcpy(&(address.sin_addr), host->h_addr, host->h_length);
-	address.sin_port = htons(self->port);
-	self->networkConnection = socket(AF_INET, SOCK_STREAM, 0);
-	mxThrowElse(self->networkConnection >= 0);
-	mxThrowElse(connect(self->networkConnection, (struct sockaddr*)&address, sizeof(address)) >= 0);
+	txSerialMachine machine = self->machines[index];
+	if (!machine) {
+		struct hostent *host;
+		struct sockaddr_in address;
+		machine = calloc(sizeof(txSerialMachineRecord), 1);
+		mxThrowElse(machine != NULL);
+		machine->tool = self;
+		machine->index = index;
+		self->machines[index] = machine;
+		host = gethostbyname(self->host);
+		mxThrowElse(host != NULL);
+		memset(&address, 0, sizeof(address));
+		address.sin_family = AF_INET;
+		memcpy(&(address.sin_addr), host->h_addr, host->h_length);
+		address.sin_port = htons(self->port);
+		machine->networkConnection = socket(AF_INET, SOCK_STREAM, 0);
+		mxThrowElse(machine->networkConnection >= 0);
+		mxThrowElse(connect(machine->networkConnection, (struct sockaddr*)&address, sizeof(address)) >= 0);
+		self->fds[index].fd = machine->networkConnection;
+	}
+	return machine;
 }
 
 void fxOpenSerial(txSerialTool self)
 {
 	speed_t speed;
-    int mcs;
 	struct termios term;
   	self->serialConnection = open(self->path, O_RDWR | O_NOCTTY | O_NDELAY);
 	mxThrowElse(self->serialConnection >= 0);
@@ -160,21 +199,35 @@ void fxOpenSerial(txSerialTool self)
 	} 
 }
 
-void fxReadNetwork(txSerialTool self)
+void fxReadNetwork(txSerialMachine machine)
 {
-	int size = read(self->networkConnection, self->networkBuffer, mxNetworkBufferSize - 1);
-	mxThrowElse(size > 0);
-// 	self->networkBuffer[size] = 0;
-// 	fprintf(stderr, "%s\n", self->networkBuffer);
-	fxWriteSerial(self, self->networkBuffer, size);
+	txSerialTool self = machine->tool;
+	int size = read(machine->networkConnection, machine->networkBuffer, mxNetworkBufferSize - 1);
+	if (size > 0) {
+		char* former = machine->networkBuffer;
+		char* current = former;
+		char* limit = former + size;
+		int offset;
+		while (current < limit) {
+			offset = current - former;
+			if ((offset >= 3) && (current[-3] == 13) && (current[-2] == 10) && (current[-1] == '<')) {
+				fxWriteSerial(self, former, offset);
+				fxWriteSerial(self, gxMachineTags[machine->index], 9);
+				former = current;
+			}
+			current++;
+		}
+		offset = limit - former;
+		if (offset)
+			fxWriteSerial(self, former, offset);
+	}
 }
 
 void fxReadSerial(txSerialTool self)
 {
-	int size = read(self->serialConnection, self->serialBuffer, mxSerialBufferSize - 1);
+	int size = read(self->serialConnection, self->serialBuffer, mxSerialBufferSize);
 	mxThrowElse(size > 0);
-// 	self->serialBuffer[size] = 0;
-// 	fprintf(stderr, "%s\n", self->serialBuffer);
+// 	fprintf(stderr, "%.*s", size, self->serialBuffer);
 	char* src = self->serialBuffer;
 	char* srcLimit = src + size;
 	int offset = self->index;
@@ -190,7 +243,13 @@ void fxReadSerial(txSerialTool self)
 		*dst++ = *src++;
 		offset++;
 		if ((offset >= 2) && (dst[-2] == 13) && (dst[-1] == 10)) {
-			if ((offset >= 10) && (dst[-10] == '<') && (dst[-9] == '/') && (dst[-8] == 'x') && (dst[-7] == 's') && (dst[-6] == 'b') && (dst[-5] == 'u') && (dst[-4] == 'g') && (dst[-3] == '>')) {
+			if ((offset >= 9) && (dst[-9] == '<') && (dst[-8] == '?') && (dst[-7] == 'x') && (dst[-6] == 's') && (dst[-5] >= '0') && (dst[-5] <= '9') && (dst[-4] == '?') && (dst[-3] == '>')) {
+				self->currentMachine = fxOpenNetwork(self, dst[-5] - '0');
+			}
+			else if ((offset >= 10) && (dst[-10] == '<') && (dst[-9] == '?') && (dst[-8] == 'x') && (dst[-7] == 's') && (dst[-6] >= '0') && (dst[-6] <= '9') && (dst[-5] == '-') && (dst[-4] == '?') && (dst[-3] == '>')) {
+				fxCloseNetwork(self, dst[-6] - '0');
+			}
+			else if ((offset >= 10) && (dst[-10] == '<') && (dst[-9] == '/') && (dst[-8] == 'x') && (dst[-7] == 's') && (dst[-6] == 'b') && (dst[-5] == 'u') && (dst[-4] == 'g') && (dst[-3] == '>')) {
 				fxWriteNetwork(self, self->buffer, offset);
 			}
 			else {
@@ -206,17 +265,21 @@ void fxReadSerial(txSerialTool self)
 
 void fxWriteNetwork(txSerialTool self, char* buffer, int size)
 {
-	int count;
-	while (size) {
-    	count = write(self->networkConnection, buffer, size);
-		mxThrowElse(count > 0);
-		buffer += count;
-		size -= count;
+	txSerialMachine machine = self->currentMachine;
+	if (machine) {
+		int count;
+		while (size) {
+			count = write(machine->networkConnection, buffer, size);
+			mxThrowElse(count > 0);
+			buffer += count;
+			size -= count;
+		}
 	}
 }
 
 void fxWriteSerial(txSerialTool self, char* buffer, int size)
 {
+// 	fprintf(stderr, "%.*s", size, buffer);
 	int count;
 	while (size) {
     	count = write(self->serialConnection, buffer, size);
@@ -230,7 +293,6 @@ static txSerialToolRecord tool;
 
 void handler(int s) {
 	txSerialTool self = &tool;
-	fxCloseNetwork(self);
 	fxCloseSerial(self);
 	exit(0); 
 }
@@ -239,8 +301,6 @@ int main(int argc, char* argv[])
 {
 	int result = 0;
 	txSerialTool self = &tool;
-	struct pollfd fds[2];
-	struct pollfd *networkPoll, *serialPoll;
 	if (argc < 4) {
 		fprintf(stderr, "### serial2xsbug <port name> <baud rate> <data bits><parity><stop bits>\n");
 		return 1;
@@ -255,34 +315,35 @@ int main(int argc, char* argv[])
 	
 	self->host = "localhost";
 	self->port = 5002;
-	self->networkConnection = -1;
 	
 	signal(SIGINT, handler);
 
 	if (setjmp(self->_jmp_buf) == 0) {
+		int index;
+		for (index = 0; index < 10; index++) {
+			self->fds[index].fd = -1 - index;
+			self->fds[index].events = POLLIN;
+			self->fds[index].revents = 0;
+		}
 		fxOpenSerial(self);
-		fxOpenNetwork(self);
-		fds[0].fd = self->serialConnection;
-		fds[0].events = POLLIN;
-		fds[0].revents = 0;
-		fds[1].fd = self->networkConnection;
-		fds[1].events = POLLIN;
-		fds[1].revents = 0;
-		serialPoll = fds;
-		networkPoll = fds + 1;
+		self->fds[10].fd = self->serialConnection;
+		self->fds[10].events = POLLIN;
+		self->fds[10].revents = 0;
 		for (;;) {
 			int res;
-			res = poll(fds, 2, 1000);
+			res = poll(self->fds, 11, 1000);
 			mxThrowElse(res >= 0);
 			if (res) {
-				if (networkPoll->revents & (POLLERR | POLLHUP)) 
+				for (index = 0; index < 10; index++) {
+					if (self->fds[index].revents & (POLLERR | POLLHUP)) 
+						mxThrowError(EPIPE);
+					if (self->fds[index].revents & POLLIN)
+						fxReadNetwork(self->machines[index]);
+				}
+				if (self->fds[10].revents & (POLLERR | POLLHUP))
 					mxThrowError(EPIPE);
-				if (serialPoll->revents & (POLLERR | POLLHUP))
-					mxThrowError(EPIPE);
-				if (serialPoll->revents & POLLIN)
+				if (self->fds[10].revents & POLLIN)
 					fxReadSerial(self);
-				if (networkPoll->revents & POLLIN)
-					fxReadNetwork(self);
 			}
 		}
 	}
@@ -290,7 +351,6 @@ int main(int argc, char* argv[])
 		fprintf(stderr, "%s:%d: %s\n", self->file, self->line, strerror(self->error));
 		result = 1;
 	}
-	fxCloseNetwork(self);
 	fxCloseSerial(self);
 	return result;
 }
