31e372f5bca49e46e5206a42d9cd2fbbabe2088b
Rick Waldron <waldron.rick@gmail.com>
2017-12-21 17:08:14 -0800

    modules/drivers: MCP230XX refactor/redesign #23

--- examples/drivers/mcp23008/main.js
+++ examples/drivers/mcp23008/main.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2016-2017  Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK.
- * 
+ *
  *   This work is licensed under the
  *       Creative Commons Attribution 4.0 International License.
  *   To view a copy of this license, visit
@@ -13,15 +13,36 @@
  */
 
 import Timer from "timer";
-import MCP23008 from "mcp23008";
+import Digital from "pins/digital";
+import {MCP23008} from "MCP230XX";
 
 export default function() {
+	const leds = new MCP23008(/* defaults to 0x20! */);
+	const io = new MCP23008({ address: 0x21 });
 	let mask = 0x88;
+
+	// These are buttons.
+	io[0].mode(Digital.Input);
+	io[1].mode(Digital.Input);
+	io[2].mode(Digital.Input);
+
+	// These are LEDs, writing to them will
+	// set the direction to OUTPUT.
+	io[3].write(0);
+	io[4].write(0);
+	io[5].write(0);
 
 	Timer.repeat(() => {
 		mask = ((mask << 1) | (mask >> 7)) & 0xFF;
 
-		for (let i = 0; i < 8; i++)
-			MCP23008.write(i, (mask & (1 << i)) ? 1 : 0);
+		// Update the leds
+		for (let i = 0; i < 8; i++) {
+			leds[i].write(mask & (1 << i) ? 1 : 0);
+
+			// Read the buttons and update the outputs
+			if (i < 3) {
+				io[i + 3].write(io[i].read() ? 0 : 1);
+			}
+		}
 	}, 50);
 }
---
+++ examples/drivers/mcp23017/main.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2016-2017  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ *
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+
+import Timer from "timer";
+import Digital from "pins/digital";
+import {MCP23017} from "MCP230XX";
+
+export default function() {
+	const expander = new MCP23017({
+		address: 0x20,
+		inputs: 0b1111111100000000,
+	});
+	const led = expander[8];
+	const button = expander[15];
+
+	let ports = { a: 0x88, b: 0x00 };
+	let indicator = 0;
+	let delay = 120;
+	let last = Date.now();
+
+	// Set pins 0-15
+	expander.bankWrite(0x0000);
+
+	button.mode(Digital.Input);
+
+	Timer.repeat(() => {
+		// Set pins 0-7 (Or: port A)
+		ports.a = ((ports.a << 1) | (ports.a >> 7)) & 0xFF;
+
+
+		// Throttle button reading...
+		let now = Date.now();
+
+		if (now >= last + delay) {
+			last = now;
+
+			// Check if the button is pressed...
+			if (expander.bankRead() >> 15) {
+				indicator ^= 1;
+			}
+		}
+		// Set pins 8-15 (Or: port B)
+		ports.b = (indicator << 8);
+
+		expander.bankWrite(ports.b | ports.a);
+	}, 50);
+}
