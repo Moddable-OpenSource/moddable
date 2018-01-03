1bb24de3445f39da49492351bc6999af026ed60e
Patrick Soquet <ps@moddable.tech>
2018-01-03 10:36:53 +0100

    XS: new regexp engine - REBUILD ALL

--- xs/makefiles/lin/xsid.mk
+++ xs/makefiles/lin/xsid.mk
@@ -41,7 +41,6 @@ C_OPTIONS =\
 	-I$(INC_DIR)\
 	-I$(PLT_DIR) \
 	-I$(SRC_DIR)\
-	-I$(SRC_DIR)/pcre\
 	-I$(TLS_DIR)\
 	-I$(TMP_DIR)\
 	-DmxCompile=1
@@ -57,7 +56,7 @@ LINK_OPTIONS = -rdynamic
 
 OBJECTS = \
 	$(TMP_DIR)/xsdtoa.o \
-	$(TMP_DIR)/xspcre.o \
+	$(TMP_DIR)/xsre.o \
 	$(TMP_DIR)/xsCommon.o \
 	$(TMP_DIR)/xsid.o
 
