8036ddbc3465b35958e41c9092bac365a3cc1b85
Peter Hoddie <peter@moddable.tech>
2018-01-03 14:45:53 -0800

    dotstar display driver thrash: clean-up code, add manifest and docs

--- build/devices/esp/setup/commodetto.js
+++ build/devices/esp/setup/commodetto.js
@@ -21,7 +21,7 @@
 import config from "mc/config";
 
 export default function (done) {
-	if (!global.screen)
+	if (!global.screen && config.screen)
 		global.screen = new (require(config.screen))({});
 	done();
 }
