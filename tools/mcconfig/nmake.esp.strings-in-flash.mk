be268bbaae6b835f983e41a155b7c7a9e113d605
Brian Friedkin <brian@moddable.tech>
2018-01-04 17:00:26 -0800

    windows build: clean up duplicate console output, extra quotes and shorten file names. fix compile warning.

--- tools/mcconfig/nmake.esp.strings-in-flash.mk
+++ tools/mcconfig/nmake.esp.strings-in-flash.mk
@@ -1,4 +1,4 @@
-	@echo "# cc" $(@F) "(strings in flash)"
+	@echo # cc $(@F) (strings in flash)
 	$(CC) $(C_DEFINES) $(C_INCLUDES) $(C_FLAGS) $< -o $@.unmapped
 	$(TOOLS_BIN)\xtensa-lx106-elf-objcopy --rename-section .rodata.str1.1=.irom0.str.1 --rename-section .text=irom0.code $@.unmapped $@ 
 	$(AR) $(AR_OPTIONS) $(APP_ARCHIVE) $@
