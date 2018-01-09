a920472f65fded6c62caaf5520e42ee4aa52cd99
Peter Hoddie <peter@moddable.tech>
2018-01-09 08:13:40 -0800

    remove obsolete code from timers, add optional support to associate timer with task

--- modules/base/timer/win/timer.c
+++ modules/base/timer/win/timer.c
@@ -132,10 +132,6 @@ modTimer modTimerAdd(int firstInterval, int secondInterval, modTimerCallback cb,
 	return timer;
 }
 
-void modTimerSetScript(modTimer timer)
-{
-}
-
 void modTimerReschedule(modTimer timer, int firstInterval, int secondInterval)
 {
 	timer->firstInterval = firstInterval;
