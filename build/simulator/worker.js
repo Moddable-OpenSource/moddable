7a4642f58761251cb69c166d6484d79fcdf12318
Patrick Soquet <ps@moddable.tech>
2018-01-19 13:19:23 +0100

    Piu simulator workers

---
+++ build/simulator/worker.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016-2017  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ * 
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+class Worker @ "Worker_destructor" {
+	constructor(module) @ "Worker_constructor";
+	postMessage(message) @ "Worker_postMessage";
+	terminate() @ "Worker_terminate";
+};
+
+export default Worker;
+
+Object.freeze(Worker.prototype);
