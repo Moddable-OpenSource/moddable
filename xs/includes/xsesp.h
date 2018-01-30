791cfaa6d309f2b034bb9abb96d18c7bd94c64d2
Peter Hoddie <peter@moddable.tech>
2018-01-29 20:39:50 -0800

    replace itoa with ltoa on ESP8266 for compatibility with 2.4

--- xs/includes/xsesp.h
+++ xs/includes/xsesp.h
@@ -116,11 +116,14 @@ extern void ESP_putc(int c);
 		} while (0)
 #endif
 #define modLogVar(msg) modLog_transmit(msg)
+#if !ESP32
+	extern char* ltoa(long value, char* result, int base);
+	#define itoa(value, result, base) ltoa(value, result, base)
+#endif
 #define modLogInt(msg) \
 	do { \
 		char temp[10]; \
-		itoa(msg, temp, 10); \
-		modLog_transmit(temp); \
+		modLog_transmit(itoa(msg, temp, 10)); \
 	} while (0)
 #define modLogHex(msg) \
 	do { \
