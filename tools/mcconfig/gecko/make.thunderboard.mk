860f7d3f4c7f2598cea24c7dc30df4e643d38a7a
Michael Kellner <mkellner@intlweb.com>
2018-01-30 16:06:20 -0800

    update (thunderboard untested)

--- tools/mcconfig/gecko/make.thunderboard.mk
+++ tools/mcconfig/gecko/make.thunderboard.mk
@@ -18,7 +18,10 @@
 #
 
 BASE = /Applications/Simplicity?Studio.app/Contents/Eclipse/developer
+
+
 # Thunderboard	(BRD4160A)
+PLATFORM_NAME = thunderboard
 SDK_BASE = $(BASE)/sdks/gecko_sdk_suite/v1.0
 HWKIT = $(SDK_BASE)/hardware/kit/EFR32MG1_BRD4160A/config
 HWINC = $(SDK_BASE)/platform/Device/SiliconLabs/EFR32MG1P/Include
@@ -28,16 +31,6 @@ HW_DEBUG_OPT= -O0
 HW_OPT = -Os
 # DEV_C_FLAGS = -fno-short-enums
 DEV_C_FLAGS = 
-
-ifeq ($(DEBUG),1)
-	LIB_DIR = $(BUILD_DIR)/tmp/gecko_thunderboard/debug/lib
-else
-	ifeq ($(INSTRUMENT),1)
-		LIB_DIR = $(BUILD_DIR)/tmp/gecko_thunderboard/instrument/lib
-	else
-		LIB_DIR = $(BUILD_DIR)/tmp/gecko_thunderboard/release/lib
-	endif
-endif
 
 C_DEFINES += -DuseRTCC
 
