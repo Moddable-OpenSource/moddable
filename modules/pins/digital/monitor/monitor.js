190d194c75794e3f9c01d39eea18f2ac5305e0eb
Peter Hoddie <peter@moddable.tech>
2018-02-08 10:17:51 -0800

    digital monitor - first time

---
+++ modules/pins/digital/monitor/monitor.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2018  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ * 
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+class Monitor @ "xs_digital_monitor_destructor" {
+	constructor(dictionary) @ "xs_digital_monitor";
+	close() @ "xs_digital_monitor_close"
+	read() @ "xs_digital_monitor_read"
+	get count() @ "xs_digital_monitor_get_count"
+}
+Monitor.Rising = 1;
+Monitor.Falling = 2;
+
+Object.freeze(Monitor.prototype);
+
+export default Monitor;
