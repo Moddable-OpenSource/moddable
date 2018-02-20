1dbffcbb3a49cfb17e7bfc003c51d8d102e51182
Brian Friedkin <brian@moddable.tech>
2018-02-19 17:15:34 -0800

    ble: work in progress

--- modules/network/ble/esp32/modBLE.c
+++ modules/network/ble/esp32/modBLE.c
@@ -18,43 +18,23 @@
  *
  */
 
-#include "xs.h"
-#include "mc.xs.h"			// for xsID_ values
+#include "xsmc.h"
+#include "xsesp.h"
+#include "mc.xs.h"
+
+#include "FreeRTOSConfig.h"
+#include "esp_bt.h"
+
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_bt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_gatt_common_api.h"
 
 #define adv_config_flag      (1 << 0)
 #define scan_rsp_config_flag (1 << 1)
 
-static esp_ble_adv_data_t adv_data = {
-    .set_scan_rsp = false,
-    .include_name = true,
-    .include_txpower = true,
-    .min_interval = 0x20,
-    .max_interval = 0x40,
-    .appearance = 0x00,
-    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
-    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
-    .service_data_len = 0,
-    .p_service_data = NULL,
-    .service_uuid_len = 32,
-    .p_service_uuid = adv_service_uuid128,
-    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
-};
-// scan response data
-static esp_ble_adv_data_t scan_rsp_data = {
-    .set_scan_rsp = true,
-    .include_name = true,
-    .include_txpower = true,
-    .min_interval = 0x20,
-    .max_interval = 0x40,
-    .appearance = 0x00,
-    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
-    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
-    .service_data_len = 0,
-    .p_service_data = NULL,
-    .service_uuid_len = 32,
-    .p_service_uuid = adv_service_uuid128,
-    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
-};
+#define PROFILE_A_APP_ID 0
 
 typedef struct {
 	xsMachine	*the;
@@ -62,8 +42,17 @@ typedef struct {
 
 	uint8_t *advertisingData;
 	uint8_t *scanResponseData;
+	
+	esp_ble_adv_params_t adv_params;
 	uint8_t adv_config_done;
 } modBLERecord, *modBLE;
+
+static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
+static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
+
+// @@ The ESP32 BT APIs don't support a refcon to tuck away this kind of stuff...
+static xsMachine *gThe;
+static xsSlot gThis;
 
 void xs_ble_initialize(xsMachine *the)
 {
@@ -72,62 +61,29 @@ void xs_ble_initialize(xsMachine *the)
 	if (!ble)
 		xsUnknownError("no memory");
 	xsmcSetHostData(xsThis, ble);
-	ble->the = the;
-	ble->obj = xsThis;
+	ble->the = gThe = the;
+	ble->obj = gThis = xsThis;
 	
-	// @@ check if we already do this
-    ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK( ret );
-
+	// Initialize platform Bluetooth modules
     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
-
     esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
-    ret = esp_bt_controller_init(&bt_cfg);
-    if (ret) {
-        ESP_LOGE(GATTS_TAG, "%s initialize controller failed\n", __func__);
-        return;
-    }
+	ESP_ERROR_CHECK(esp_bt_controller_init(&bt_cfg))
+	ESP_ERROR_CHECK(esp_bt_controller_enable(ESP_BT_MODE_BLE));
+	ESP_ERROR_CHECK(esp_bluedroid_init());
+	ESP_ERROR_CHECK(esp_bluedroid_enable());
 
-    ret = esp_bt_controller_enable(ESP_BT_MODE_BLE);
-    if (ret) {
-        ESP_LOGE(GATTS_TAG, "%s enable controller failed\n", __func__);
-        return;
-    }
-    ret = esp_bluedroid_init();
-    if (ret) {
-        ESP_LOGE(GATTS_TAG, "%s init bluetooth failed\n", __func__);
-        return;
-    }
-    ret = esp_bluedroid_enable();
-    if (ret) {
-        ESP_LOGE(GATTS_TAG, "%s enable bluetooth failed\n", __func__);
-        return;
-    }
+	// Register callbacks
+	ESP_ERROR_CHECK(esp_ble_gap_register_callback(gap_event_handler));
+    ESP_ERROR_CHECK(esp_ble_gatts_register_callback(gatts_event_handler));
+    ESP_ERROR_CHECK(esp_ble_gatts_app_register(PROFILE_A_APP_ID));
 
-    ret = esp_ble_gatts_register_callback(gatts_event_handler);
-    if (ret){
-        ESP_LOGE(GATTS_TAG, "gatts register error, error code = %x", ret);
-        return;
-    }
-    ret = esp_ble_gap_register_callback(gap_event_handler);
-    if (ret){
-        ESP_LOGE(GATTS_TAG, "gap register error, error code = %x", ret);
-        return;
-    }
-    ret = esp_ble_gatts_app_register(PROFILE_A_APP_ID);
-    if (ret){
-        ESP_LOGE(GATTS_TAG, "gatts app register error, error code = %x", ret);
-        return;
-    }
+	// Stack is ready
+	xsCall1(ble->obj, xsID_callback, xsString("onReady"));
 }
 
 void xs_ble_close(xsMachine *the)
 {
-	modBLE ble = xsmcGetHostData(xsThis);
+	modBLE ble = (modBLE)xsmcGetHostData(xsThis);
 	xs_ble_destructor(ble);
 	xsmcSetHostData(xsThis, NULL);
 }
@@ -136,37 +92,116 @@ void xs_ble_destructor(void *data)
 {
 	modBLE ble = data;
 	if (ble) {
+		if (ble->advertisingData)
+			c_free(ble->advertisingData);
+		if (ble->scanResponseData)
+			c_free(ble->scanResponseData);
 		c_free(ble);
 	}
 }
 
 void xs_ble_start_advertising(xsMachine *the)
 {
-	modBLE ble = xsmcGetHostData(xsThis);
+	modBLE ble = (modBLE)xsmcGetHostData(xsThis);
 	uint32_t intervalMin = xsmcToInteger(xsArg(0));
 	uint32_t intervalMax = xsmcToInteger(xsArg(1));
-	uint8_t *advertisingData = xsmcToArrayBuffer(xsArg(2));
+	uint8_t *advertisingData = (uint8_t*)xsmcToArrayBuffer(xsArg(2));
 	uint32_t advertisingDataLength = xsGetArrayBufferLength(xsArg(2));
-	uint8_t *scanResponseData = xsmcTest(xsArg(3)) ? xsmcToArrayBuffer(xsArg(3)) : NULL;
+	uint8_t *scanResponseData = xsmcTest(xsArg(3)) ? (uint8_t*)xsmcToArrayBuffer(xsArg(3)) : NULL;
 	uint32_t scanResponseDataLength = xsmcTest(xsArg(3)) ? xsGetArrayBufferLength(xsArg(3)) : 0;
 	
-	ble->advertisingData = c_malloc(advertisingDataLength);
+	// Save the advertising and scan response data. The buffers cannot be freed until the GAP callback confirmation.
+	ble->advertisingData = (uint8_t*)c_malloc(advertisingDataLength);
 	if (!ble->advertisingData)
 		xsUnknownError("no memory");
 	c_memmove(ble->advertisingData, advertisingData, advertisingDataLength);
 	if (scanResponseData) {
-		ble->scanResponseData = c_malloc(scanResponseDataLength);
+		ble->scanResponseData = (uint8_t*)c_malloc(scanResponseDataLength);
 		if (!ble->scanResponseData)
 			xsUnknownError("no memory");
 		c_memmove(ble->scanResponseData, scanResponseData, scanResponseDataLength);
 	}
 	
-	// config raw advertising data
-	// config raw scan response data
-	// start advertising with intervals specified
+	// Initialize the advertising parameters
+	ble->adv_params.adv_int_min = intervalMin;
+	ble->adv_params.adv_int_max = intervalMax;
+	ble->adv_params.adv_type = ADV_TYPE_IND;
+	ble->adv_params.own_addr_type = BLE_ADDR_TYPE_PUBLIC;
+	ble->adv_params.channel_map = ADV_CHNL_ALL;
+	ble->adv_params.adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY;
+
+	// Set the advertising and scan response data
+	ble->adv_config_done = adv_config_flag;
+	esp_ble_gap_config_adv_data_raw(advertisingData, advertisingDataLength);
+	if (scanResponseData) {
+		ble->adv_config_done |= scan_rsp_config_flag;
+		esp_ble_gap_config_scan_rsp_data_raw(scanResponseData, scanResponseDataLength);
+	}
 }
+	
 
 void xs_ble_stop_advertising(xsMachine *the)
 {
+	esp_ble_gap_stop_advertising();
 }
+
+static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
+{
+	xsBeginHost(gThe);
+	
+	modBLE ble = (modBLE)xsmcGetHostData(gThis);
+
+	switch(event) {
+		case ESP_GAP_BLE_ADV_DATA_RAW_SET_COMPLETE_EVT:
+			ble->adv_config_done &= ~adv_config_flag;
+			if (0 == ble->adv_config_done)
+				esp_ble_gap_start_advertising(&ble->adv_params);
+        	break;
+		case ESP_GAP_BLE_SCAN_RSP_DATA_RAW_SET_COMPLETE_EVT:
+			ble->adv_config_done &= ~scan_rsp_config_flag;
+			if (0 == ble->adv_config_done)
+				esp_ble_gap_start_advertising(&ble->adv_params);
+			break;
+		case ESP_GAP_BLE_ADV_START_COMPLETE_EVT:	// The advertising data can be freed after advertising starts
+			c_free(ble->advertisingData);
+			ble->advertisingData = NULL;
+			if (ble->scanResponseData) {
+				c_free(ble->scanResponseData);
+				ble->scanResponseData = NULL;
+			}
+			break;
+		case ESP_GAP_BLE_ADV_STOP_COMPLETE_EVT:
+			break;
+		default:
+			break;
+    }
+    
+	xsEndHost(gThe);
+}
+
+void xs_ble_set_device_name(xsMachine *the)
+{
+	char *deviceName = xsmcToString(xsArg(0));
+	esp_ble_gap_set_device_name(deviceName);
+}
+
+static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param)
+{
+	xsBeginHost(gThe);
+	
+	modBLE ble = (modBLE)xsmcGetHostData(gThis);
+
+	switch(event) {
+        case ESP_GATTS_REG_EVT:
+            break;
+    	case ESP_GATTS_CONNECT_EVT:
+			xsCall1(ble->obj, xsID_callback, xsString("onConnected"));
+        	break;
+    	case ESP_GATTS_DISCONNECT_EVT:
+    	    break;
+	}
+	
+	xsEndHost(gThe);
+}
+
 
