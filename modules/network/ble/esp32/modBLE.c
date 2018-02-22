0b42f17243f19d3159f90547701d375019d8d0db
Brian Friedkin <brian@moddable.tech>
2018-02-21 17:33:00 -0800

    ble: work in progress

--- modules/network/ble/esp32/modBLE.c
+++ modules/network/ble/esp32/modBLE.c
@@ -27,6 +27,7 @@
 
 #include "esp_gap_ble_api.h"
 #include "esp_gatts_api.h"
+#include "esp_gattc_api.h"
 #include "esp_bt_defs.h"
 #include "esp_bt_main.h"
 #include "esp_gatt_common_api.h"
@@ -36,25 +37,50 @@
 
 #define PROFILE_A_APP_ID 0
 
+enum {
+	IDLE = 0,
+	ADVERTISING,
+	SCANNING,
+	CONNECTING,
+	CONNECTED,
+	SERVICES
+};
+
 typedef struct {
 	xsMachine	*the;
 	xsSlot		obj;
 
+	uint16_t state;
+	
+	// server
+	esp_gatt_if_t gatts_if;
 	uint8_t *advertisingData;
 	uint8_t *scanResponseData;
-	
 	esp_ble_adv_params_t adv_params;
 	esp_ble_scan_params_t scan_params;
 	uint8_t adv_config_done;
-	uint8_t connecting;
+
+	// client
+	esp_gatt_if_t gattc_if;
+	uint16_t conn_id;
 } modBLERecord, *modBLE;
+
+typedef struct {
+	xsMachine	*the;
+	xsSlot		obj;
+
+	esp_gatt_if_t gattc_if;
+	uint16_t conn_id;
+} modBLEConnectionRecord, *modBLEConnection;
 
 static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
 static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
+static void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param);
 
 // @@ The ESP32 BT APIs don't support a refcon to tuck away this kind of stuff...
 static xsMachine *gThe;
 static xsSlot gThis;
+
 
 void xs_ble_initialize(xsMachine *the)
 {
@@ -77,7 +103,9 @@ void xs_ble_initialize(xsMachine *the)
 	// Register callbacks
 	ESP_ERROR_CHECK(esp_ble_gap_register_callback(gap_event_handler));
     ESP_ERROR_CHECK(esp_ble_gatts_register_callback(gatts_event_handler));
+    ESP_ERROR_CHECK(esp_ble_gattc_register_callback(gattc_event_handler));
     ESP_ERROR_CHECK(esp_ble_gatts_app_register(PROFILE_A_APP_ID));
+    ESP_ERROR_CHECK(esp_ble_gattc_app_register(PROFILE_A_APP_ID));
 
 	// Stack is ready
 	xsCall1(ble->obj, xsID_callback, xsString("onReady"));
@@ -177,8 +205,27 @@ void xs_ble_stop_scanning(xsMachine *the)
 void xs_ble_connect(xsMachine *the)
 {
 	modBLE ble = (modBLE)xsmcGetHostData(xsThis);
+	uint8_t *address = (uint8_t*)xsmcToArrayBuffer(xsArg(0));
+	esp_bd_addr_t bda;
+	c_memmove(bda, address, sizeof(bda));
+	ble->state = CONNECTING;
+    esp_ble_gattc_open(ble->gattc_if, bda, true);
+}
+
+void xs_ble_connection_initialize(xsMachine *the)
+{
+}
 	
-	// @@ xsArg(0) is an array buffer address
+void xs_ble_connection_disconnect(xsMachine *the)
+{
+}
+
+void xs_ble_gatt_client_discover_all_primary_services(xsMachine *the)
+{
+	esp_gatt_if_t gattc_if = xsmcToInteger(xsArg(0));
+	uint16_t conn_id = xsmcToInteger(xsArg(1));
+modLog("discover all primary services");
+	esp_ble_gattc_search_service(gattc_if, conn_id, NULL);
 }
 
 static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
@@ -212,7 +259,7 @@ static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param
 			esp_ble_gap_start_scanning(0);			// 0 == scan until explicitly stopped
 			break;
 		case ESP_GAP_BLE_SCAN_RESULT_EVT:
-			if (ESP_GAP_SEARCH_INQ_RES_EVT == param->scan_rst.search_evt) {
+			if ((ble->state < CONNECTING) && (ESP_GAP_SEARCH_INQ_RES_EVT == param->scan_rst.search_evt)) {
 				esp_ble_gap_cb_param_t *scan_result = param;
 				xsmcVars(3);
 				xsVar(0) = xsmcNewObject();
@@ -238,10 +285,9 @@ static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_
 
 	switch(event) {
         case ESP_GATTS_REG_EVT:
+        	ble->gatts_if = gatts_if;
             break;
     	case ESP_GATTS_CONNECT_EVT:
-    		// @@ Create connection object here
-			xsCall1(ble->obj, xsID_callback, xsString("onConnected"));
         	break;
     	case ESP_GATTS_DISCONNECT_EVT:
     	    break;
@@ -250,4 +296,72 @@ static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_
 	xsEndHost(gThe);
 }
 
+static void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param)
+{
+	xsBeginHost(gThe);
+	
+    esp_ble_gattc_cb_param_t *p_data = (esp_ble_gattc_cb_param_t *)param;
+	modBLE ble = (modBLE)xsmcGetHostData(gThis);
+
+    switch (event) {
+		case ESP_GATTC_REG_EVT:
+        	ble->gattc_if = gattc_if;
+        	break;
+    	case ESP_GATTC_CONNECT_EVT:
+    		if (ble->state == CONNECTING) {
+    			ble->state = CONNECTED;
+				xsmcVars(4);
+				xsVar(0) = xsmcNewObject();
+				xsmcSetInteger(xsVar(1), gattc_if);
+				xsmcSetInteger(xsVar(2), p_data->connect.conn_id);
+				xsmcSet(xsVar(0), xsID_iface, xsVar(1));
+				xsmcSet(xsVar(0), xsID_connection, xsVar(2));
+				xsmcSetArrayBuffer(xsVar(3), p_data->connect.remote_bda, 6);
+				xsmcSet(xsVar(0), xsID_address, xsVar(3));
+				xsCall2(ble->obj, xsID_callback, xsString("onConnected"), xsVar(0));
+    		}
+    		break;
+		case ESP_GATTC_SEARCH_RES_EVT: {
+			esp_gatt_srvc_id_t *srvc_id =(esp_gatt_srvc_id_t *)&p_data->search_res.srvc_id;
+			uint8_t uuid[16];
+			uint16_t uuid_length;
+modLog("found service");
+			if (1/*srvc_id->is_primary*/) {
+modLog("primary service");
+				if (srvc_id->id.uuid.len == ESP_UUID_LEN_16) {
+					uuid_length = ESP_UUID_LEN_16;
+					uuid[0] = srvc_id->id.uuid.uuid.uuid16 & 0xFF;
+					uuid[1] = (srvc_id->id.uuid.uuid.uuid16 >> 8) & 0xFF;
+				}
+				if (srvc_id->id.uuid.len == ESP_UUID_LEN_32) {
+					uuid_length = ESP_UUID_LEN_32;
+					uuid[0] = srvc_id->id.uuid.uuid.uuid32 & 0xFF;
+					uuid[1] = (srvc_id->id.uuid.uuid.uuid32 >> 8) & 0xFF;
+					uuid[2] = (srvc_id->id.uuid.uuid.uuid32 >> 16) & 0xFF;
+					uuid[3] = (srvc_id->id.uuid.uuid.uuid32 >> 24) & 0xFF;
+				}
+				else {
+					uuid_length = ESP_UUID_LEN_128;
+					c_memmove(uuid, srvc_id->id.uuid.uuid.uuid128, ESP_UUID_LEN_128);
+				}
+				xsmcVars(4);
+				xsVar(0) = xsmcNewObject();
+				xsmcSetArrayBuffer(xsVar(1), uuid, uuid_length);
+				xsmcSetInteger(xsVar(2), p_data->search_res.start_handle);
+				xsmcSetInteger(xsVar(3), p_data->search_res.end_handle);
+				xsmcSet(xsVar(0), xsID_uuid, xsVar(1));
+				xsmcSet(xsVar(0), xsID_start, xsVar(2));
+				xsmcSet(xsVar(0), xsID_end, xsVar(3));
+modLog("calling callback");
+				xsCall2(ble->obj, xsID_callback, xsString("_onService"), xsVar(0));
+			}
+			break;
+		}
+		case ESP_GATTC_SEARCH_CMPL_EVT:
+			xsCall1(ble->obj, xsID_callback, xsString("_onService"));	// procedure complete
+			break;
+	}
+
+	xsEndHost(gThe);
+}
 
