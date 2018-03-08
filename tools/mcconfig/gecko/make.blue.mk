4529fd646b9c0a8de7a1695850d6e63e8e25777f
Michael Kellner <mkellner@slate.local>
2018-03-08 12:12:34 -0800

    doc tweaks. blue starter

---
+++ tools/mcconfig/gecko/make.blue.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (c) 2016-2018  Moddable Tech, Inc.
+#
+#   This file is part of the Moddable SDK Tools.
+#
+#   The Moddable SDK Tools is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   The Moddable SDK Tools is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with the Moddable SDK Tools.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+BASE = /Applications/Simplicity?Studio.app/Contents/Eclipse/developer
+
+
+# BlueGecko WSTK	(BRD4100A) -- 32kB ram, 256kB flash
+PLATFORM_NAME = blue
+SDK_BASE = $(BASE)/sdks/gecko_sdk_suite/v2.1
+HWKIT = $(SDK_BASE)/hardware/kit/EFR32BG1_BRD4100A/config
+HWINC = $(SDK_BASE)/platform/Device/SiliconLabs/EFR32BG1P/Include
+HWPART = EFR32BG1P232F256GM48
+HWCPU = cortex-m4
+HW_DEBUG_OPT= -O0
+HW_OPT = -Os
+# DEV_C_FLAGS = -fno-short-enums
+DEV_C_FLAGS = 
+
+C_DEFINES += -DuseRTCC
+
+include $(MODDABLE)/tools/mcconfig/gecko/make.gecko_common.mk
+
