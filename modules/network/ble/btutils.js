451f85e3beea49c9d9dd661fff9fb5ef255d549d
Brian Friedkin <brian@moddable.tech>
2018-03-09 15:54:45 -0800

    ble: refactor a little to eliminate some modules

--- modules/network/ble/btutils.js
+++ modules/network/ble/btutils.js
@@ -1,296 +1,318 @@
 /*
- *     Copyright (C) 2010-2016 Marvell International Ltd.
- *     Copyright (C) 2002-2010 Kinoma, Inc.
+ * Copyright (c) 2016-2018  Moddable Tech, Inc.
  *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
+ *   This file is part of the Moddable SDK Runtime.
+ * 
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
+ */
+/*
+ * Portions based on Kinoma LowPAN Framework: Kinoma Bluetooth 4.2 Stack
  */
 
-/**
- * Kinoma LowPAN Framework: Kinoma Bluetooth Stack
- * Bluetooth v4.2 - Bluetooth Utilities
- */
+import Hex from "hex";
+import GAP from "gap";
 
-import ByteBuffer from "buffers";
-import Utils from "utils";
-
-const BD_ADDR_SIZE = 6;
-const BD_ADDR_SEPARATOR = ":";
-
-const UUID_SIZE = 16;
-const BASE_UUID = [
-	0xFB, 0x34, 0x9B, 0x5F, 0x80, 0x00,
-	0x00, 0x80,
-	0x00, 0x10,
-	0x00, 0x00,
-	0x00, 0x00, 0x00, 0x00
-];
-const UUID_SEPARATOR = "-";
-
-function isArrayEquals(a1, a2) {
-	if (a1.length != a2.length) {
-		return false;
+export class BluetoothAddress {
+	static toString(buffer) {
+		return Hex.toString(buffer, ':');
 	}
-	return isArrayEquals2(a1, 0, a2, 0, a1.length);
+	static toBuffer(string) {
+		return Hex.toBuffer(string, ':');
+	}
 }
 
-function isArrayEquals2(a1, off1, a2, off2, len) {
-	for (let i = 0; i < len; i++) {
-		if (a1[i + off1] != a2[i + off2]) {
-			return false;
-		}
-	}
-	return true;
-}
-
-/**
- * A class represents Bluetooth UUID
- */
 export class UUID {
-	constructor(byteArray) {
-		this._array = byteArray;
-	}
-	/**
-	 * Static function to create instance by byte array.
-	 */
-	static getByUUID(byteArray) {
-		if (byteArray.length != UUID_SIZE
-				&& byteArray.length != Utils.INT_16_SIZE) {
-			return null;
-		}
-		return new UUID(byteArray);
-	}
-	/**
-	 * Static function to create instance by 16-bit UUID.
-	 */
-	static getByUUID16(uuid16) {
-		return new UUID(Utils.toByteArray(uuid16, 2, true));
-	}
-	/**
-	 * Static function to create instance by string representation.
-	 */
-	static getByString(uuidString) {
-		if (uuidString.length == 4) {
-			/* UUID16 String */
-			return UUID.getByUUID16(parseInt(uuidString, 16));
-		}
-		let buffer = ByteBuffer.allocate(16, true);
-		let str = uuidString.split(UUID_SEPARATOR);
-		buffer.putInt16(parseInt(str[4].substring(8, 12), 16));
-		buffer.putInt16(parseInt(str[4].substring(4, 8), 16));
-		buffer.putInt16(parseInt(str[4].substring(0, 4), 16));
-		buffer.putInt16(parseInt(str[3], 16));
-		buffer.putInt16(parseInt(str[2], 16));
-		buffer.putInt16(parseInt(str[1], 16));
-		buffer.putInt32(parseInt(str[0], 16));
-		return new UUID(buffer.array);
-	}
-	/**
-	 * Return true if this UUID is 16-bit UUID.
-	 */
-	isUUID16() {
-		if (this._array.length == Utils.INT_16_SIZE) {
-			return true;
-		} else {
-			return isArrayEquals2(this._array, 0, BASE_UUID, 0, UUID_SIZE - 4);
-		}
-	}
-	/**
-	 * Get 16-bit UUID.
-	 */
-	toUUID16() {
-		let offset = (this._array.length == UUID_SIZE) ? 12 : 0;
-		return Utils.toInt(this._array, offset, Utils.INT_16_SIZE, true);
-	}
-	/**
-	 * Get 128-bit UUID in byte array. If this UUID is 16-bit UUID, will convert.
-	 */
-	toUUID128() {
-		if (this.isUUID16()) {
-			let array = BASE_UUID.slice();
-			array[12] = this._array[0];
-			array[13] = this._array[1];
-			array[14] = 0x00;
-			array[15] = 0x00;
-			return array;
-		}
-		return this._array;
-	}
-	/**
-	 * Get string representation of this UUID.
-	 */
-	toString() {
-		if (this.isUUID16()) {
-			return Utils.toHexString(this.toUUID16(), 2, "");
-		}
-		let uuidArray = this.toUUID128();
-		let str = Utils.toHexString(uuidArray[0], 1, "");
-		for (let i = 1; i < UUID_SIZE; i++) {
-			str = Utils.toHexString(uuidArray[i], 1, "") + str;
-			if (i == 5 || i == 7 || i == 9 || i == 11) {
-				str = UUID_SEPARATOR + str;
-			}
-		}
-		return str;
-	}
-	/**
-	 * Return raw array
-	 */
-	getRawArray() {
-		return this._array;
-	}
-	/**
-	 * Check if target equals to this UUID
-	 */
-	equals(target) {
-		if (this == target) {
-			return true;
-		}
-		if (!(target instanceof UUID)) {
-			return false;
-		}
-		if (this.isUUID16() && target.isUUID16()) {
-			return isArrayEquals(this._array, target._array);
-		} else {
-			return isArrayEquals(this.toUUID128(), target.toUUID128());
-		}
-	}
+	static toString(buffer) @ "xs_btuuid_toString"
+	static toBuffer(string) @ "xs_btuuid_toBuffer"
 }
 UUID.CCCD = '2902';
 
-/**
- * A class represents Bluetooth address
- */
-export class BluetoothAddress {
-	constructor(byteArray) {
-		this._array = byteArray;
+function serializeUUID16List(data) {
+	let count = data.length;
+	let buffer = new ArrayBuffer(count * 2);
+	let result = new Uint8Array(buffer);
+	for (let j = 0, i = 0; i < count; ++i) {
+		let uuid = new Uint8Array(UUID.toBuffer(data[i]));
+		result[j++] = uuid[1];
+		result[j++] = uuid[0];
 	}
-	/**
-	 * Static function to create instance by byte array
-	 */
-	static getByAddress(byteArray, random) {
-		if (byteArray.length < BD_ADDR_SIZE) {
-			return null;
-		}
-		if (byteArray.length >= (BD_ADDR_SIZE + 1)) {
-			return new LEBluetoothAddress(byteArray.slice(0, BD_ADDR_SIZE),
-				(byteArray[BD_ADDR_SIZE] == 0x01));
-		}
-		if (random !== undefined) {
-			return new LEBluetoothAddress(byteArray, random);
-		}
-		return new BluetoothAddress(byteArray);
-	}
-	/**
-	 * Static function to create instance by string representation.
-	 */
-	static getByString(addressString, random) {
-		let byteArray = new Uint8Array(BD_ADDR_SIZE);
-		if (BD_ADDR_SEPARATOR.length > 0) {
-			let ar = addressString.split(BD_ADDR_SEPARATOR);
-			for (let i = 0; i < BD_ADDR_SIZE; i++) {
-				byteArray[i] = parseInt(ar[i], 16);
-			}
-		} else {
-			for (let i = 0; i < BD_ADDR_SIZE; i++) {
-				let pos = i * 2;
-				byteArray[i] = parseInt(addressString.substring(pos, pos + 2), 16);
-			}
-		}
-		if (random !== undefined) {
-			return new LEBluetoothAddress(byteArray, random);
-		}
-		return new BluetoothAddress(byteArray);
-	}
-	/**
-	 * Get string representation of this address.
-	 */
-	toString() {
-		let byteArray = this.getRawArray();
-		let str = Utils.toHexString(byteArray[0], 1, "");
-		for (let i = 1; i < BD_ADDR_SIZE; i++) {
-			str += BD_ADDR_SEPARATOR + Utils.toHexString(byteArray[i], 1, "");
-		}
-		return str;
-	}
-	/**
-	 * Return raw array
-	 */
-	getRawArray() {
-		return this._array;
-	}
-	/**
-	 * Check if target equals to this BluetoothAddress
-	 */
-	equals(target) {
-		if (this == target) {
-			return true;
-		}
-		if (!(target instanceof BluetoothAddress)) {
-			return false;
-		}
-		return isArrayEquals(this._array, target._array);
-	}
+	return result;
 }
 
-const RANDOM_STATIC = 0x3;
-const RANDOM_PRIVATE = 0x0;
-const RANDOM_RESOLVABLE = 0x01;
+function serializeUUID128List(data) {
+	let count = data.length;
+	let buffer = new ArrayBuffer(count * 16);
+	let result = new Uint8Array(buffer);
+	for (let i = 0; i < count; ++i) {
+		let uuid = (new Uint8Array(UUID.toBuffer(data[i]))).reverse();
+		result.set(uuid, i * 16);
+	}
+	return result;
+}
 
-export class LEBluetoothAddress extends BluetoothAddress {
-	constructor(byteArray, random = false) {
-		super(byteArray);
-		this._random = random;
+function serializeString(data) {
+	let buffer = ArrayBuffer.fromString(data);
+	let result = new Uint8Array(buffer);
+	return result;
+}
+
+function serializeManufacturerSpecificData({identifier, data = null}) {
+	let length = 2 + (data ? data.length : 0);
+	let buffer = new ArrayBuffer(length);
+	let result = new Uint8Array(buffer);
+	result[0] = identifier & 0xFF;
+	result[1] = (identifier >> 8) & 0xFF;
+	if (data)
+		result.set(data, 2);
+	return result;
+}
+
+function serializeConnectionInterval({intervalMin, intervalMax}) {
+	let buffer = new ArrayBuffer(4);
+	let result = new DataView(buffer);
+	result.setUint16(intervalMin, 0, true);
+	result.setUint16(intervalMax, 2, true);
+	return result;
+}
+
+function serializeServiceData16({uuid, data = null}) {
+	let length = 2 + (data ? data.length : 0);
+	let buffer = new ArrayBuffer(length);
+	let result = new Uint8Array(buffer);
+	result[0] = uuid & 0xFF;
+	result[1] = (uuid >> 8) & 0xFF;
+	if (data)
+		result.set(data, 2);
+	return result;
+}
+
+function serializeServiceData128({uuid, data = null}) {
+	let length = 16 + (data ? data.length : 0);
+	let buffer = new ArrayBuffer(length);
+	let result = new Uint8Array(buffer);
+	result.set((new Uint8Array(UUID.toBuffer(uuid))).reverse(), 0);
+	if (data)
+		result.set(data, 16);
+	return result;
+}
+
+function serializeAddress(data) {
+	let result = new Uint8Array(BluetoothAddress.toBuffer(data));
+	return result;
+}
+
+const AdvertisementSerializer = {
+	["incompleteUUID16List"]: param => {
+		return {
+			type: GAP.ADType.INCOMPLETE_UUID16_LIST,
+			data: serializeUUID16List(param)
+		};
+	},
+	["completeUUID16List"]: param => {
+		return {
+			type: GAP.ADType.COMPLETE_UUID16_LIST,
+			data: serializeUUID16List(param)
+		};
+	},
+	["incompleteUUID128List"]: param => {
+		return {
+			type: GAP.ADType.INCOMPLETE_UUID128_LIST,
+			data: serializeUUID128List(param)
+		};
+	},
+	["completeUUID128List"]: param => {
+		return {
+			type: GAP.ADType.COMPLETE_UUID128_LIST,
+			data: serializeUUID128List(param)
+		};
+	},
+	["shortName"]: param => {
+		return {
+			type: GAP.ADType.SHORTENED_LOCAL_NAME,
+			data: serializeString(param)
+		};
+	},
+	["completeName"]: param => {
+		return {
+			type: GAP.ADType.COMPLETE_LOCAL_NAME,
+			data: serializeString(param)
+		};
+	},
+	["flags"]: param => {
+		return {
+			type: GAP.ADType.FLAGS,
+			data: [param & 0xFF]
+		};
+	},
+	["manufacturerSpecific"]: param => {
+		return {
+			type: GAP.ADType.MANUFACTURER_SPECIFIC_DATA,
+			data: serializeManufacturerSpecificData(param)
+		};
+	},
+	["txPowerLevel"]: param => {
+		return {
+			type: GAP.ADType.TX_POWER_LEVEL,
+			data: [param & 0xFF]
+		};
+	},
+	["connectionInterval"]: param => {
+		return {
+			type: GAP.ADType.MANUFACTURER_SPECIFIC_DATA,
+			data: serializeConnectionInterval(param)
+		};
+	},
+	["solicitationUUID16List"]: param => {
+		return {
+			type: GAP.ADType.SOLICITATION_UUID16_LIST,
+			data: [data & 0xFF, (data >> 8) & 0xFF]
+		};
+	},
+	["solicitationUUID128List"]: param => {
+		return {
+			type: GAP.ADType.SOLICITATION_UUID128_LIST,
+			data: serializeUUID128List(param)
+		};
+	},
+	["serviceDataUUID16"]: param => {
+		return {
+			type: GAP.ADType.SERVICE_DATA_UUID16,
+			data: serializeServiceData16(param)
+		};
+	},
+	["serviceDataUUID128"]: param => {
+		return {
+			type: GAP.ADType.SERVICE_DATA_UUID128,
+			data: serializeServiceData128(param)
+		};
+	},
+	["appearance"]: param => {
+		return {
+			type: GAP.ADType.APPEARANCE,
+			data: [data & 0xFF, (data >> 8) & 0xFF]
+		};
+	},
+	["publicAddress"]: param => {
+		return {
+			type: GAP.ADType.PUBLIC_TARGET_ADDRESS,
+			data: serializeAddress(param)
+		};
+	},
+	["randomAddress"]: param => {
+		return {
+			type: GAP.ADType.RANDOM_TARGET_ADDRESS,
+			data: serializeAddress(param)
+		};
+	},
+	["advertisingInterval"]: param => {
+		return {
+			type: GAP.ADType.ADVERTISING_INTERVAL,
+			data: serializeUint16(param)
+		};
+	},
+//	["deviceAddress"]: null,
+	["role"]: param => {
+		return {
+			type: GAP.ADType.LE_ROLE,
+			data: serializeByte(param)
+		};
+	},
+	["uri"]: param => {
+		return {
+			type: GAP.ADType.URI,
+			data: serializeString(param)
+		};
 	}
-	get type() {
-		return (this._array[BD_ADDR_SIZE - 1] >> 6) & 0x3;
+};
+
+export class Advertisement {
+	constructor(buffer) {
+		this._buffer = buffer;
+		this._data = new Uint8Array(buffer);
 	}
-	get typeString() {
-		if (!this._random) {
-			return "public";
+	get completeName() {
+		let index = this.find(GAP.ADType.COMPLETE_LOCAL_NAME);
+		if (-1 != index) {
+			return this._getStringType(index);
 		}
-		if (this.type == RANDOM_STATIC) {
-			return "static";
+	}
+	get shortName() {
+		let index = this.find(GAP.ADType.SHORTENED_LOCAL_NAME);
+		if (-1 != index)
+			return this._getStringType(index);
+	}
+	get manufacturerSpecific() {
+		let index = this.find(GAP.ADType.MANUFACTURER_SPECIFIC_DATA);
+		if (-1 != index) {
+			let data = this._data;
+			let adLength = data[index];
+			let start = index + 2;
+			let identifier = data[start] | (data[start+1] << 8);
+			start += 2;
+			let end = start + adLength - 1
+			return { identifier, data: new Uint8Array(this._buffer.slice(start, end)) };
 		}
-		if (this.type == RANDOM_RESOLVABLE) {
-			return "resolvable";
+	}
+	get flags() {
+		let index = this.find(GAP.ADType.FLAGS);
+		if (-1 != index)
+			return this._data[index+2];
+	}
+	find(type) {
+		let data = this._data;
+		let i = 0, length = data.byteLength;
+		while (i < length) {
+			let adLength = data[i]; ++i;
+			let adType = data[i]; ++i;
+			if (type == adType)
+				return i - 2;
+			else
+				i += adLength - 1;
 		}
-		return "private";
+		return -1;
 	}
-	isRandom() {
-		return this._random;
-	}
-	isIdentity() {
-		return !this._random || (this.type == RANDOM_STATIC);
-	}
-	isResolvable() {
-		return this._random && (this.type == RANDOM_RESOLVABLE);
-	}
-	equals(target) {
-		if (target instanceof LEBluetoothAddress) {
-			if (this.isRandom() != target.isRandom()) {
-				return false;
-			}
+	static serialize(obj) {
+		let parts = [];
+		for (let key in obj)
+			parts.push(AdvertisementSerializer[key](obj[key]));
+		let length = 2 * parts.length;	// one byte each for type and length
+		for (let i = 0; i < parts.length; ++i) {
+			let part = parts[i];
+			length += part.data.length;
 		}
-		return super.equals(target);
+		if (length > GAP.MAX_AD_LENGTH)
+			throw new Error("advertisement exceeds maximum length");
+		let buffer = new ArrayBuffer(length);
+		let advertisement = new Uint8Array(buffer);
+		for (let i = 0, j = 0; i < parts.length; ++i) {
+			let part = parts[i];
+			advertisement[j++] = 1 + part.data.length;
+			advertisement[j++] = part.type;
+			advertisement.set(part.data, j);
+			j += part.data.length;
+		}
+		return buffer;
 	}
-	toByteArray() {
-		let array = new Uint8Array(BD_ADDR_SIZE + 1);
-		array.set(this._array);
-		array[BD_ADDR_SIZE] = this._random ? 0x01 : 0x00;
-		return array;
+	
+	_getStringType(index) {
+		let adLength = this._data[index];
+		let start = index + 2, end = start + adLength - 1;
+		return String.fromArrayBuffer(this._buffer.slice(start, end));
 	}
 }
 
 export default {
-	UUID, BluetoothAddress, LEBluetoothAddress
+	BluetoothAddress, UUID, Advertisement
 };
