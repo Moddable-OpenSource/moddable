db9e2e5d23a30d64e5a56f81f85f4cd6b53bca30
Michael Kellner <mkellner@slate.local>
2018-03-14 00:06:30 -0700

    gecko: use new way to specify which peripheral interface to use

--- build/devices/gecko/base/debugger.c
+++ build/devices/gecko/base/debugger.c
@@ -30,6 +30,7 @@
 #if mxDebug
 #include "em_device.h"
 #include "em_usart.h"
+#include "em_leuart.h"
 #include "em_system.h"
 
 int gDebuggerSetup = 0;
@@ -42,6 +43,48 @@ int gDebuggerSetup = 0;
 #endif
 #ifndef MODDEF_DEBUGGER_BAUD
 	#define MODDEF_DEBUGGER_BAUD	115200
+#endif
+
+#define Q(x) #x
+#define QUOTE(x) Q(x)
+
+#if defined(MODDEF_DEBUGGER_INTERFACE_LEUART)
+	#if (MODDEF_DEBUGGER_INTERFACE_LEUART == 0)
+		#define DEBUGGER_PORT	LEUART0
+		#define DEBUGGER_CLOCK	cmuClock_LEUART0
+		#define DEBUGGER_INIT_CLOCK() CMU_ClockEnable(cmuClock_CORELE, true); CMU_ClockSelectSet(cmuClock_LFB, cmuSelect_LFXO); CMU_ClockDivSet(cmuClock_LEUART0, cmuClkDiv_1); CMU_ClockEnable(cmuClock_LEUART0, true);
+
+	#elif (MODDEF_DEBUGGER_INTERFACE_LEUART == 1)
+		#define DEBUGGER_PORT	LEUART1
+		#define DEBUGGER_CLOCK	cmuClock_LEUART1
+		#define DEBUGGER_INIT_CLOCK() CMU->LFCLKSEL &= ~_CMU_LFCLKSEL_LFB_MASK; CMU->LFCLKSEL |= CMU_LFCLKSEL_LFBE_ULFRCO; CMU->LFBCLKEN0 |= CMU_LFBCLKEN0_LEUART1;
+	#endif
+#elif defined(MODDEF_DEBUGGER_INTERFACE_UART)
+	#if (MODDEF_DEBUGGER_INTERFACE_UART == 0)
+		#define DEBUGGER_PORT	UART0
+		#define DEBUGGER_CLOCK	cmuClock_UART0
+		#define DEBUGGER_INIT_CLOCK() CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_UART0;
+	#elif (MODDEF_DEBUGGER_INTERFACE_UART == 1)
+		#define DEBUGGER_PORT	UART1
+		#define DEBUGGER_CLOCK	cmuClock_UART1
+		#define DEBUGGER_INIT_CLOCK() CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_UART1;
+	#endif
+#elif defined(MODDEF_DEBUGGER_INTERFACE_USART)
+	#if (MODDEF_DEBUGGER_INTERFACE_USART == 0)
+		#define DEBUGGER_PORT	USART0
+		#define DEBUGGER_CLOCK	cmuClock_USART0
+		#define DEBUGGER_INIT_CLOCK() CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_USART0;
+	#elif (MODDEF_DEBUGGER_INTERFACE_USART == 1)
+		#define DEBUGGER_PORT	USART1
+		#define DEBUGGER_CLOCK	cmuClock_USART1
+		#define DEBUGGER_INIT_CLOCK() CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_USART1;
+	#elif (MODDEF_DEBUGGER_INTERFACE_USART == 2)
+		#define DEBUGGER_PORT	USART2
+		#define DEBUGGER_CLOCK	cmuClock_USART2
+		#define DEBUGGER_INIT_CLOCK() CMU->HFPERCLKEN0 |= CMU_HFPERCLKEN0_USART2;
+	#endif
+#else
+	#error unknown debugger interface
 #endif
 
 void modLog_transmit(const char *msg)
@@ -64,21 +107,21 @@ void modLog_transmit(const char *msg)
 }
 
 void ESP_putc(int c) {
-    while( !(MODDEF_DEBUGGER_PORT->STATUS & (1 << 6)) ); // wait for TX buffer to empty
-//    while(USART_GetFlagStatus(MODDEF_DEBUGGER_PORT, USART_FLAG_TXE) == RESET ); // wait for TX buffer to empty
-    MODDEF_DEBUGGER_PORT->TXDATA = c;       // print each character of the test string
+    while( !(DEBUGGER_PORT->STATUS & (1 << 6)) ); // wait for TX buffer to empty
+//    while(USART_GetFlagStatus(DEBUGGER_PORT, USART_FLAG_TXE) == RESET ); // wait for TX buffer to empty
+    DEBUGGER_PORT->TXDATA = c;       // print each character of the test string
 }
 
 int ESP_getc(void) {
 	if (!ESP_isReadable())
 		return -1;
-	return MODDEF_DEBUGGER_PORT->RXDATA;
+	return DEBUGGER_PORT->RXDATA;
 }
 
 uint8_t ESP_isReadable() {
-	if (MODDEF_DEBUGGER_PORT->STATUS & (1<<7))
+	if (DEBUGGER_PORT->STATUS & (1<<7))
 		return 1;
-//	if (USART_GetFlagStatus(MODDEF_DEBUGGER_PORT, USART_FLAG_RXNE) == SET)
+//	if (USART_GetFlagStatus(DEBUGGER_PORT, USART_FLAG_RXNE) == SET)
 //		return 1;
 	return 0;
 }
@@ -89,40 +132,46 @@ int ESP_getc(void) { return -1; }
 uint8_t ESP_isReadable() { return 0; }
 #endif
 
-
 void setupDebugger() {
 #if mxDebug
 #if MIGHTY_GECKO || THUNDERBOARD2
 	USART_InitAsync_TypeDef serialInit = USART_INITASYNC_DEFAULT;
-	CMU_ClockEnable(MODDEF_DEBUGGER_CLOCK, true);
+	CMU_ClockEnable(DEBUGGER_CLOCK, true);
 
 	GPIO_PinModeSet(MODDEF_DEBUGGER_TX_PORT, MODDEF_DEBUGGER_TX_PIN, gpioModePushPull, 1);
 	GPIO_PinModeSet(MODDEF_DEBUGGER_RX_PORT, MODDEF_DEBUGGER_RX_PIN, gpioModeInput, 0);
 
 	serialInit.baudrate = MODDEF_DEBUGGER_BAUD;
 
-	USART_InitAsync(MODDEF_DEBUGGER_PORT, &serialInit);
+	USART_InitAsync(DEBUGGER_PORT, &serialInit);
 
-	MODDEF_DEBUGGER_PORT->ROUTEPEN = USART_ROUTEPEN_RXPEN | USART_ROUTEPEN_TXPEN;
-	MODDEF_DEBUGGER_PORT->ROUTELOC0  = ( MODDEF_DEBUGGER_PORT->ROUTELOC0 & ~( _USART_ROUTELOC0_TXLOC_MASK | _USART_ROUTELOC0_RXLOC_MASK ) );
-	MODDEF_DEBUGGER_PORT->ROUTELOC0 |= ( MODDEF_DEBUGGER_TX_LOCATION << _USART_ROUTELOC0_TXLOC_SHIFT );
-	MODDEF_DEBUGGER_PORT->ROUTELOC0 |= ( MODDEF_DEBUGGER_RX_LOCATION << _USART_ROUTELOC0_RXLOC_SHIFT );
+	DEBUGGER_PORT->ROUTEPEN = USART_ROUTEPEN_RXPEN | USART_ROUTEPEN_TXPEN;
+	DEBUGGER_PORT->ROUTELOC0  = ( DEBUGGER_PORT->ROUTELOC0 & ~( _USART_ROUTELOC0_TXLOC_MASK | _USART_ROUTELOC0_RXLOC_MASK ) );
+	DEBUGGER_PORT->ROUTELOC0 |= ( MODDEF_DEBUGGER_TX_LOCATION << _USART_ROUTELOC0_TXLOC_SHIFT );
+	DEBUGGER_PORT->ROUTELOC0 |= ( MODDEF_DEBUGGER_RX_LOCATION << _USART_ROUTELOC0_RXLOC_SHIFT );
 
 	gDebuggerSetup = 1;
 
 #elif GIANT_GECKO
-	USART_InitAsync_TypeDef serialInit = USART_INITASYNC_DEFAULT;
+	CMU_ClockEnable(DEBUGGER_CLOCK, true);
+	DEBUGGER_INIT_CLOCK();
 
 	GPIO_PinModeSet(MODDEF_DEBUGGER_TX_PORT, MODDEF_DEBUGGER_TX_PIN, gpioModePushPull, 1);
 	GPIO_PinModeSet(MODDEF_DEBUGGER_RX_PORT, MODDEF_DEBUGGER_RX_PIN, gpioModeInput, 0);
 
-	CMU_ClockEnable(MODDEF_DEBUGGER_CLOCK, true);
-
+#if defined(MODDEF_DEBUGGER_INTERFACE_LEUART)
+	LEUART_Init_TypeDef serialInit = LEUART_INIT_DEFAULT;
 	serialInit.baudrate = MODDEF_DEBUGGER_BAUD;
+	LEUART_Init(DEBUGGER_PORT, &serialInit);
+#elif defined(MODDEF_DEBUGGER_INTERFACE_UART) || defined(MODDEF_DEBUGGER_INTERFACE_USART)
+	USART_InitAsync_TypeDef serialInit = USART_INITASYNC_DEFAULT;
+	serialInit.baudrate = MODDEF_DEBUGGER_BAUD;
+	USART_InitAsync(DEBUGGER_PORT, &serialInit);
+#else
+	modLog("unknown debugger PORT");
+#endif
 
-	USART_InitAsync(MODDEF_DEBUGGER_PORT, &serialInit);
-
-	MODDEF_DEBUGGER_PORT->ROUTE = (MODDEF_DEBUGGER_LOCATION << _USART_ROUTE_LOCATION_SHIFT)
+	DEBUGGER_PORT->ROUTE = (MODDEF_DEBUGGER_LOCATION << _USART_ROUTE_LOCATION_SHIFT)
 			| USART_ROUTE_RXPEN | USART_ROUTE_TXPEN;
 
 	gDebuggerSetup = 1;
