cc5bca3bba6550772d3c7f1d917a19be16cec247
Michael Kellner <mkellner@moddable.tech>
2018-03-30 17:21:52 -0700

    gecko: analog, radio sample main.c's add simpleAnalog example

---
+++ examples/pins/simpleAnalog/main.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016-2018  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ * 
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+
+import Analog from "pins/analog";
+import Digital from "pins/digital";
+import Timer from "timer";
+
+Timer.repeat(id => {
+	let exciter = new Digital({pin: 6, port: "gpioPortB", mode: Digital.Output});
+	exciter.write(0);				// turn on ground to potentiometers
+	let value1 = Analog.read(1);
+	let value2 = Analog.read(2);
+	exciter.write(1);
+	trace("value1: " + value1 + " value2: " + value2 + "\n");
+}, 100);
