6dcd2cc79130093b57d338477e1698637137c6cb
Peter Hoddie <peter@moddable.tech>
2018-04-02 11:43:42 -0700

    add maxVersion to dictionary to reduce memory use; get working on esp8266

--- modules/data/qrcode/qrcode.c
+++ modules/data/qrcode/qrcode.c
@@ -28,58 +28,73 @@
 
 void xs_qrcode(xsMachine *the)
 {
-	uint8_t qr0[qrcodegen_BUFFER_LEN_MAX];
-	uint8_t tempBuffer[qrcodegen_BUFFER_LEN_MAX];
+	size_t bufferSize;
+	uint8_t *qr0 = NULL;
 	void *data;
 	xsType type;
 	uint8_t ok;
 	int x, y;
 	uint8_t *module;
+	int maxVersion = qrcodegen_VERSION_MAX;
 
-	xsmcVars(3);
+	xsTry {
+		xsmcVars(3);
 
-	xsmcGet(xsVar(0), xsArg(0), xsID_input);
-	type = xsmcTypeOf(xsVar(0));
-	if (xsStringType == type) {
-		data = xsmcToString(xsVar(0));
+		if (xsmcHas(xsArg(0), xsID_maxVersion)) {
+			xsmcGet(xsVar(0), xsArg(0), xsID_maxVersion);
+			maxVersion = xsmcToInteger(xsVar(0));
+		}
 
-		ok = qrcodegen_encodeText(data, tempBuffer, qr0,
-				qrcodegen_Ecc_MEDIUM, qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX, qrcodegen_Mask_AUTO, true);
-	}
-	else {
-		unsigned int dataSize;
+		bufferSize = qrcodegen_BUFFER_LEN_FOR_VERSION(maxVersion);
+		qr0 = c_malloc(bufferSize << 1);
+		if (!qr0)
+			xsUnknownError("no memory");
 
-		if (xsmcIsInstanceOf(xsVar(0), xsArrayBufferPrototype)) {
-			data = xsmcToArrayBuffer(xsVar(0));
-			dataSize = xsGetArrayBufferLength(xsVar(0));
+		xsmcGet(xsVar(0), xsArg(0), xsID_input);
+		type = xsmcTypeOf(xsVar(0));
+		if (xsStringType == type) {
+			data = xsmcToString(xsVar(0));
+			ok = qrcodegen_encodeText(data, qr0 + bufferSize, qr0,
+					qrcodegen_Ecc_MEDIUM, qrcodegen_VERSION_MIN, maxVersion, qrcodegen_Mask_AUTO, true);
 		}
 		else {
-			data = xsmcGetHostData(xsVar(0));
-			xsmcGet(xsVar(0), xsVar(0), xsID_byteLength);
-			dataSize = xsmcToInteger(xsVar(0));
+			unsigned int dataSize;
+
+			if (xsmcIsInstanceOf(xsVar(0), xsArrayBufferPrototype)) {
+				data = xsmcToArrayBuffer(xsVar(0));
+				dataSize = xsGetArrayBufferLength(xsVar(0));
+			}
+			else {
+				data = xsmcGetHostData(xsVar(0));
+				xsmcGet(xsVar(0), xsVar(0), xsID_byteLength);
+				dataSize = xsmcToInteger(xsVar(0));
+			}
+
+			c_memcpy(qr0 + bufferSize, data, dataSize);
+			ok = qrcodegen_encodeBinary(qr0 + bufferSize, dataSize, qr0,
+					qrcodegen_Ecc_MEDIUM, qrcodegen_VERSION_MIN, maxVersion, qrcodegen_Mask_AUTO, true);
 		}
 
-		if (dataSize > sizeof(tempBuffer))
-			xsUnknownError("input too big");
-		c_memcpy(tempBuffer, data, dataSize);
+		if (!ok)
+			xsUnknownError("qrcode failed");
 
-		ok = qrcodegen_encodeBinary(tempBuffer, dataSize, qr0,
-				qrcodegen_Ecc_MEDIUM, qrcodegen_VERSION_MIN, qrcodegen_VERSION_MAX, qrcodegen_Mask_AUTO, true);
+		int size = qrcodegen_getSize(qr0);
+
+		xsResult = xsArrayBuffer(NULL, size * size);
+
+		xsmcSetInteger(xsVar(0), size);
+		xsmcSet(xsResult, xsID_size, xsVar(0));
+
+		module = xsmcToArrayBuffer(xsResult);
+		for (y = 0; y < size; y++) {
+			for (x = 0; x < size; x++)
+				*module++ = qrcodegen_getModule(qr0, x, y);
+		}
+	}
+	xsCatch {
+		xsResult = xsUndefined;
 	}
 
-	if (!ok)
-		xsUnknownError("qtcode failed");
-
-	int size = qrcodegen_getSize(qr0);
-
-	xsResult = xsArrayBuffer(NULL, size * size);
-
-	xsmcSetInteger(xsVar(0), size);
-	xsmcSet(xsResult, xsID_size, xsVar(0));
-
-	module = xsmcToArrayBuffer(xsResult);
-	for (y = 0; y < size; y++) {
-		for (x = 0; x < size; x++)
-			*module++ = qrcodegen_getModule(qr0, x, y);
-	}
+	if (qr0)
+		c_free(qr0);
 }
