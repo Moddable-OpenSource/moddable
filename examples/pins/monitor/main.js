60ba38832e59ac70e2b02575d8bdc04f65ba9ebd
Michael Kellner <mkellner@moddable.tech>
2018-04-19 12:37:06 -0700

    tweaks - use config to specify pins on different hardware for monitor example

--- examples/pins/monitor/main.js
+++ examples/pins/monitor/main.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017  Moddable Tech, Inc.
+ * Copyright (c) 2018  Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK.
  * 
@@ -15,23 +15,39 @@
 import Timer from "timer";
 import Digital from "pins/digital";
 import Monitor from "monitor";
+import config from "mc/config";
 
-let led1 = new Digital({pin: 4, port: "gpioPortF", mode: Digital.Output});
-let led2 = new Digital({pin: 5, port: "gpioPortF", mode: Digital.Output});
+let led1 = null;
+let led2 = null;
 
-let monitor1 = new Monitor({pin: 6, port: "gpioPortF", mode: Digital.InputPullUp, edge: Monitor.Rising | Monitor.Falling});
-monitor1.onChanged = function() {
-	trace("Button 0, ", this.read(), " times\n");
+if (config.led1_pin)
+	led1 = new Digital({pin: config.led1_pin, port: config.led1_port, mode: Digital.Output});
+if (config.led2_pin)
+	led2 = new Digital({pin: config.led2_pin, port: config.led2_port, mode: Digital.Output});
+
+let monitor1 = null;
+let monitor2 = null;
+
+if (config.button1_pin) {
+	monitor1 = new Monitor({pin: config.button1_pin, port: config.button1_port, mode: Digital.InputPullUp, edge: Monitor.Rising | Monitor.Falling});
+	monitor1.onChanged = function() {
+		trace("Button 1, ", this.read(), " times\n");
+	}
 }
-let monitor2 = new Monitor({pin: 7, port: "gpioPortF", mode: Digital.InputPullUp, edge: Monitor.Rising | Monitor.Falling});
-monitor2.onChanged = function() {
-	trace("Button 1, ", this.read(), " times\n");
+
+if (config.button2_pin) {
+	monitor2 = new Monitor({pin: config.button2_pin, port: config.button2_port, mode: Digital.InputPullUp, edge: Monitor.Rising | Monitor.Falling});
+	monitor2.onChanged = function() {
+		trace("Button 2, ", this.read(), " times\n");
+	}
 }
 
 let count = 0;
 Timer.repeat(() => {
 	trace(`repeat ${++count} \n`);
-	led1.write(~count & 1);
-	led2.write(count & 1);
+	if (led1)
+		led1.write(~count & 1);
+	if (led2)
+		led2.write(count & 1);
 }, 200);
 
