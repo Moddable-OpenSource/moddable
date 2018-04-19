d287e3404cfe4af82c978e2b322c9caaa46f1373
Michael Kellner <mkellner@moddable.tech>
2018-04-19 16:04:43 -0700

    fix monitor example for esp32

--- examples/pins/monitor/main.js
+++ examples/pins/monitor/main.js
@@ -20,23 +20,35 @@ import config from "mc/config";
 let led1 = null;
 let led2 = null;
 
-if (config.led1_pin)
-	led1 = new Digital({pin: config.led1_pin, port: config.led1_port, mode: Digital.Output});
-if (config.led2_pin)
-	led2 = new Digital({pin: config.led2_pin, port: config.led2_port, mode: Digital.Output});
+if (undefined != config.led1_pin)
+	if (config.led1_port)
+		led1 = new Digital({pin: config.led1_pin, port: config.led1_port, mode: Digital.Output});
+	else
+		led1 = new Digital({pin: config.led1_pin, mode: Digital.Output});
+if (undefined != config.led2_pin)
+	if (config.led2_port)
+		led2 = new Digital({pin: config.led2_pin, port: config.led2_port, mode: Digital.Output});
+	else
+		led2 = new Digital({pin: config.led2_pin, mode: Digital.Output});
 
 let monitor1 = null;
 let monitor2 = null;
 
-if (config.button1_pin) {
-	monitor1 = new Monitor({pin: config.button1_pin, port: config.button1_port, mode: Digital.InputPullUp, edge: Monitor.Rising | Monitor.Falling});
+if (undefined != config.button1_pin) {
+	if (config.button1_port)
+		monitor1 = new Monitor({pin: config.button1_pin, port: config.button1_port, mode: Digital.InputPullUp, edge: Monitor.Rising | Monitor.Falling});
+	else
+		monitor1 = new Monitor({pin: config.button1_pin, mode: Digital.InputPullUp, edge: Monitor.Rising | Monitor.Falling});
 	monitor1.onChanged = function() {
 		trace("Button 1, ", this.read(), " times\n");
 	}
 }
 
-if (config.button2_pin) {
-	monitor2 = new Monitor({pin: config.button2_pin, port: config.button2_port, mode: Digital.InputPullUp, edge: Monitor.Rising | Monitor.Falling});
+if (undefined != config.button2_pin) {
+	if (config.button2_port)
+		monitor2 = new Monitor({pin: config.button2_pin, port: config.button2_port, mode: Digital.InputPullUp, edge: Monitor.Rising | Monitor.Falling});
+	else
+		monitor2 = new Monitor({pin: config.button2_pin, mode: Digital.InputPullUp, edge: Monitor.Rising | Monitor.Falling});
 	monitor2.onChanged = function() {
 		trace("Button 2, ", this.read(), " times\n");
 	}
