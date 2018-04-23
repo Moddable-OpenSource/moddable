9512d70a9bb371721c8e4ab19800b524a7450e71
Brian Friedkin <brian@moddable.tech>
2018-04-23 16:49:02 -0700

    ble: add characteristic notification support; heart rate server app

---
+++ examples/network/ble/heart-rate-server/main.js
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2016-2018  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ * 
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+ /*
+ 	https://www.bluetooth.org/docman/handlers/downloaddoc.ashx?doc_id=239866&_ga=2.113570511.973536733.1524505559-1286694985.1517851833
+ 	https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.heart_rate_measurement.xml
+	https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.body_sensor_location.xml
+ */
+
+import BLEServer from "bleserver";
+import Timer from "timer";
+
+const BPM_CHARACTERISTIC = "2A37";
+const BODY_SENSOR_LOCATION_CHARACTERISTIC = "2A38";
+const DEVICE_NAME = "Moddable HRM";
+
+const bpm = new Uint8Array([0, 60]);				// flags, beats per minute
+const body_sensor_location = new Uint8Array([1]);	// chest
+
+class HeartRateService extends BLEServer {
+	onReady() {
+		this.timer = 0;
+		this.deviceName = DEVICE_NAME;
+		this.onDisconnected();
+		this.deploy();
+	}
+	onConnected() {
+		this.stopAdvertising();
+	}
+	onDisconnected() {
+		if (this.timer) {
+			Timer.clear(this.timer);
+			this.timer = 0;
+		}
+		this.startAdvertising({
+			advertisingData: {shortName: "HRS", completeUUID16List: ["180D"]},
+			scanResponseData: {flags: 6, completeName: "HRS Example"}
+		});
+	}
+	onCharacteristicRead(params) {
+		let uuid = params.characteristic.uuid;
+		if (BODY_SENSOR_LOCATION_CHARACTERISTIC == uuid)
+			return body_sensor_location.buffer;
+		else if (BPM_CHARACTERISTIC == uuid)
+			return bpm.buffer;
+	}
+	onCharacteristicNotifyEnabled(params) {
+		let characteristic = params.characteristic;
+		this.timer = Timer.repeat(id => {
+			this.notifyValue(characteristic, bpm.buffer);
+		}, 1000);
+	}
+	onCharacteristicNotifyDisabled(params) {
+		Timer.clear(this.timer);
+		this.timer = 0;
+	}
+}
+
+let hrs = new HeartRateService;
+
+
+
