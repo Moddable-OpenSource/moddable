8da204234697d5d07894ff9d6c4f82aa61e66a98
Patrick Soquet <ps@moddable.tech>
2018-04-23 15:03:51 +0200

    XS linker freezes more objects

--- xs/tools/xsl.h
+++ xs/tools/xsl.h
@@ -218,7 +218,7 @@ extern void fxWriteStrips(txLinker* linker, FILE* file);
 
 /* xslSlot.c */
 extern void fxLinkerScriptCallback(txMachine* the);
-extern txInteger fxPrepareHeap(txMachine* the);
+extern txInteger fxPrepareHeap(txMachine* the, txBoolean stripping);
 extern void fxPrintBuilders(txMachine* the, FILE* file);
 extern void fxPrintHeap(txMachine* the, FILE* file, txInteger count, txBoolean dummy);
 extern void fxPrintStack(txMachine* the, FILE* file, txBoolean dummy);
