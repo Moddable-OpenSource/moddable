f7e5e333612b0918c93f7b3cedc0b7f5f8991739
Patrick Soquet <ps@moddable.tech>
2018-04-28 16:47:25 +0200

    module preference macOS

--- modules/files/preference/mac/modPreference.c
+++ modules/files/preference/mac/modPreference.c
@@ -18,21 +18,106 @@
  *
  */
 
-#include "xsmc.h"
+#include "xs.h"
+#include "mc.defines.h"
 
-void xs_preference_set(xsMachine *the)
+void xs_preference_delete(xsMachine *the)
 {
+	char buffer[1024];
+	c_strcpy(buffer, PIU_DOT_SIGNATURE);
+	c_strcat(buffer, "."); 
+	c_strcat(buffer, xsToString(xsArg(0))); 
+	CFStringRef domain = CFStringCreateWithCString(NULL, buffer, kCFStringEncodingUTF8);
+	CFStringRef key = CFStringCreateWithCString(NULL, xsToString(xsArg(1)), kCFStringEncodingUTF8);
+	CFPreferencesSetAppValue(key, NULL, domain);
+	CFPreferencesAppSynchronize(domain);
+	CFRelease(key);
+	CFRelease(domain);
 }
 
 void xs_preference_get(xsMachine *the)
 {
-}
-
-void xs_preference_delete(xsMachine *the)
-{
+	char buffer[1024];
+	c_strcpy(buffer, PIU_DOT_SIGNATURE);
+	c_strcat(buffer, "."); 
+	c_strcat(buffer, xsToString(xsArg(0))); 
+	CFStringRef domain = CFStringCreateWithCString(NULL, buffer, kCFStringEncodingUTF8);
+	CFStringRef key = CFStringCreateWithCString(NULL, xsToString(xsArg(1)), kCFStringEncodingUTF8);
+	CFTypeRef value = (CFTypeRef)CFPreferencesCopyAppValue(key, domain);
+	if (value) {
+		CFTypeID type = CFGetTypeID(value);
+		if (type == CFBooleanGetTypeID()) {
+			xsResult = xsBoolean(CFBooleanGetValue(value));
+		}
+		else if (type == CFNumberGetTypeID()) {
+			if (CFNumberIsFloatType(value)) {
+				xsNumberValue number;
+				CFNumberGetValue(value, kCFNumberDoubleType, &number);
+				xsResult = xsNumber(number);
+			}
+			else {
+				xsIntegerValue integer;
+				CFNumberGetValue(value, kCFNumberLongType, &integer);
+				xsResult = xsInteger(integer);
+			}
+		}
+		else if (type == CFStringGetTypeID()) {
+			CFRange range = CFRangeMake(0, CFStringGetLength(value));
+			CFIndex length;
+			CFStringGetBytes(value, range, kCFStringEncodingUTF8, 0, false, NULL, 0, &length);
+			xsResult = xsStringBuffer(NULL, length);
+			CFStringGetBytes(value, range, kCFStringEncodingUTF8, 0, false, (UInt8 *)xsToString(xsResult), length, NULL);
+		}
+		else
+			xsResult = xsArrayBuffer((void*)CFDataGetBytePtr(value), CFDataGetLength(value));
+		CFRelease(value);
+	}
+	CFRelease(key);
+	CFRelease(domain);
 }
 
 void xs_preference_keys(xsMachine *the)
 {
+	xsUnknownError("unimplemented");
+}
+
+void xs_preference_set(xsMachine *the)
+{
+	CFTypeRef value = NULL;
+	switch (xsTypeOf(xsArg(2))) {
+	case xsBooleanType:
+		value = (xsToBoolean(xsArg(2))) ? kCFBooleanTrue : kCFBooleanFalse;
+		break;
+	case xsIntegerType: {
+		xsIntegerValue integer = xsToInteger(xsArg(2));
+		value = CFNumberCreate(NULL, kCFNumberLongType, &integer);
+		} break;
+	case xsNumberType: {
+		xsNumberValue number = xsToNumber(xsArg(2));
+		value = CFNumberCreate(NULL, kCFNumberDoubleType, &number);
+		} break;
+	case xsStringType:
+		value = CFStringCreateWithCString(NULL, xsToString(xsArg(2)), kCFStringEncodingUTF8);
+		break;
+	case xsReferenceType:
+		if (xsIsInstanceOf(xsArg(2), xsArrayBufferPrototype))
+			value = CFDataCreate(NULL, xsToArrayBuffer(xsArg(2)), xsGetArrayBufferLength(xsArg(2)));
+		break;
+	}
+	if (value) {
+		char buffer[1024];
+		c_strcpy(buffer, PIU_DOT_SIGNATURE);
+		c_strcat(buffer, "."); 
+		c_strcat(buffer, xsToString(xsArg(0))); 
+		CFStringRef domain = CFStringCreateWithCString(NULL, buffer, kCFStringEncodingUTF8);
+		CFStringRef key = CFStringCreateWithCString(NULL, xsToString(xsArg(1)), kCFStringEncodingUTF8);
+		CFPreferencesSetAppValue(key, value, domain);
+		CFPreferencesAppSynchronize(domain);
+		CFRelease(key);
+		CFRelease(domain);
+		CFRelease(value);
+	}
+	else
+		xsUnknownError("unsupported type");
 }
 
