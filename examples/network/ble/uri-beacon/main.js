0817629884906dd9b588d1ce4919c9dd6a68f0db
Brian Friedkin <brian@moddable.tech>
2018-05-11 17:50:38 -0700

    ble: optimizations

--- examples/network/ble/uri-beacon/main.js
+++ examples/network/ble/uri-beacon/main.js
@@ -20,6 +20,9 @@ import BLEServer from "bleserver";
 
 const URI = "http://www.moddable.com";
 
+const prefixes = [ 'http://www.', 'https://www.', 'http://', 'https://', 'urn:uuid:' ];
+const suffixes = [ '.com/', '.org/', '.edu/', '.net/', '.info/', '.biz/', '.gov/', '.com', '.org', '.edu', '.net', '.info', '.biz', '.gov' ];
+
 class URIBeacon extends BLEServer {
 	onReady() {
 		this.startAdvertising({
@@ -29,90 +32,15 @@ class URIBeacon extends BLEServer {
 	encodeData(uri) {
 		let flags = 0;
 		let txPower = 0xEE;
-		let prefix, suffix;
-		
-		if (uri.startsWith('http://www.')) {
-			uri = uri.slice(11);
-			prefix = 0;
-		}
-		else if (uri.startsWith('https://www.')) {
-			uri = uri.slice(12);
-			prefix = 1;
-		}
-		else if (uri.startsWith('http://')) {
-			uri = uri.slice(7);
-			prefix = 2;
-		}
-		else if (uri.startsWith('https://')) {
-			uri = uri.slice(8);
-			prefix = 3;
-		}
-		else if (uri.startsWith('urn:uuid:')) {
-			uri = uri.slice(9);
-			prefix = 4;
-		}
-		else
+		let prefix = prefixes.findIndex(item => uri.startsWith(item));
+		if (-1 == prefix)
 			throw new Error('invalid uri prefix');
-
-		if (uri.endsWith('.com/')) {
-			uri = uri.slice(0, -5);
-			suffix = 0;
-		}
-		else if (uri.endsWith('.org/')) {
-			uri = uri.slice(0, -5);
-			suffix = 1;
-		}
-		else if (uri.endsWith('.edu/')) {
-			uri = uri.slice(0, -5);
-			suffix = 2;
-		}
-		else if (uri.endsWith('.net/')) {
-			uri = uri.slice(0, -5);
-			suffix = 3;
-		}
-		else if (uri.endsWith('.info/')) {
-			uri = uri.slice(0, -6);
-			suffix = 4;
-		}
-		else if (uri.endsWith('.biz/')) {
-			uri = uri.slice(0, -5);
-			suffix = 5;
-		}
-		else if (uri.endsWith('.gov/')) {
-			uri = uri.slice(0, -5);
-			suffix = 6;
-		}
-		else if (uri.endsWith('.com')) {
-			uri = uri.slice(0, -4);
-			suffix = 7;
-		}
-		else if (uri.endsWith('.org')) {
-			uri = uri.slice(0, -4);
-			suffix = 8;
-		}
-		else if (uri.endsWith('.edu')) {
-			uri = uri.slice(0, -4);
-			suffix = 9;
-		}
-		else if (uri.endsWith('.net')) {
-			uri = uri.slice(0, -4);
-			suffix = 10;
-		}
-		else if (uri.endsWith('.info')) {
-			uri = uri.slice(0, -5);
-			suffix = 11;
-		}
-		else if (uri.endsWith('.biz')) {
-			uri = uri.slice(0, -4);
-			suffix = 12;
-		}
-		else if (uri.endsWith('.gov')) {
-			uri = uri.slice(0, -4);
-			suffix = 13;
-		}
-		else
+		uri = uri.slice(prefixes[prefix].length);
+		let suffix = suffixes.findIndex(item => uri.endsWith(item));
+		if (-1 == suffix)
 			throw new Error('invalid uri suffix');
-			
+		uri = uri.slice(0, -suffixes[suffix].length);
+
 		let data = new Array(flags, txPower, prefix);
 		data = data.concat(Array.from(new Uint8Array(ArrayBuffer.fromString(uri))));
 		data.push(suffix);
