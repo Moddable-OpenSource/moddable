81a6f735c71988801965d6423a8b6bfb356189a2
Brian Friedkin <brian@moddable.tech>
2018-05-14 12:23:21 -0700

    ble: add stub client for completeness

---
+++ modules/network/ble/sim/modBLEClient.c
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2016-2018  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ * 
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "xsmc.h"
+
+void xs_ble_client_initialize(xsMachine *the)
+{
+}
+
+void xs_ble_client_close(xsMachine *the)
+{
+}
+
+void xs_ble_client_destructor(void *data)
+{
+}
+
+void xs_ble_client_set_device_name(xsMachine *the)
+{
+}
+
+void xs_ble_client_start_scanning(xsMachine *the)
+{
+}
+
+void xs_ble_client_stop_scanning(xsMachine *the)
+{
+}
+
+void xs_ble_client_connect(xsMachine *the)
+{
+}
+
+void xs_gap_connection_initialize(xsMachine *the)
+{
+}
+	
+void xs_gap_connection_disconnect(xsMachine *the)
+{
+}
+
+void xs_gap_connection_read_rssi(xsMachine *the)
+{
+}
+
+void xs_gatt_client_discover_primary_services(xsMachine *the)
+{
+}
+
+void xs_gatt_service_discover_characteristics(xsMachine *the)
+{
+}
+
+void xs_gatt_characteristic_discover_all_characteristic_descriptors(xsMachine *the)
+{
+}
+
+void xs_gatt_characteristic_write_without_response(xsMachine *the)
+{
+}
+
+void xs_gatt_characteristic_read_value(xsMachine *the)
+{
+}
+
+void xs_gatt_characteristic_enable_notifications(xsMachine *the)
+{
+}
+
+void xs_gatt_characteristic_disable_notifications(xsMachine *the)
+{
+}
+
+void xs_gatt_descriptor_write_value(xsMachine *the)
+{
+}
