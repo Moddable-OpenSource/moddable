c9b706d42beb1f8e866f59407c26162136064fc6
chris <chris@moddable.tech>
2018-05-22 15:26:57 -0700

    Set of Open Sans fonts for development. Full set (with degree symbol) and a numbers set in multiple sizes.

---
+++ examples/pins/servo-sweep/main.js
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016-2017  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ *
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+
+import Servo from "pins/servo";
+import Timer from "timer";
+
+/* let servo = new Servo({pin: 5}); */
+let servo = new Servo({pin: 5, min: 500, max: 2400});
+
+let angle = 0;
+Timer.repeat(() => {
+	servo.write(angle);
+	angle = angle ? 0 : 180;
+}, 3000);
