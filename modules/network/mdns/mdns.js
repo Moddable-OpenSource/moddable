2bc588f9c77dff0dcbed933ce097c071ff4acce2
Peter Hoddie <peter@moddable.tech>
2018-06-11 09:54:37 -0700

    handle name look-up

--- modules/network/mdns/mdns.js
+++ modules/network/mdns/mdns.js
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2018  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ *
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 import {Socket} from "socket";
 import Net from "net";
 
@@ -95,15 +115,17 @@ class MDNS extends Socket {
 				return;
 		}
 
-		if (!service || !protocol)
-			return;
+		if (service || protocol) {
+			if (service && protocol) {
+				service = this.services.find(item => (item.name === service) && (item.protocol === protocol))
+				if (!service)
+					return;
+			}
+		}
 
-		service = this.services.find(item => (item.name === service) && (item.protocol === protocol))
-		if (!service)
-			return;
 
 		// respond
-		let numQuestions = Math.min((header[4] << 8) | header[5], 4);
+		let numQuestions = (header[4] << 8) | header[5]
 		let mask = 0;
 		while (numQuestions--) {
 			let cType = this.read(Number) << 8;
