f60b99143fddfc5617468336cfc099cd2be6a0ea
Brian Friedkin <brian@moddable.tech>
2018-06-19 12:31:59 -0700

    windows: time stub

---
+++ modules/base/time/win/modTime.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2016-2018  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ * 
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+
+#include "xs.h"
+
+void xs_time_set(xsMachine *the)
+{
+}
+
+void xs_time_timezone_get(xsMachine *the)
+{
+}
+
+void xs_time_timezone_set(xsMachine *the)
+{
+}
+
+void xs_time_dst_get(xsMachine *the)
+{
+}
+
+void xs_time_dst_set(xsMachine *the)
+{
+}
+
+void xs_time_ticks(xsMachine *the)
+{
+}
