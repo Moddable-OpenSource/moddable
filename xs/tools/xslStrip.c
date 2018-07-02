e6b9c60761c3c842624d26e41c960c8807536902
Patrick Soquet <ps@moddable.tech>
2018-07-02 16:26:46 +0200

    XS: strip shared cluster - REBUILD ALL FROM SCRATCH

--- xs/tools/xslStrip.c
+++ xs/tools/xslStrip.c
@@ -322,6 +322,14 @@ void fxStripDefaults(txLinker* linker, FILE* file)
 		fprintf(file, "\tfxRunProgramEnvironment,\n");
 	else
 		fprintf(file, "\tC_NULL,\n");
+	if (fxIsLinkerSymbolUsed(linker, mxID(_Atomics))) {
+		fprintf(file, "\tfxInitializeSharedCluster,\n");
+		fprintf(file, "\tfxTerminateSharedCluster,\n");
+	}
+	else {
+		fprintf(file, "\tC_NULL,\n");
+		fprintf(file, "\tC_NULL,\n");
+	}
 	fprintf(file, "};\n\n");
 
 	fprintf(file, "const txBehavior* ICACHE_RAM_ATTR gxBehaviors[XS_BEHAVIOR_COUNT]  = {\n");
