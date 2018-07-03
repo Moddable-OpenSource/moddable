5e9758f2d8a7403f4f872b362e4cd7d1de8e674c
Peter Hoddie <peter@moddable.tech>
2018-07-03 11:27:19 -0700

    use release tools

--- tools/mcconfig/make.esp32.mk
+++ tools/mcconfig/make.esp32.mk
@@ -125,9 +125,9 @@ ESPTOOL = $(IDF_PATH)/components/esptool_py/esptool/esptool.py
 AR_FLAGS = crs
 
 ifeq ($(HOST_OS),Darwin)
-MODDABLE_TOOLS_DIR = $(BUILD_DIR)/bin/mac/debug
+MODDABLE_TOOLS_DIR = $(BUILD_DIR)/bin/mac/release
 else
-MODDABLE_TOOLS_DIR = $(BUILD_DIR)/bin/lin/debug
+MODDABLE_TOOLS_DIR = $(BUILD_DIR)/bin/lin/release
 endif
 BUILDCLUT = $(MODDABLE_TOOLS_DIR)/buildclut
 COMPRESSBMF = $(MODDABLE_TOOLS_DIR)/compressbmf
