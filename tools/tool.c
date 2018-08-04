0e169c2cf54e2f022fc6865f932b6c3b18776e92
Patrick Soquet <ps@moddable.tech>
2018-08-04 12:38:45 -0700

    XS: xsPrepareMachine - REBUILD ALL FROM SCRATCH

--- tools/tool.c
+++ tools/tool.c
@@ -22,7 +22,6 @@
 #include "xsAll.h"
 #include "xs.h"
 #include "mc.xs.h"
-extern txPreparation* xsPreparation();
 
 #if mxWindows
 	#include <direct.h>
@@ -58,30 +57,9 @@ void fxAbort(xsMachine* the)
 
 int main(int argc, char* argv[]) 
 {
-	static txMachine root;
 	int error = 0;
-	txMachine* machine = &root;
-	txPreparation* preparation = xsPreparation();
-	
-	c_memset(machine, 0, sizeof(txMachine));
-	machine->preparation = preparation;
-	machine->keyArray = preparation->keys;
-	machine->keyCount = (txID)preparation->keyCount + (txID)preparation->creation.keyCount;
-	machine->keyIndex = (txID)preparation->keyCount;
-	machine->nameModulo = preparation->nameModulo;
-	machine->nameTable = preparation->names;
-	machine->symbolModulo = preparation->symbolModulo;
-	machine->symbolTable = preparation->symbols;
-	
-	machine->stack = &preparation->stack[0];
-	machine->stackBottom = &preparation->stack[0];
-	machine->stackTop = &preparation->stack[preparation->stackCount];
-	
-	machine->firstHeap = &preparation->heap[0];
-	machine->freeHeap = &preparation->heap[preparation->heapCount - 1];
-	machine->aliasCount = (txID)preparation->aliasCount;
 
-	machine = fxCloneMachine(&preparation->creation, machine, "tool", NULL);
+	xsMachine* machine = fxPrepareMachine(NULL, xsPreparation(), "tool", NULL, NULL);
 	
 	xsBeginHost(machine);
 	{
