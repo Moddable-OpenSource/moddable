7532f14316407e766e682ff130fa4343fc98d845
Michael Kellner <mkellner@moddable.tech>
2018-09-20 20:19:35 -0700

    support base m5stack module

---
+++ build/devices/esp32/targets/m5stack/setup-target.js
@@ -0,0 +1,28 @@
+import Digital from "pins/digital";
+import Monitor from "monitor";
+
+import AudioOut from "pins/i2s";
+import Resource from "Resource";
+
+export default function (done) {
+	global.button = {
+		a: new Monitor({pin: 39, mode: Digital.InputPullUp, edge: Monitor.RisingEdge | Monitor.FallingEdge}),
+		b: new Monitor({pin: 38, mode: Digital.InputPullUp, edge: Monitor.RisingEdge | Monitor.FallingEdge}),
+		c: new Monitor({pin: 37, mode: Digital.InputPullUp, edge: Monitor.RisingEdge | Monitor.FallingEdge}),
+	};
+	button.a.onChanged = button.b.onChanged = button.c.onChanged = nop;
+
+	global.speaker = new AudioOut({streams: 4});
+	speaker.callback = function() {this.stop()};
+	speaker.enqueue(0, AudioOut.Samples, new Resource("bflatmajor.maud"));
+	speaker.enqueue(0, AudioOut.Callback, 0);
+	speaker.start();
+
+	//@@ microphone
+
+	done();
+}
+
+function nop() {
+}
+
