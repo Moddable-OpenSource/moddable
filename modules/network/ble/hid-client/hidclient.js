131e7d07a201c6d9209fd69c133ca2c44eaa53b5
Brian Friedkin <brian@moddable.tech>
2018-09-21 14:58:43 -0700

    boilerplate

--- modules/network/ble/hid-client/hidclient.js
+++ modules/network/ble/hid-client/hidclient.js
@@ -1,14 +1,20 @@
  /*
  * Copyright (c) 2016-2018  Moddable Tech, Inc.
  *
- *   This file is part of the Moddable SDK.
+ *   This file is part of the Moddable SDK Runtime.
  * 
- *   This work is licensed under the
- *       Creative Commons Attribution 4.0 International License.
- *   To view a copy of this license, visit
- *       <http://creativecommons.org/licenses/by/4.0>.
- *   or send a letter to Creative Commons, PO Box 1866,
- *   Mountain View, CA 94042, USA.
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 /*
