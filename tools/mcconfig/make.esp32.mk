c58f49580165219e6cff09ef7054913ce8f4068e
Brian Friedkin <brian@moddable.tech>
2018-10-15 16:15:41 -0700

    update for latest ESP-IDF

--- tools/mcconfig/make.esp32.mk
+++ tools/mcconfig/make.esp32.mk
@@ -61,6 +61,9 @@ INC_DIRS = \
  	$(IDF_PATH)/components/freertos \
  	$(IDF_PATH)/components/freertos/include \
  	$(IDF_PATH)/components/freertos/include/freertos \
+ 	$(IDF_PATH)/components/lwip/lwip/src/include \
+ 	$(IDF_PATH)/components/lwip/port/esp32/include \
+ 	$(IDF_PATH)/components/lwip/include/apps \
  	$(IDF_PATH)/components/lwip/include/lwip \
  	$(IDF_PATH)/components/lwip/include/lwip/port \
  	$(IDF_PATH)/components/mbedtls/include \
