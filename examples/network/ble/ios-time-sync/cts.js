d1c60d35beee96c401e94accfe4d18979463ab86
Brian Friedkin <brian@moddable.tech>
2018-10-22 14:03:43 -0700

    ble: example app that synchronizes ESP32 clock over BLE from an iPhone

---
+++ examples/network/ble/ios-time-sync/cts.js
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2016-2018  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ * 
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+
+import BLEClient from "bleclient";
+import BLEServer from "bleserver";
+import {uuid} from "btutils";
+import GAP from "gap";
+
+class CTSAuthenticator extends BLEServer {
+	constructor(client) {
+		super();
+		this.client = client;
+	}
+	onReady() {
+		this.deviceName = "Moddable Zero";
+		this.securityParameters = { mitm:true };
+		this.onDisconnected();
+	}
+	onConnected(device) {
+		this.device = device;
+		this.stopAdvertising();
+	}
+	onAuthenticated() {
+		this.client.onAuthenticated(this.device);
+	}
+	onDisconnected() {
+		this.startAdvertising({
+			advertisingData: {flags: 6, completeName: this.deviceName, solicitationUUID16List: [uuid`1805`]}
+		});
+	}
+}
+Object.freeze(CTSAuthenticator.prototype);
+
+class CTSClient extends BLEClient {
+	constructor(client, device) {
+		super();
+		this.client = client;
+		this.device = device;
+	}
+	onReady() {
+		this.CURRENT_TIME_SERVICE_UUID = uuid`1805`;
+		this.CURRENT_TIME_CHARACTERISTIC_UUID = uuid`2A2B`;
+		this.APPLE_IDENTIFIER = 0x004C;
+		this.securityParameters = { mitm:true };
+	}
+	onSecurityParameters() {
+		this.connect(this.device);
+	}
+	onConnected(device) {
+		device.discoverPrimaryService(this.CURRENT_TIME_SERVICE_UUID);
+	}
+	onServices(services) {
+		let time_service = services.find(service => service.uuid.equals(this.CURRENT_TIME_SERVICE_UUID));
+		if (time_service)
+			time_service.discoverCharacteristic(this.CURRENT_TIME_CHARACTERISTIC_UUID);
+	}
+	onCharacteristics(characteristics) {
+		if (characteristics.length)
+			characteristics[0].readValue();
+	}
+	onCharacteristicValue(characteristic, value) {
+		let current_date_time = new Uint8Array(value);
+		let year = (current_date_time[1] << 8) | current_date_time[0];
+		let month = current_date_time[2];
+		let day = current_date_time[3];
+		let hour = current_date_time[4];
+		let minute = current_date_time[5];
+		let second = current_date_time[6];
+		let day_of_week = current_date_time[7];
+		
+		let date = new Date(year, month - 1, day, hour, minute, second);
+		this.client.onTimeRead(Math.round(date/1000.0));
+	}
+}
+Object.freeze(CTSClient.prototype);
+
+export {CTSAuthenticator, CTSClient};
---
+++ examples/network/ble/ios-time-sync/main.js
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2016-2018  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ * 
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+/*
+	This example demonstrates how to synchronize the ESP32 clock with an iPhone over BLE.
+	Launch the example and connect to the Moddable Zero from your iPhone Bluetooth settings.
+	After synchronizing the time over BLE, the current time and date will be displayed on the LCD.
+	
+	mcconfig -d -m -p esp32/moddable_zero -r 270
+
+	https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.service.current_time.xml
+*/
+
+import {CTSAuthenticator, CTSClient} from "cts";
+import Time from "time";
+import Timer from "timer";
+import Poco from "commodetto/Poco";
+import parseBMF from "commodetto/parseBMF";
+import Resource from "Resource";
+
+let render = new Poco(screen, { rotation:270 });
+let timeFont = parseBMF(new Resource("OpenSans-Regular-52.bf4"));
+let dateFont = parseBMF(new Resource("OpenSans-Regular-20.bf4"));
+let blue = render.makeColor(0x19, 0x2e, 0xab);
+let white = render.makeColor(255, 255, 255);
+
+class TimeSyncClient {
+	start() {
+		this.server = new CTSAuthenticator(this);
+	}
+	onAuthenticated(device) {
+		this.client = new CTSClient(this, device);
+	}
+	onTimeRead(date) {    
+		Time.set(date);
+		this.server.close();
+		this.client.close();
+		this.displayTime();
+	}
+	displayTime() {
+		let timer = Timer.repeat(() => {
+			let date = new Date();
+			let hours = String(date.getHours());
+			let minutes = String(date.getMinutes());
+			let seconds = String(date.getSeconds());
+			let ampm = (hours > 11 ? ' PM' : ' AM');
+			if (hours > 12)
+				hours -= 12;
+			if (1 == minutes.length)
+				minutes = '0' + minutes;
+			if (1 == seconds.length)
+				seconds = '0' + seconds;
+			let time = hours + ':' + minutes + ':' + seconds + ampm;
+			let timeWidth = render.getTextWidth(time, timeFont);
+			let x = (render.width - timeWidth) >> 1;
+			let y = ((render.height - timeFont.height) >> 1) - 10;
+			render.begin(x, y, timeWidth, timeFont.height);
+				render.fillRectangle(blue, 0, 0, render.width, render.height);
+				render.drawText(time, timeFont, white, x, y);
+			render.end();
+			
+			date = date.toString().split(' ');
+			date.length = 4;
+			date = date.join(' ');
+			let dateWidth = render.getTextWidth(date, dateFont);
+			x = (render.width - dateWidth) >> 1;
+			y = (render.height - dateFont.height) - 10;
+			render.begin(x, y, dateWidth, dateFont.height);
+				render.fillRectangle(blue, 0, 0, render.width, render.height);
+				render.drawText(date, dateFont, white, x, y);
+			render.end();
+		}, 500);
+	}
+}
+
+render.begin();
+	render.fillRectangle(blue, 0, 0, render.width, render.height);
+render.end();
+
+let client = new TimeSyncClient;
+client.start();
