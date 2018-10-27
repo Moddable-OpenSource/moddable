db588a4bcd462fb7569527079a6cb838681c052c
Peter Hoddie <peter@moddable.tech>
2018-10-26 23:02:03 -0700

    get claimed name; report more state in http response

--- examples/network/mdns/httpserver/main.js
+++ examples/network/mdns/httpserver/main.js
@@ -1,20 +1,26 @@
 /*
 	This example claims the local name "example". It provides an HTTP server that can be
-	acccessed "http://example.local/" from a web browser on any computer or phone
+	acccessed at "http://example.local/" from a web browser on any computer or mobile device
  	that supports mDNS.
 
  	If the name "example" is already claimed on the current network, a unique name with be
- 	found by adding a "-2" or "-3", etc. to the end of the requested name.
+ 	found by appending "-2" or "-3", etc. to the requested name.
 
- 	The name selected is output to the console. The MDNS class supports an optional
+ 	The name selected is output to the console. The MDNS constructor provides an optional
 	callback to report the selected name to the host application.
 
 */
 
 import MDNS from "mdns";
 import {Server} from "http"
+import Net from "net"
 
-new MDNS({hostName: "example"});
+let hostName = "example";		// the hostName to claim. updated by callback to actual name claimed.
+
+new MDNS({hostName}, function(message, value) {
+	if (1 === message)
+		hostName = value;
+});
 
 let counter = 0;
 let server = new Server({port: 80});
@@ -22,7 +28,10 @@ server.callback = function(message, value) {
 	if (2 == message)
 		this.path = value;
 
-	if (8 == message)
-		return {headers: ["Content-type", "text/plain"],
-				body: `Hello, client requesting path ${this.path}.\nThis is request #${++counter} to this server.`};
+	if (8 == message) {
+		let body =	`Client requested path ${this.path}.\n` +
+					`Request #${++counter} to this server.\n` +
+					`Server host name "${hostName}.local" at address ${Net.get("IP")} on network "${Net.get("SSID")}".\n`;
+		return {headers: ["Content-Type", "text/plain"], body};
+	}
 }
