cff029a265ff6c5dde00b4daebb10191c7db452b
Peter Hoddie <peter@moddable.tech>
2018-10-28 07:47:31 -0700

    mDNS NTP client & service examples

---
+++ examples/network/mdns/ntpservice/main.js
@@ -0,0 +1,61 @@
+/*
+	This example implements a local time service on the local network. This allows devices on
+	the local network to retrieve the current time without having to access the internet
+	and without having to hardcode a list of ntp servers.
+
+	The DNS-SD time service is advertised as time _ntp._udp, the same as the ntpd in Linux.
+ 	Additionally, the service puts the current UTC tiem as a string in the TXT record.
+	This is updaed once a minute. The value is for debugging purposes; use the NTP
+	service itself to retrieve the current time.
+
+	The NTP server itself is trivial, it only fills in the header byte and current time.
+
+	The ntpclient example uses the _ntp._udp service to retrieve the current time.
+*/
+
+import Timer from "timer";
+import MDNS from "mdns";
+import {Socket} from "socket";
+
+function timeInMinutes() {
+	let now = new Date;
+	now.setUTCSeconds(0);
+	now.setUTCMilliseconds(0);
+	return now.valueOf();
+}
+
+function millisecondsToNextMinute() {
+	let now = new Date;
+	return (60 * 1000) - ((now.getUTCSeconds() * 1000) + now.getUTCMilliseconds());
+}
+
+class SNTP extends Socket {
+	constructor() {
+		super({kind: "UDP", port: 123});
+	}
+	callback(message, value, address, port) {
+		if (2 !== message)
+			return;
+
+		const packet = new DataView(new ArrayBuffer(48));
+		packet.setUint8(0, 0x24);
+		packet.setUint32(40, ((Date.now() / 1000) | 0) + 2208988800);
+		this.write(address, port, packet.buffer);
+	}
+}
+
+const timeService = {name: "ntp", protocol: "udp", port: 123, txt: {}};
+
+const mdns = new MDNS({hostName: "time"}, function(message, value) {
+	if ((1 == message) && value) {
+		timeService.txt.now = timeInMinutes();
+		this.add(timeService);
+
+		Timer.set(function() {
+			timeService.txt.now = timeInMinutes();
+			mdns.update(timeService);
+		}, millisecondsToNextMinute(), 60 * 1000);
+	}
+});
+
+new SNTP;
