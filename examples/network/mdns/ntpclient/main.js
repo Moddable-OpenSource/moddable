7fd6283c7b5dd6ccbea43513309f4e37c7e93a5d
Michael Kellner <mkellner@moddable.tech>
2018-10-29 16:22:20 -0700

    boilerplate

--- examples/network/mdns/ntpclient/main.js
+++ examples/network/mdns/ntpclient/main.js
@@ -1,4 +1,17 @@
 /*
+ * Copyright (c) 2018  Moddable Tech, Inc. 
+ *
+ *   This file is part of the Moddable SDK.
+ *
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+/*
 	This example uses a local time service to retrieve the current time.
 
 	It searches for a DNS-SD _ntp._udp time service. When found, it makes
