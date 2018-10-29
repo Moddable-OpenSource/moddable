cb513532d272b4fe4e2337f53b88122bcc1b150c
Michael Kellner <mkellner@moddable.tech>
2018-10-29 16:22:20 -0700

    boilerplate

--- examples/network/mdns/ntpservice/main.js
+++ examples/network/mdns/ntpservice/main.js
@@ -1,4 +1,17 @@
 /*
+ * Copyright (c) 2018  Moddable Tech, Inc. 
+ *
+ *   This file is part of the Moddable SDK.
+ *
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+/*
 	This example implements a local time service on the local network. This allows devices on
 	the local network to retrieve the current time without having to access the internet
 	and without having to hardcode a list of ntp servers.
