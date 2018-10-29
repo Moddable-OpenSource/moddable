4c5b9b4d8f634c0d8208e70a91fd0dbf570f3a28
Michael Kellner <mkellner@moddable.tech>
2018-10-29 16:22:20 -0700

    boilerplate

--- examples/network/mdns/httpserver/main.js
+++ examples/network/mdns/httpserver/main.js
@@ -1,4 +1,17 @@
 /*
+ * Copyright (c) 2018  Moddable Tech, Inc. 
+ *
+ *   This file is part of the Moddable SDK.
+ *
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+/*
 	This example claims the local name "example". It provides an HTTP server that can be
 	acccessed at "http://example.local/" from a web browser on any computer or mobile device
  	that supports mDNS.
