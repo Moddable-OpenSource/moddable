e4f75df1b032c40cb82dfdac0fbc908dc567620c
Patrick Soquet <ps@moddable.tech>
2018-11-20 11:46:29 +0100

    XS 8.6.1 for eshost/jsvu - REBUILD ALL

--- xs/sources/xsCommon.h
+++ xs/sources/xsCommon.h
@@ -90,7 +90,7 @@ typedef struct {
 #define XS_ATOM_VERSION 0x56455253 /* 'VERS' */
 #define XS_MAJOR_VERSION 8
 #define XS_MINOR_VERSION 6
-#define XS_PATCH_VERSION 0
+#define XS_PATCH_VERSION 1
 
 #define XS_DIGEST_SIZE 16
 #define XS_VERSION_SIZE 4
