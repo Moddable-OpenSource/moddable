fbe20acaa0f18ae03ab42965ef95497db4c467dd
Lizzie Prader <lizzie@moddable.tech>
2019-02-07 11:20:51 -0800

    update weather example

--- examples/piu/weather/main.js
+++ examples/piu/weather/main.js
@@ -15,6 +15,7 @@
 import {} from "piu/MC";
 import { Request } from "http";
 import Timeline from "piu/Timeline";
+import Timer from "timer";
 
 const APPID = "94de4cda19a2ba07d3fa6450eb80f091";
 const country = "us";
@@ -30,140 +31,111 @@ const TORNADO = 6;
 const DARKCLOUD = 7;
 
 const WHITE = "#E7DFDD";
-const BLACK = "#3b3a36";
-const ORANGE = [21, 0.36];	//"#AD7252";
-const GREEN = [101, 0.39]; 	//"#6DB14E";
-const BLUE = [183, 0.36];	//"#52A9AD";
+const BLACK = "#202020";
 
 const blackSkin = new Skin({ fill:BLACK });
 const iconTexture = new Texture("icons-alpha.png");
-const iconSkin = new Skin({ texture:iconTexture, x: 0, y: 0, width: 150, height: 150, variants: 150, color: WHITE, aspect:"fit" });
+const iconSkin = new Skin({ 
+	texture: iconTexture, 
+	color: [BLACK, WHITE], 
+	x: 0, y: 0, width: 150, height: 150, variants: 150, states: 0,
+});
 
 const OpenSans52 = new Style({ font: "normal normal normal 52px Open Sans" });
-const OpenSans20 = new Style({ font: "semibold 20px Open Sans", color: "#6DB14E" });
-const OpenSans28 = new Style({ font: "semibold 28px Open Sans" });
+const OpenSans28 = new Style({ font: "semibold 28px Open Sans", color: WHITE });
 
 function titleCase(str) {
 	return str.split(' ').map(i => i[0].toUpperCase() + i.substring(1)).join(' ')
 }
 
+class AnimatedTextBehavior extends Behavior {
+	onCreate(port, data) {
+		this.string = data.string;
+		this.style = data.style;
+		this.color = data.color;
+		this.hue = data.hue;
+	}
+	onDisplaying(port) {
+		this.labelSize = port.measureString(this.string, this.style);
+		this.leftMargin = (port.width - this.labelSize.width)/2;
+		port.interval = 50;
+		port.start();
+	}
+	onDraw(port, x, y, w, h) {
+		let color = hsl(this.hue, 1, 0.75);
+		port.drawString(this.string, this.style, color, this.leftMargin, 0, this.labelSize.width, port.height);
+		this.hue++;
+	}
+	onTimeChanged(port) {
+		port.invalidate();
+	}
+};
+
 const AnimatedText = Port.template($ => ({
-	left: 240, width:240,
-	Behavior: class extends Behavior {
-		onCreate(port, data) {
-			this.string = data.string;
-			this.style = data.style;
-			this.color = data.color;
-			this.l = 0.5;
-			this.direction = -1;
-		}
-		onDisplaying(port) {
-			this.labelSize = port.measureString(this.string, this.style);
-			this.leftMargin = (port.width - this.labelSize.width)/2;
-		}
-		onStartFlash(port) {
-			this.direction = 0;
-			port.interval = 75;
-			port.start();
-		}
-		onStopFlash(port) {
-			port.stop();
-			this.direction = -1;
-		}
-		onDraw(port) {
-			let color = hsl(this.color[0], this.color[1], this.l);
-			port.drawString(this.string, this.style, color, this.leftMargin, 0, this.labelSize.width, port.height);
-			if ((this.direction == 0) && (this.l > 0.6)){
-				this.direction = 1;
-			} else if ((this.direction == 1) && (this.l < 0.5)) {
-				this.onStopFlash(port);
-			}
-			if (this.direction == 0) this.l += 0.03;
-			else if (this.direction == 1) this.l -= 0.03;
-		}
-		onTimeChanged(port) {
-			if (this.direction > -1) port.invalidate();
-		}
-	}
+	width:240,
+	Behavior: AnimatedTextBehavior
 }));
 
-let MainCol = Column.template($ => ({
-	left:0, top:0, right: 0, bottom: 0, 
-	skin: blackSkin,
+class MainColBehavior extends Behavior {
+	onCreate(content, data) {
+		this.data = data;
+		this.transitioningIn = 1;
+	}
+	onDisplaying(column) {
+		this.timeline = (new Timeline)
+			.to(column.content("city"), { x:0 }, 750, Math.backEaseOut, 0)
+			.to(column.content("temp"), { x:0 }, 750, Math.backEaseOut, -500)
+			.from(column.content("condition"), { y: 320 }, 400, Math.quadEaseOut, -750)
+			.to(column.content("icon"), { state: 1 }, 500, Math.quadEaseOut, -250)
+		column.duration = this.timeline.duration;
+		column.time = 0;
+		this.timeline.seekTo(0);
+		column.start();
+	}
+	onTimeChanged(column) {
+		this.timeline.seekTo(column.time);
+	}
+	onFinished(column) {
+		if (this.transitioningIn) {
+			this.transitioningIn = false;
+			column.distribute("onStartFlash");	
+		} else {
+			application.defer("onAddNextScreen");
+		}
+	}
+	onTransitionOut(column) {
+		this.timeline = (new Timeline)
+			.to(column.content("icon"), { state: 0 }, 400, Math.quadEaseOut, 0)
+			.to(column.content("city"), { x:-240 }, 400, Math.backEaseOut, -200)
+			.to(column.content("condition"), { y:320 }, 400, Math.quadEaseOut, -400)
+			.to(column.content("temp"), { x:240 }, 400, Math.backEaseOut, -280)
+		column.duration = this.timeline.duration;
+		column.time = 0;
+		this.timeline.seekTo(0);
+		column.start();
+	}
+}
+
+const MainCol = Column.template($ => ({
+	left:0, top:0, right: 0, bottom: 0, skin: blackSkin,
 	contents:[
-		new AnimatedText({ string: $.city, style: OpenSans28, color: GREEN }, { name: "city", top: 18, height: 38, }),
-		new AnimatedText({ string: $.temp, style: OpenSans52, color: BLUE }, { name: "temp", top: -4, height: 58, }),
-		Content($, { name: "icon", left:285, width: 150, top: 10, height:150, skin: iconSkin, variant: $.icon }),
-		new AnimatedText({ string: $.condition, style: OpenSans20, color: ORANGE }, { name: "condition", top: 3, height: 26, }),	
+		new AnimatedText({ hue: 0, string: $.city, style: OpenSans28, }, { name: "city", top: 15, height: 38, left: 240,  }),
+		new AnimatedText({ hue: 90, string: $.temp, style: OpenSans52, }, { name: "temp", top: -4, height: 58, left: -240 }),
+		Content($, { name: "icon", width: 150, top: 10, height:150, skin: iconSkin, variant: $.icon }),
+		new AnimatedText({ hue: 270, string: $.condition, style: OpenSans28, }, { name: "condition", top: 3, height: 38, }),	
 	],
-	Behavior: class extends Behavior {
-		onCreate(content, data) {
-			this.data = data;
-			this.transitioningIn = 1;
-		}
-		onDisplaying(column) {
-			this.timeline = (new Timeline)
-				.to(column.content("city"), { x:0 }, 500, Math.quadEaseOut, 0)
-				.to(column.content("temp"), { x:0 }, 500, Math.quadEaseOut, -450)
-				.to(column.content("icon"), { x:45 }, 500, Math.quadEaseOut, -450)
-				.to(column.content("condition"), { x:0 }, 500, Math.quadEaseOut, -450)
-			column.duration = this.timeline.duration;
-			column.time = 0;
-			column.start();
-		}
-		onTimeChanged(column) {
-			this.timeline.seekTo(column.time);
-		}
-		onFinished(column) {
-			if (this.transitioningIn) {
-				this.transitioningIn = false;
-				column.distribute("onStartFlash");	
-			} else {
-				application.defer("onAddNextScreen");
-			}
-		}
-		onTransitionOut(column) {
-			this.timeline = (new Timeline)
-				.to(column.content("city"), { x:-240 }, 400, Math.quadEaseIn, 0)
-				.to(column.content("temp"), { x:-240 }, 400, Math.quadEaseIn, -280)
-				.to(column.content("icon"), { x:-285 }, 400, Math.quadEaseIn, -280)
-				.to(column.content("condition"), { x:-240 }, 400, Math.quadEaseIn, -280)
-			column.duration = this.timeline.duration;
-			column.time = 0;
-			column.start();
-		}
-	}
+	Behavior: MainColBehavior
 }));
 
-const WeatherApp = Application.template($ => ({
-	contents: [
-		Label($, {
-			left: 0, right: 0, top: 0, bottom: 0, 
-			skin: blackSkin, style: OpenSans20, string: "Loading...",
-			Behavior: class extends Behavior {
-				onTransitionOut(label) {
-					application.defer("onAddNextScreen");
-				}
-			}
-		}),
-	],
-	Behavior: class extends Behavior {
+class WeatherAppBehavior extends Behavior {
 	  	onCreate(application, data) {
 	  		this.data = data;
-			this.zipIndex = 0;
-			let zip = zips[this.zipIndex];
-			this.getWeatherData(application, zip); 
-			application.interval = 8000;
-			application.start();
 	  	}
 		onDisplaying(application) {
 			if (application.height != 320 || application.width != 240)
 				trace("WARNING: This application was designed to run on a 240x320 screen.\n");
-		}
-		onTimeChanged(application) {
-			if (++this.zipIndex >= zips.length) this.zipIndex = 0;
-			let zip = zips[this.zipIndex];
-			this.getWeatherData(application, zip);     
+			this.zipIndex = 0;
+			this.getWeatherData(application, zips[this.zipIndex]); 
 		}
 		getWeatherData(application, zip) {
 			let request = new Request({
@@ -206,18 +178,41 @@ const WeatherApp = Application.template($ => ({
 							break;
 					}
 					this.data.city = value.name,
-					this.data.temp = value.main.temp + " F",
+					this.data.temp = Math.round(value.main.temp) + " F",
 					this.data.condition = titleCase(value.weather[0].main),
 					this.data.icon = toDraw
 					application.first.delegate("onTransitionOut");
+					Timer.set(() => {
+						this.getNext(application);
+					}, 5000);
 				}
 			}
+		}
+		getNext(application) {
+			if (++this.zipIndex >= zips.length) this.zipIndex = 0;
+			let zip = zips[this.zipIndex];
+			this.getWeatherData(application, zip);     
 		}
 		onAddNextScreen(application) {
 			application.empty();
 			application.add(new MainCol(this.data));
 		}
-	},
+}
+
+const WeatherApp = Application.template($ => ({
+	left: 0, right: 0, top: 0, bottom: 0, skin: blackSkin, 
+	contents: [
+		Label($, {
+			left: 0, right: 0, top: 0, bottom: 0, 
+			skin: blackSkin, style: OpenSans28, string: "Loading...",
+			Behavior: class extends Behavior {
+				onTransitionOut(label) {
+					application.defer("onAddNextScreen");
+				}
+			}
+		}),
+	],
+	Behavior: WeatherAppBehavior
 }));
 
 export default new WeatherApp({});
