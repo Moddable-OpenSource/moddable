2ae1e061caf03baf36f1d4432485f5a11768b419
Michael Kellner <mkellner@moddable.tech>
2019-03-25 04:36:37 -0700

    boilerplate, cleanup

--- build/devices/qca4020/targets/rigel/setup-target.js
+++
@@ -1,9 +0,0 @@
-import config from "mc/config";
-import Digital from "pins/digital";
-
-export default function (done) {
-	if (config.autobacklight)
-		Digital.write(config.backlight_pin, 1);
-
-	done();
-}
