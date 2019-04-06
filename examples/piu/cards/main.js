13fe7ecf2070bb93802e15f1f3c0af5527cfb951
Andy Carle <andy@moddable.tech>
2019-04-05 17:43:41 -0700

    Update the cards example to be responsive/rotatable.

--- examples/piu/cards/main.js
+++ examples/piu/cards/main.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017  Moddable Tech, Inc.
+ * Copyright (c) 2016-2019  Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK.
  * 
@@ -19,37 +19,37 @@ const team = [
 	{
 		name: "Andy Carle",
 		title: "Product & Prototype Engineer",
-		address: "andy@moddable.tech",
+		address: "andy@moddable.com",
 	},
 	{
 		name: "Brian Friedkin",
 		title: "Principal Engineer",
-		address: "brian@moddable.tech",
+		address: "brian@moddable.com",
 	},
 	{
 		name: "Chris Krueger",
 		title: "Creative Director",
-		address: "chris@moddable.tech",
+		address: "chris@moddable.com",
 	},
 	{
 		name: "Lizzie Prader",
 		title: "Software Engineer",
-		address: "lizzie@moddable.tech",
+		address: "lizzie@moddable.com",
 	},
 	{
 		name: "Michael Kellner",
 		title: "CFO & Embedded Engineer",
-		address: "mkellner@moddable.tech",
+		address: "mkellner@moddable.com",
 	},
 	{
 		name: "Peter Hoddie",
 		title: "Principal",
-		address: "peter@moddable.tech",
+		address: "peter@moddable.com",
 	},
 	{
 		name: "Patrick Soquet",
 		title: "Software Architect",
-		address: "ps@moddable.tech",
+		address: "ps@moddable.com",
 	},
 ];
 const WHITE = "white";
@@ -64,80 +64,97 @@ const labelStyle = new Style({ font:"semibold 16px Open Sans", color:WHITE, hori
 
 let CardApplication = Application.template($ => ({
 	skin:whiteSkin,
-	Behavior: class extends Behavior {
-		onCreate(application, anchors) {
-			this.reverse = false;
-
-			this.anchors = anchors;
-			let index = this.index = 0;
-			let person = team[index];
-			for (let i in person)
-				anchors[i].string = person[i];
-				
-			this.timeline = (new Timeline)
-				.to(anchors.background.behavior, { x:0 }, 500, Math.quadEaseOut, 500)
-				.to(anchors.logo, { x:20, y:15, state:1 }, 500, Math.quadEaseOut, -500)
-				.to(anchors.company, { x:40, y:15 }, 500, Math.quadEaseOut, -500)
-				.to(anchors.name, { x:0 }, 250, Math.quadEaseOut, -125)
-				.to(anchors.title, { x:0 }, 250, Math.quadEaseOut, -125)
-				.to(anchors.address, { x:0 }, 250, Math.quadEaseOut)
-			application.duration = this.timeline.duration + 500;
-			application.start();	
-		}
-		onDisplaying(application) {
-			if (application.height != 320 || application.width != 240)
-				trace("WARNING: This application was designed to run on a 240x320 screen.\n");
-		}
-		onFinished(application) {
-			this.reverse = !this.reverse;
-			if (!this.reverse) {
-				this.index++;
-				if (this.index >= team.length)
-					this.index = 0;
-				let person = team[this.index];
-				let anchors = this.anchors;
-				for (let i in person)
-					anchors[i].string = person[i];
-			}
-			application.time = 0;
-			application.start();
-		}
-		onTimeChanged(application) {
-			let time = application.time;
-			if (this.reverse)
-				time = application.duration - time;
-			this.timeline.seekTo(time);
-		}
-	},
 	contents: [
-		Die($, {
-			anchor:"background", left:0, right:0, top:0, bottom:0, 
+		Layout($, {
+			left: 0, right: 0, top: 0, bottom: 0,
 			Behavior: class extends Behavior {
-				onCreate(die) {
-					this.die = die;
-					this.left = 240;
+				onCreate(layout, anchors){
+					this.reverse = false;
+					this.anchors = anchors;
+					let index = this.index = 0;
+					let person = team[index];
+					for (let i in person)
+						anchors[i].string = person[i];
 				}
-				get x() {
-					return this.left;
+				onDisplaying(layout) {
+					this.resetTimeline(layout);
+					layout.start();
 				}
-				set x(it) {
-					it = this.left = Math.round(it);
-					this.die.set(it, 0, 240 - it, 320);
-					this.die.cut();
+				onFinished(layout) {
+					this.reverse = !this.reverse;
+					if (!this.reverse) {
+						this.index++;
+						if (this.index >= team.length)
+							this.index = 0;
+						let person = team[this.index];
+						let anchors = this.anchors;
+						for (let i in person)
+							anchors[i].string = person[i];
+					}
+					layout.time = 0;
+					layout.start();
+
+				}
+				onTimeChanged(layout) {
+					let time = layout.time;
+					if (this.reverse)
+						time = layout.duration - time;
+					this.timeline.seekTo(time);
+				}
+				resetTimeline(layout) {
+					let anchors = this.anchors;
+					this.timeline = (new Timeline)
+						.on(anchors.background.behavior, { x: [layout.width, 0] }, 500, Math.quadEaseOut, 500)
+						.on(anchors.logo, { x: [(layout.width - 60) / 2, 20], y: [(layout.height - 60) / 2, 15], state: [0, 1] }, 500, Math.quadEaseOut, -500)
+						.on(anchors.company, { x: [layout.width, 40] }, 500, Math.quadEaseOut, -500)
+						.on(anchors.name, { x: [layout.width, 0] }, 250, Math.quadEaseOut, -125)
+						.on(anchors.title, { x: [layout.width, 0] }, 250, Math.quadEaseOut, -125)
+						.on(anchors.address, { x: [layout.width, 0] }, 250, Math.quadEaseOut)
+
+					layout.duration = this.timeline.duration + 500;
+				}
+				onFitHorizontally(layout, width) {
+					this.anchors.name.width = this.anchors.title.width = this.anchors.address.width = this.anchors.background.width = width;
+					this.resetTimeline(layout);
+					return width;
+				}
+				onFitVertically(layout, height) {
+					this.anchors.background.height = height;
+					this.resetTimeline(layout);
+					return height;
 				}
 			},
-			contents: [
-				Content($, { left:0, right:0, top:0, bottom:0, skin:blueSkin, state:0 } ),
-			],
-		}),
-		Content($, { anchor:"logo", left:90, top:130, skin:logoSkin, state:0 } ),
-		Content($, { anchor:"company", left:240, top:15, skin:companySkin, state:1 } ),
-		Label($, { anchor:"name", left:240, width:240, top:80, height:20, style:labelStyle, state:0 } ),
-		Text($, { anchor:"title", left:240, width:240, top:102, height:45, style:labelStyle, state:0 } ),
-		Label($, { anchor:"address", left:240, width:240, top:280, height:20, style:labelStyle, state:0 } ),
+			contents:[
+				Die($, {
+					anchor: "background", left: 0, top: 0, width: 0, height: 0,
+					Behavior: class extends Behavior {
+						onCreate(die) {
+							this.die = die;
+							this.left = 0;
+						}
+						get x() {
+							return this.left;
+						}
+						set x(it) {
+							it = this.left = Math.round(it);
+							this.die.set(it, 0, application.width - it, application.height);
+							this.die.cut();
+						}
+					},
+					contents: [
+						Content($, { left: 0, right: 0, top: 0, bottom: 0, skin: blueSkin, state: 0 }),
+					],
+				}),
+				Content($, { anchor: "logo", left: 20, top: 15, skin: logoSkin, state: 1 }),
+				Content($, { anchor: "company", left: 40, top: 15, skin: companySkin, state: 1 }),
+				Label($, { anchor: "name", left: 0, width: 0, top: 80, height: 20, style: labelStyle, state: 0 }),
+				Text($, { anchor: "title", left: 0, width: 0, top: 102, height: 45, style: labelStyle, state: 0 }),
+				Label($, { anchor: "address", left: 0, width: 0, bottom: 20, height: 20, style: labelStyle, state: 0 }),
+			]
+		})
 	]
 }));
 
 export default function () {
-	new CardApplication({}, { displayListLength:1024, touchCount:0 });
+	new CardApplication({}, { displayListLength:2048, touchCount:0 });
 }
