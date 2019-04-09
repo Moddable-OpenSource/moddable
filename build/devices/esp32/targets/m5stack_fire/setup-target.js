d2bd27b9d695e7965c780e2881012bb022658a66
Andy Carle <andy@moddable.tech>
2019-04-08 23:54:58 -0700

    Add support for the M5Stack Fire's built-in MPU-6050 to the m5stack_fire target's host. Includes support for global accelerometer and gyro objects in addition to config:autorotate for Piu apps on the m5stack fire.

--- build/devices/esp32/targets/m5stack_fire/setup-target.js
+++ build/devices/esp32/targets/m5stack_fire/setup-target.js
@@ -6,6 +6,16 @@ import Monitor from "monitor";
 import AudioOut from "pins/i2s";
 import Resource from "Resource";
 
+import Timer from "timer";
+import MPU6050 from "mpu6050";
+
+import config from "mc/config";
+
+const state = {
+	handleRotation: nop
+};
+
+
 export default function (done) {
 	global.lights = new NeoPixel({});
 
@@ -23,6 +33,77 @@ export default function (done) {
 	speaker.start();
 
 	//@@ microphone
+
+	state.accelerometerGyro = new MPU6050;
+
+	global.accelerometer = {
+		onreading: nop
+	}
+
+	global.gyro = {
+		onreading: nop
+	}
+
+	accelerometer.start = function(frequency){
+		accelerometer.stop();
+		state.accelerometerTimerID = Timer.repeat(id => {
+			state.accelerometerGyro.configure({ operation: "accelerometer" });
+			const sample = state.accelerometerGyro.sample();
+			if (sample) {
+				sample.y *= -1;
+				sample.z *= -1;
+				state.handleRotation(sample);
+				accelerometer.onreading(sample);
+			}
+		}, frequency);
+	}
+
+	gyro.start = function(frequency){
+		gyro.stop();
+		state.gyroTimerID = Timer.repeat(id => {
+			state.accelerometerGyro.configure({ operation: "gyroscope" });
+			const sample = state.accelerometerGyro.sample();
+			if (sample) {
+				let {x, y, z} = sample;
+				const temp = x;
+				x = y * -1;
+				y = temp * -1;
+				z *= -1;
+				gyro.onreading({ x, y, z });
+			}
+		}, frequency);
+	}
+
+	accelerometer.stop = function(){
+		if (undefined !== state.accelerometerTimerID)
+			Timer.clear(state.accelerometerTimerID);
+		delete state.accelerometerTimerID;
+	}
+
+	gyro.stop = function () {
+		if (undefined !== state.gyroTimerID)
+			Timer.clear(state.gyroTimerID);
+		delete state.gyroTimerID;
+	}
+	
+	if (config.autorotate && global.Application){
+		state.handleRotation = function (reading) {
+			if (Math.abs(reading.y) > Math.abs(reading.x)) {
+				if (reading.y < -0.7 && application.rotation != 90) {
+					application.rotation = 90;
+				} else if (reading.y > 0.7 && application.rotation != 270) {
+					application.rotation = 270;
+				}
+			} else {
+				if (reading.x < -0.7 && application.rotation != 180) {
+					application.rotation = 180;
+				} else if (reading.x > 0.7 && application.rotation != 0) {
+					application.rotation = 0;
+				}
+			}
+		}
+		accelerometer.start(300);
+	}
 
 	done();
 }
