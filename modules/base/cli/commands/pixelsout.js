dee8f8f0f3294418901790cf7569f8faf45e4eed
Peter Hoddie <peter@moddable.tech>
2019-04-21 17:24:19 -0700

    reorganize cli/pixelsout to build for targets without display driver; telnet example builds on non-embedded targets

--- modules/base/cli/commands/pixelsout.js
+++ modules/base/cli/commands/pixelsout.js
@@ -1,65 +1,70 @@
 /*
- *     Copyright (C) 2016-2017 Moddable Tech, Inc.
+ *     Copyright (C) 2016-2019 Moddable Tech, Inc.
  *     All rights reserved.
  */
 
 import CLI from "cli";
 
 CLI.install(function(command, parts) {
-	if ("pixelsout" !== command)
+	if ("help" === command) {
+		this.line("screen - print information");
+		this.line("screen rotate degrees - rotate display");
+		this.line("screen fill r g b [x y width height] - uses specified color to fill full screen or specified area");
+		return;
+	}
+
+	if ("screen" !== command)
 		return false;
 
-	if (!parts.length)
-		throw new Error("missing open/close/fill");
+	if (!global.screen) {
+		this.line("no screen found");
+		return true;
+	}
+
+	if (!parts.length) {
+		this.line(`width ${screen.width}`);
+		this.line(`height ${screen.height}`);
+		this.line(`pixelFormat ${screen.pixelFormat}`);
+		this.line(`async ${screen.async ? true : false}`);
+		if (undefined === screen.rotation)
+			this.line(`rotation unsupported`);
+		else
+			this.line(`rotation ${screen.rotation}`);
+		return true;
+	}
 
 	switch (parts.shift().toLowerCase()) {
-		case "open":
-			if (this.pixelsout)
-				throw new Error("already open");
-			let module = require(parts[0]);
-			this.pixelsout = new module({});
-			this.line(`width ${this.pixelsout.width}, height ${this.pixelsout.height}`);
-			break;
-
-		case "close":
-			if (!this.pixelsout)
-				throw new Error("none open");
-//			this.pixelsout.close();
-			delete this.pixelsout;
+		case "rotate":
+			if (undefined === screen.rotation)
+				this.line(`rotation unsupported`);
+			else
+				screen.rotation = parseInt(parts[0]);
 			break;
 
 		case "fill":
-			if (!this.pixelsout)
-				throw new Error("none open");
 			let r = parseInt(parts[0]), g = parseInt(parts[1]), b = parseInt(parts[2])
 			let x = 0, y = 0;
-			let width = this.pixelsout.width;
-			let height = this.pixelsout.height;
+			let width = screen.width;
+			let height = screen.height;
 			if (parts.length >= 7) {
 				x = parseInt(parts[3]);
 				y = parseInt(parts[4]);
 				width = parseInt(parts[5]);
 				height = parseInt(parts[6]);
-//@@			pixelsOut.adaptInvalid()
+//@@			screen.adaptInvalid()
 			}
 
-			let buffer = new ArrayBuffer(this.pixelsout.pixelsToBytes(width));
+			let buffer = new ArrayBuffer(screen.pixelsToBytes(width));
 			let pixels = new Uint16Array(buffer);	//@@ consider pixelFormat to make color and array type
 			pixels.fill(((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3));
-			this.pixelsout.begin(x, y, width, height);
+			screen.begin(x, y, width, height);
 			for (let i = height; i > 0; i--)
-				this.pixelsout.send(buffer);
-			this.pixelsout.end();
-			break;
-
-		case "help":
-			this.line("pixelsout open name - opens PixelsOut module of 'name' and displays dimensions");
-			this.line("pixelsout close - closes active PixelsOut");
-			this.line("pixelsout fill r g b [x y width height] - uses specified color to fill full screen or specified area");
+				screen.send(buffer);
+			screen.end();
 			break;
 
 		default:
-			this.line("unknown pixelsout option");
+			this.line("unknown screen option");
 			break;
 
 	}
