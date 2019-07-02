04b45a9ad03fb14c3b0f3d4f91402674332cc017
Brian Friedkin <brian@moddable.tech>
2019-07-02 10:30:11 -0700

    expanding keyboard

---
+++ examples/piu/vertical-expanding-keyboard/main.js
@@ -0,0 +1,108 @@
+/*
+ * Copyright (c) 2016-2019  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ * 
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+
+import {} from "piu/MC";
+import {VerticalExpandingKeyboard} from "keyboard";
+import {KeyboardField} from "common/keyboard";
+
+const PASSWORDMODE = false;	//Set to true to replace input with asterisks, false for clear text. 
+const TRANSITION = true;	//Set to true to transition keyboard in and out. 
+
+const HeaderSkin = Skin.template({fill:"#0082ff"});
+const FieldSkin = Skin.template({fill:"white"});
+const BackgroundSkin = Skin.template({ fill:"white" });
+
+const KeyboardStyle = Style.template({ font:"18px Roboto", color:"black"  });
+const FieldStyle = Style.template({ font:"20px Open Sans", color: "black", horizontal:"left", vertical:"middle" });
+const HeaderStyle = Style.template({ font:"20px Open Sans", color: "white", horizontal:"left", vertical:"middle" });
+
+const ModdableLogoTexture = Texture.template({ path:"moddable-logo-mask.png" });
+const ModdableLogoSkin = Skin.template({ Texture:ModdableLogoTexture, x:0, y:0, width:88, height:34, color:"white" });
+
+const Header = Container.template($ => ({
+	contents: [
+		Content($, { left:76, top:0, bottom:0, Skin:ModdableLogoSkin }),
+	]
+}));
+
+const KeyboardContainer = Column.template($ => ({
+	left:0, right:0, top:0, bottom:0, active:true,
+	contents:[
+		Header($, {
+			anchor: "HEADER", left:0, right:0, height:40, 
+			Style:HeaderStyle, Skin:HeaderSkin
+		}),
+		KeyboardField($, {
+			anchor:"FIELD", password:PASSWORDMODE, left:32, right:0, top:30, bottom:30,
+			Skin:FieldSkin, Style:FieldStyle, visible:false
+		}),
+		Container($, {
+			anchor: "KEYBOARD", left:0, right:0, bottom:0, height:185, 
+			Skin:BackgroundSkin
+		}),
+	],
+	Behavior: class extends Behavior {
+		onCreate(column, data){
+			this.data = data;
+			this.addKeyboard();
+		}
+		onTouchEnded(column){
+			if (1 != this.data["KEYBOARD"].length)
+				this.addKeyboard();
+		}
+		addKeyboard() {
+			this.data["KEYBOARD"].add(VerticalExpandingKeyboard(this.data, {
+				style:new KeyboardStyle(), target:this.data["FIELD"], doTransition:TRANSITION
+			}));
+		}
+	}
+}));
+
+class KeyboardAppBehavior extends Behavior {
+	onCreate(application, data) {
+		this.data = data;
+	}
+	onKeyboardRowsContracted(application) {
+		// keyboard rows contracted back to 1x view
+	}
+	onKeyboardRowsExpanded(application) {
+		// keyboard rows expanded
+	}
+	onKeyboardOK(application, string) {
+		trace(`String is: ${string}\n`);
+		this.data["FIELD"].visible = false;
+	}
+	onKeyboardTransitionFinished(application, out) {
+		if (out) {
+			let keyboard = this.data["KEYBOARD"];
+			keyboard.remove(keyboard.first);
+		}
+		else {
+			this.data["FIELD"].visible = true;
+		}
+	}
+}
+Object.freeze(KeyboardAppBehavior.prototype);
+
+const KeyboardApp = Application.template($ => ({
+	active:true, Skin:BackgroundSkin,
+	Behavior:KeyboardAppBehavior,
+	contents: [
+		KeyboardContainer($),
+	],
+}));
+
+export default function() {
+	new KeyboardApp({}, { commandListLength:2448, displayListLength:2600, touchCount:1 });
+}
---
+++ modules/input/expanding-keyboard/horizontal/keyboard.js
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2016-2019  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ * 
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import {} from "piu/MC";
+import Timeline from "piu/Timeline";
+import {BottomKeyRowBehavior, ExpandingKeyRowBehavior, ToggleModes, Keys, Colors, KeyRowBehavior, KeyboardBehavior} from "common/keyboard";
+
+const TOPMARGIN = 4;
+const KEYHEIGHT = 36;
+const KEYWIDTHS = [28, 30, 32, 34, 36, 38, 40];
+const KEYOFFSETS = [0, 31, 64, 99, 136, 175, 216];
+
+const RowTexture0 = Texture.template({ path:"horz-7pt-28w.png" });
+const RowTexture1 = Texture.template({ path:"horz-7pt-30w.png" });
+const RowTexture2 = Texture.template({ path:"horz-7pt-32w.png" });
+const RowTexture3 = Texture.template({ path:"horz-7pt-34w.png" });
+const RowTexture4 = Texture.template({ path:"horz-7pt-36w.png" });
+const RowTexture5 = Texture.template({ path:"horz-7pt-38w.png" });
+const RowTexture6 = Texture.template({ path:"horz-7pt-40w.png" });
+
+const BottomRowTexture = Texture.template({ path:"horz-bottom-row.png" });
+const GlyphsTexture = Texture.template({ path:"horz-glyph-strip.png" });
+
+class HorizontalBottomKeyRowBehavior extends BottomKeyRowBehavior {
+	onDraw(port) {
+		let keyboard = this.keyboard;
+		let keyWidth = keyboard.keyWidth;
+		let keyHeight = keyboard.keyHeight;
+		let toggleMode = keyboard.toggleMode;
+		let expanded = keyboard.expanded;
+		let texture = keyboard.keyBottomRowTexture;
+		let offsets = this.data.x;
+		let widths = this.data.widths;
+		let tracker = this.tracker;
+		let x, color;
+		
+		let glyphTexture = new GlyphsTexture;
+		
+		x = offsets[0];
+		color = (toggleMode == ToggleModes.SHIFT ? Colors.KEYTOGGLED : Colors.SPECIALKEY);
+		port.drawTexture(texture, color, x, 0, x, 0, widths[0], keyHeight);
+		port.drawTexture(glyphTexture, Colors.TEXTDOWN, x + 14, 8, 0, 0, 14, 20);
+
+		x = offsets[1];
+		color = (toggleMode == ToggleModes.ALT ? Colors.KEYTOGGLED : Colors.SPECIALKEY);
+		port.drawTexture(texture, color, x, 0, x, 0, widths[1], keyHeight);
+		port.drawTexture(glyphTexture, Colors.TEXTDOWN, x + (widths[1] >> 1) - 16, 8, 26, 0, 32, 20);
+
+		x = offsets[2];
+		color = (tracker && tracker.down ? Colors.KEYDOWN : Colors.KEY);
+		port.drawTexture(texture, color, x, 0, x, 0, widths[2], keyHeight);
+
+		x = offsets[3];
+		color = (tracker && tracker.down ? Colors.KEYDOWN : Colors.SPECIALKEY);
+		port.drawTexture(texture, color, x, 0, 0, 0, widths[0], keyHeight);
+		port.drawTexture(glyphTexture, Colors.TEXTDOWN, x + (widths[3] >> 1) - 7, 8, 14, 0, 13, 20);
+
+		x = offsets[4];
+		port.drawTexture(texture, color, x, 0, 0, 0, widths[0], keyHeight);
+		if (!expanded)
+			port.drawString(keyboard.ok, port.container.style, Colors.TEXTDOWN, x + ((widths[4] - keyboard.okMeasure.width) >> 1), ((keyHeight - keyboard.okMeasure.height) >> 1) - 1, widths[4], keyboard.okMeasure.height);
+		else
+			port.drawTexture(glyphTexture, Colors.TEXTDOWN, x + (widths[4] >> 1) - 17, 10, 58, 0, 32, 20);
+
+		if (tracker && !tracker.down) {
+			delete this.tracker;
+		}
+	}
+}
+Object.freeze(HorizontalBottomKeyRowBehavior.prototype);
+
+class HorizontalKeyboardBehavior extends KeyboardBehavior {
+	onCreate(container, $, data) {
+		super.onCreate(container, $, data);
+		this.keyRowTexture = [new RowTexture0, new RowTexture1, new RowTexture2, new RowTexture3, new RowTexture4, new RowTexture5, new RowTexture6];
+		this.keyBottomRowTexture = new BottomRowTexture;
+		this.keyHeight = KEYHEIGHT;
+		this.keyWidths = KEYWIDTHS;
+		this.keyOffsets = KEYOFFSETS;
+		this.innerKeyGap = 3;
+	}
+}
+Object.freeze(HorizontalKeyboardBehavior.prototype);
+
+const KeyRow = Port.template($ => ({
+	left:0, right:0, top:TOPMARGIN, height:KEYHEIGHT, active:true
+}));
+
+const ExpandingKeyRow = KeyRow.template($ => ({
+	Behavior:ExpandingKeyRowBehavior,
+}));
+
+const BottomKeyRow = KeyRow.template($ => ({
+	left:6, right:7, 
+	Behavior:HorizontalBottomKeyRowBehavior,
+}));
+
+export const HorizontalExpandingKeyboard = Column.template($ => ({
+	left:0, right:0, top:0, bottom:0, active:true,
+	Behavior:HorizontalKeyboardBehavior,
+	contents: [
+		ExpandingKeyRow(Keys[ToggleModes.NONE][0], { index:0, x:6 }),
+		ExpandingKeyRow(Keys[ToggleModes.NONE][1], { index:1, x:22 }),
+		ExpandingKeyRow(Keys[ToggleModes.NONE][2], { index:2, x:6 }),
+		BottomKeyRow(Keys[3][0], { index:3, x:Uint16Array.of(0, 43, 96, 224, 267), widths:Uint8Array.of(40, 50, 125, 40, 40) }),
+	]
+}));
