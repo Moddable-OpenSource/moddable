137b659349d1ed2705c61c082fb39af4f685b80c
Patrick Soquet <ps@moddable.tech>
2019-07-03 10:45:03 +0200

    XS: do not skip implemented proposals!

--- xs/tools/xst.c
+++ xs/tools/xst.c
@@ -651,13 +651,11 @@ void fxRunFile(txContext* context, char* path)
 		yaml_node_item_t* item = value->data.sequence.items.start;
 		while (item < value->data.sequence.items.top) {
 			yaml_node_t* node = yaml_document_get_node(document, *item);
-			if (!strcmp((char*)node->data.scalar.value, "hashbang")
-			||	!strcmp((char*)node->data.scalar.value, "numeric-separator-literal")
+			if (!strcmp((char*)node->data.scalar.value, "FinalizationGroup")
+			||	!strcmp((char*)node->data.scalar.value, "WeakRef")
 #ifndef mxRegExpUnicodePropertyEscapes
  			||	!strcmp((char*)node->data.scalar.value, "regexp-unicode-property-escapes")
 #endif
-			||	!strcmp((char*)node->data.scalar.value, "FinalizationGroup")
-			||	!strcmp((char*)node->data.scalar.value, "WeakRef")
 			) {
 				sloppy = 0;
 				strict = 0;
