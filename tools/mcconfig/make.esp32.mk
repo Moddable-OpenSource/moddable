8f80a071b806d43f6ab9f621d70e839b69d22c73
Brian Friedkin <brian@moddable.tech>
2019-07-24 17:23:03 -0700

    xsesp.h -> xsHost.h

--- tools/mcconfig/make.esp32.mk
+++ tools/mcconfig/make.esp32.mk
@@ -134,7 +134,7 @@ XS_DIRS = \
 	$(PLATFORM_DIR)/lib/pow
 XS_HEADERS = \
 	$(XS_DIR)/includes/xs.h \
-	$(XS_DIR)/includes/xsesp.h \
+	$(XS_DIR)/platforms/esp/xsHost.h \
 	$(XS_DIR)/includes/xsmc.h \
 	$(XS_DIR)/sources/xsAll.h \
 	$(XS_DIR)/sources/xsCommon.h \
