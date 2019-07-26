e1c1a83b121a2ffd8eb4814bdc2be02d02342bf2
Peter Hoddie <peter@moddable.tech>
2019-07-26 14:34:37 -0700

    notices

--- examples/io/udp/sntp/sntp.js
+++ examples/io/udp/sntp/sntp.js
@@ -1,4 +1,18 @@
-import UDP from "builtin/socket/udp";
+/*
+ * Copyright (c) 2019  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ *
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+
+ import UDP from "builtin/socket/udp";
 
 class SNTP extends UDP {
 	#onTime;
