203cd3a7d9d5e34630635b1e1644a222920f8007
Brian Friedkin <brian@moddable.tech>
2019-08-28 16:01:03 -0700

    fix modMilliseconds() overflow on windows

--- xs/sources/xsPlatforms.c
+++ xs/sources/xsPlatforms.c
@@ -500,7 +500,11 @@ uint32_t modMilliseconds()
 {
 	c_timeval tv;
 	c_gettimeofday(&tv, NULL);
+#if mxWindows
+	return (uint32_t)(uint64_t)(((double)(tv.tv_sec) * 1000.0) + ((double)(tv.tv_usec) / 1000.0));
+#else
 	return (uint32_t)(((double)(tv.tv_sec) * 1000.0) + ((double)(tv.tv_usec) / 1000.0));
+#endif
 }
 #endif
 
