b8d1213258ff782ae42bbfbd58168ef491515199
Brian Friedkin <brian@moddable.tech>
2019-10-30 16:32:55 -0700

    get connect/disconnect working; working on reliable start/stop advertising

--- modules/network/ble/nrf52/modBLEServer.c
+++ modules/network/ble/nrf52/modBLEServer.c
@@ -26,6 +26,7 @@
 
 #include "sdk_errors.h"
 #include "ble.h"
+#include "ble_conn_params.h"
 #include "ble_gap.h"
 #include "nrf_sdh.h"
 #include "nrf_sdh_ble.h"
@@ -35,6 +36,31 @@
 
 #define APP_BLE_CONN_CFG_TAG 1
 #define APP_BLE_OBSERVER_PRIO 3
+#define FIRST_CONN_PARAMS_UPDATE_DELAY      5000                                    /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (5 seconds). */
+#define NEXT_CONN_PARAMS_UPDATE_DELAY       30000                                   /**< Time between each call to sd_ble_gap_conn_param_update after the first call (30 seconds). */
+#define MAX_CONN_PARAMS_UPDATE_COUNT        3                                       /**< Number of attempts before giving up the connection parameter negotiation. */
+
+#define LOG_GATTS 0
+#if LOG_GATTS
+	#define LOG_GATTS_EVENT(event) logGATTSEvent(event)
+	#define LOG_GATTS_MSG(msg) modLog(msg)
+	#define LOG_GATTS_INT(i) modLogInt(i)
+#else
+	#define LOG_GATTS_EVENT(event)
+	#define LOG_GATTS_MSG(msg)
+	#define LOG_GATTS_INT(i)
+#endif
+
+#define LOG_GAP 1
+#if LOG_GAP
+	#define LOG_GAP_EVENT(event) logGAPEvent(event)
+	#define LOG_GAP_MSG(msg) modLog(msg)
+	#define LOG_GAP_INT(i) modLogInt(i)
+#else
+	#define LOG_GAP_EVENT(event)
+	#define LOG_GAP_MSG(msg)
+	#define LOG_GAP_INT(i)
+#endif
 
 static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context);
 static void bleServerCloseEvent(void *the, void *refcon, uint8_t *message, uint16_t messageLength);
@@ -46,10 +72,11 @@ typedef struct {
 	xsSlot obj;
 
 	// connection
-	uint16_t conn_handle;
+	int16_t conn_handle;
 	ble_gap_addr_t remote_bda;
 	
 	// advertising
+	uint8_t advertising;
 	uint8_t adv_handle;
 	uint8_t adv_data[BLE_GAP_ADV_SET_DATA_SIZE_MAX];
 	uint8_t scan_rsp_data[BLE_GAP_ADV_SET_DATA_SIZE_MAX];
@@ -72,6 +99,7 @@ static modBLE gBLE = NULL;
 void xs_ble_server_initialize(xsMachine *the)
 {
 	uint32_t ram_start = 0;
+    ble_conn_params_init_t cp_init;
     ret_code_t err_code;
     
 	if (NULL != gBLE)
@@ -97,14 +125,23 @@ void xs_ble_server_initialize(xsMachine *the)
     if (NRF_SUCCESS == err_code)
     	err_code = nrf_sdh_ble_enable(&ram_start);
 
+	// Initialize connection parameters (required)
+    if (NRF_SUCCESS == err_code) {
+		c_memset(&cp_init, 0, sizeof(cp_init));
+		cp_init.first_conn_params_update_delay = FIRST_CONN_PARAMS_UPDATE_DELAY;
+		cp_init.next_conn_params_update_delay  = NEXT_CONN_PARAMS_UPDATE_DELAY;
+		cp_init.max_conn_params_update_count   = MAX_CONN_PARAMS_UPDATE_COUNT;
+		err_code = ble_conn_params_init(&cp_init);
+    }
+
 	if (NRF_SUCCESS != err_code)
 		xsUnknownError("ble initialization failed");
 
     // Register a handler for BLE events.
 	NRF_SDH_BLE_OBSERVER(m_ble_observer, APP_BLE_OBSERVER_PRIO, ble_evt_handler, NULL);
-		
+
     // Create a FreeRTOS task for the BLE stack.
-    nrf_sdh_freertos_init(NULL, &gBLE->erase_bonds);
+	nrf_sdh_freertos_init(NULL, &gBLE->erase_bonds);
     
 	modMessagePostToMachine(the, NULL, 0, bleServerReadyEvent, NULL);
 }
@@ -135,10 +172,8 @@ void xs_ble_server_destructor(void *data)
 
 void xs_ble_server_disconnect(xsMachine *the)
 {
-	if (-1 != gBLE->conn_handle) {
+	if (-1 != gBLE->conn_handle)
 		sd_ble_gap_disconnect(gBLE->conn_handle, BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
-		gBLE->conn_handle = -1;
-	}
 }
 
 void xs_ble_server_get_local_address(xsMachine *the)
@@ -171,6 +206,10 @@ void xs_ble_server_start_advertising(xsMachine *the)
 	ble_gap_adv_data_t adv_data;
 	ble_gap_adv_params_t adv_params;
 	
+	if (gBLE->advertising)
+		xsUnknownError("ble already advertising");
+		
+modLog("in start advertising");
 	uint16_t intervalMin = xsmcToInteger(xsArg(0));
 	uint16_t intervalMax = xsmcToInteger(xsArg(1));
 	uint8_t *advertisingData = (uint8_t*)xsmcToArrayBuffer(xsArg(2));
@@ -198,13 +237,27 @@ void xs_ble_server_start_advertising(xsMachine *the)
 	if (NRF_SUCCESS == err_code)
 		err_code = sd_ble_gap_adv_start(gBLE->adv_handle, APP_BLE_CONN_CFG_TAG);
 	
-	if (NRF_SUCCESS != err_code)
+	if (NRF_SUCCESS != err_code) {
+		modLogInt(err_code);
 		xsUnknownError("ble start advertising failed");
+	}
+	
+	gBLE->advertising = 1;
 }
 	
 void xs_ble_server_stop_advertising(xsMachine *the)
 {
-	sd_ble_gap_adv_stop(gBLE->adv_handle);
+    ret_code_t err_code;
+    
+	if (!gBLE->advertising)
+		xsUnknownError("ble not advertising");
+		
+	gBLE->advertising = 0;
+	err_code = sd_ble_gap_adv_stop(gBLE->adv_handle);
+	if (NRF_ERROR_INVALID_STATE != err_code) {
+		modLogInt(err_code);
+		xsUnknownError("ble stop advertising failed");
+	}
 }
 
 void xs_ble_server_characteristic_notify_value(xsMachine *the)
@@ -266,11 +319,10 @@ void bleServerCloseEvent(void *the, void *refcon, uint8_t *message, uint16_t mes
 
 void bleServerConnectedEvent(void *the, void *refcon, uint8_t *message, uint16_t messageLength)
 {
-	if (!gBLE)
-		return;
+	if (!gBLE) return;
 
 	ble_gap_evt_t *gap_evt = (ble_gap_evt_t*)message;
-	uint16_t conn_handle = gap_evt->conn_handle;
+	int16_t conn_handle = gap_evt->conn_handle;
 	xsBeginHost(gBLE->the);
 	if (-1 != gBLE->conn_handle)
 		goto bail;
@@ -285,8 +337,63 @@ void bleServerConnectedEvent(void *the, void *refcon, uint8_t *message, uint16_t
 	xsCall2(gBLE->obj, xsID_callback, xsString("onConnected"), xsVar(0));
 bail:
 	xsEndHost(gBLE->the);
+}
+
+void bleServerDisconnectedEvent(void *the, void *refcon, uint8_t *message, uint16_t messageLength)
+{
+	if (!gBLE) return;
+
+    ret_code_t err_code;
+	ble_gap_evt_t *gap_evt = (ble_gap_evt_t*)message;
+	int16_t conn_handle = gap_evt->conn_handle;
 	
-	
+	if (gBLE->advertising) {
+		gBLE->advertising = 0;
+		err_code = sd_ble_gap_adv_stop(gBLE->adv_handle);
+		if (NRF_ERROR_INVALID_STATE != err_code) {
+			modLogInt(err_code);
+			xsUnknownError("ble stop advertising failed");
+		}
+	}
+	xsBeginHost(gBLE->the);
+	if (conn_handle != gBLE->conn_handle)
+		goto bail;
+	gBLE->conn_handle = -1;
+	xsmcVars(3);
+	xsVar(0) = xsmcNewObject();
+	xsmcSetInteger(xsVar(1), conn_handle);
+	xsmcSet(xsVar(0), xsID_connection, xsVar(1));
+	xsmcSetArrayBuffer(xsVar(2), gBLE->remote_bda.addr, 6);
+	xsmcSet(xsVar(0), xsID_address, xsVar(2));
+	xsCall2(gBLE->obj, xsID_callback, xsString("onDisconnected"), xsVar(0));
+bail:
+	xsEndHost(gBLE->the);
+}
+
+static void logGAPEvent(uint16_t evt_id) {
+	switch(evt_id) {
+		case BLE_GAP_EVT_CONNECTED: modLog("BLE_GAP_EVT_CONNECTED"); break;
+		case BLE_GAP_EVT_DISCONNECTED: modLog("BLE_GAP_EVT_DISCONNECTED"); break;
+		case BLE_GAP_EVT_CONN_PARAM_UPDATE: modLog("BLE_GAP_EVT_CONN_PARAM_UPDATE"); break;
+		case BLE_GAP_EVT_SEC_PARAMS_REQUEST: modLog("BLE_GAP_EVT_SEC_PARAMS_REQUEST"); break;
+		case BLE_GAP_EVT_SEC_INFO_REQUEST: modLog("BLE_GAP_EVT_SEC_INFO_REQUEST"); break;
+		case BLE_GAP_EVT_PASSKEY_DISPLAY: modLog("BLE_GAP_EVT_PASSKEY_DISPLAY"); break;
+		case BLE_GAP_EVT_KEY_PRESSED: modLog("BLE_GAP_EVT_KEY_PRESSED"); break;
+		case BLE_GAP_EVT_AUTH_KEY_REQUEST: modLog("BLE_GAP_EVT_AUTH_KEY_REQUEST"); break;
+		case BLE_GAP_EVT_LESC_DHKEY_REQUEST: modLog("BLE_GAP_EVT_LESC_DHKEY_REQUEST"); break;
+		case BLE_GAP_EVT_AUTH_STATUS: modLog("BLE_GAP_EVT_AUTH_STATUS"); break;
+		case BLE_GAP_EVT_CONN_SEC_UPDATE: modLog("BLE_GAP_EVT_CONN_SEC_UPDATE"); break;
+		case BLE_GAP_EVT_TIMEOUT: modLog("BLE_GAP_EVT_TIMEOUT"); break;
+		case BLE_GAP_EVT_RSSI_CHANGED: modLog("BLE_GAP_EVT_RSSI_CHANGED"); break;
+		case BLE_GAP_EVT_CONN_PARAM_UPDATE_REQUEST: modLog("BLE_GAP_EVT_CONN_PARAM_UPDATE_REQUEST"); break;
+		case BLE_GAP_EVT_SCAN_REQ_REPORT: modLog("BLE_GAP_EVT_SCAN_REQ_REPORT"); break;
+		case BLE_GAP_EVT_PHY_UPDATE_REQUEST: modLog("BLE_GAP_EVT_PHY_UPDATE_REQUEST"); break;
+		case BLE_GAP_EVT_PHY_UPDATE: modLog("BLE_GAP_EVT_PHY_UPDATE"); break;
+		case BLE_GAP_EVT_DATA_LENGTH_UPDATE_REQUEST: modLog("BLE_GAP_EVT_DATA_LENGTH_UPDATE_REQUEST"); break;
+		case BLE_GAP_EVT_DATA_LENGTH_UPDATE: modLog("BLE_GAP_EVT_DATA_LENGTH_UPDATE"); break;
+		case BLE_GAP_EVT_QOS_CHANNEL_SURVEY_REPORT: modLog("BLE_GAP_EVT_QOS_CHANNEL_SURVEY_REPORT"); break;
+		case BLE_GAP_EVT_ADV_SET_TERMINATED: modLog("BLE_GAP_EVT_ADV_SET_TERMINATED"); break;
+	}
 }
 
 void ble_evt_handler(const ble_evt_t *p_ble_evt, void * p_context)
@@ -295,14 +402,17 @@ void ble_evt_handler(const ble_evt_t *p_ble_evt, void * p_context)
 
 	if (!gBLE) return;
 	
+	LOG_GAP_EVENT(p_ble_evt->header.evt_id);
+	
     switch (p_ble_evt->header.evt_id)
     {
-		case BLE_GAP_EVT_CONNECTED: {
+		case BLE_GAP_EVT_CONNECTED:
 			modMessagePostToMachine(gBLE->the, (uint8_t*)&p_ble_evt->evt.gap_evt, sizeof(ble_gap_evt_t), bleServerConnectedEvent, NULL);
 			break;
-		}
 		case BLE_GAP_EVT_DISCONNECTED:
+			modMessagePostToMachine(gBLE->the, (uint8_t*)&p_ble_evt->evt.gap_evt, sizeof(ble_gap_evt_t), bleServerDisconnectedEvent, NULL);
 			break;
+			
 		case BLE_GAP_EVT_CONN_PARAM_UPDATE:
 			break;
 		case BLE_GAP_EVT_SEC_PARAMS_REQUEST:
