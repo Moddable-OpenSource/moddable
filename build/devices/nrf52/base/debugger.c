2a3e43fdee50213319376d5d97c862ceb30dce3d
Michael Kellner <mkellner@moddable.tech>
2019-10-30 17:05:42 -0700

    use raw uart

--- build/devices/nrf52/base/debugger.c
+++ build/devices/nrf52/base/debugger.c
@@ -24,14 +24,12 @@
 #include "mc.defines.h"
 
 #include "nrf.h"
-#include "nrf_serial.h"
-
+#include "nrfx_uart.h"
 #include "queue.h"
 
 #if mxDebug
 
 #define DEBUGGER_STACK	768
-#define SERIAL_BUFFER_SIZE			(1024)
 
 #ifndef MODDEF_DEBUGGER_RX_PIN
 	#define MODDEF_DEBUGGER_RX_PIN	31
@@ -43,69 +41,59 @@
 	#define MODDEF_DEBUGGER_BAUDRATE	NRF_UART_BAUDRATE_115200
 #endif
 
-static void sleep_handler(void);
-static void event_handler(struct nrf_serial_s const *p_serial, nrf_serial_event_t event);
-
-NRF_SERIAL_DRV_UART_CONFIG_DEF(gDebuggerUartConfig,
-		MODDEF_DEBUGGER_RX_PIN, MODDEF_DEBUGGER_TX_PIN,
-		-1, -1, NRF_UART_HWFC_DISABLED, NRF_UART_PARITY_EXCLUDED,
-		MODDEF_DEBUGGER_BAUDRATE, UART_DEFAULT_CONFIG_IRQ_PRIORITY);
+nrfx_uart_config_t gDebuggerUartConfig = {
+	.pseltxd = MODDEF_DEBUGGER_TX_PIN,
+	.pselrxd = MODDEF_DEBUGGER_RX_PIN,
+	.pselcts = -1,
+	.pselrts = -1,
+	.p_context = NULL,
+	.hwfc = NRF_UART_HWFC_DISABLED,
+	.parity = NRF_UART_PARITY_EXCLUDED,
+	.baudrate = MODDEF_DEBUGGER_BAUDRATE,
+	.interrupt_priority = UART_DEFAULT_CONFIG_IRQ_PRIORITY };
 		
-NRF_SERIAL_QUEUES_DEF(gDebuggerQueues, SERIAL_BUFFER_SIZE, SERIAL_BUFFER_SIZE);
-
-#define SERIAL_BUFF_TX_SIZE 1     // SERIAL_BUFFER_SIZE
-#define SERIAL_BUFF_RX_SIZE 1     // SERIAL_BUFFER_SIZE
-NRF_SERIAL_BUFFERS_DEF(gDebuggerBuffers, SERIAL_BUFF_TX_SIZE, SERIAL_BUFF_RX_SIZE);
-
-//@@ modLog might not be working because we're using MODE_DMA which only reads from RAM, not flash
-NRF_SERIAL_CONFIG_DEF(gDebuggerSerialConfig, NRF_SERIAL_MODE_DMA, &gDebuggerQueues, &gDebuggerBuffers, event_handler, sleep_handler);
-
-NRF_SERIAL_UART_DEF(gDebuggerUarte, 0);
+nrfx_uart_t gDebuggerUart = {
+    .p_reg        = NRFX_CONCAT_2(NRF_UART, 0),
+    .drv_inst_idx = NRFX_CONCAT_3(NRFX_UART, 0, _INST_IDX),
+};
 
 int ESP_getc(void);
 void ESP_putc(int c);
 void setupDebugger();
 
-static uint8_t readable = 0;
-static volatile uint8_t sReceivedRx = 0;
-
-static void sleep_handler(void) {
-	__WFE();
-	__SEV();
-	__WFE();
-}
-
-static uint32_t xx_tx_done_count = 0;
+static uint32_t xx_tx_data_count = 0;
 static uint32_t xx_rx_data_count = 0;
+static uint32_t xx_tx_drv_err = 0;
 
 #define DEBUG_QUEUE_LEN			8
-#define DEBUG_QUEUE_ITEM_SIZE	4
+#define DEBUG_QUEUE_ITEM_SIZE           4
 static QueueHandle_t gUARTQueue;
 
-#define DEBUG_TASK_CREATED	6
-#define DEBUG_READABLE		13
+#define DEBUG_TASK_CREATED    6
+#define DEBUG_READABLE        13
+#define DEBUG_WRITABLE        14
+#define DEBUG_DRV_ERR         15
 
-// event_handler happens in IRQ
-static void event_handler(struct nrf_serial_s const *p_serial, nrf_serial_event_t event) {
-	uint32_t msg;
-	switch (event) {
-		case NRF_SERIAL_EVENT_TX_DONE:
-                        xx_tx_done_count++;
+// uart_handler happens in IRQ
+static void uart_handler(nrfx_uart_event_t const *p_event, void *p_context) {
+	uint32_t msg = 0;
+
+	switch (p_event->type) {
+		case NRFX_UART_EVT_TX_DONE:
+			msg = DEBUG_WRITABLE;
+			xx_tx_data_count++;
 			break;
-		case NRF_SERIAL_EVENT_RX_DATA:
-			readable = 1;
-            xx_rx_data_count++;
-                        if (!sReceivedRx) {
-                          msg = DEBUG_READABLE;
-                          xQueueSendFromISR(gUARTQueue, &msg, NULL);
-                          sReceivedRx = 1;
-                        }
+		case NRFX_UART_EVT_RX_DONE:
+			msg = DEBUG_READABLE;
+			xx_rx_data_count++;
 			break;
-		case NRF_SERIAL_EVENT_DRV_ERR:
-			break;
-		case NRF_SERIAL_EVENT_FIFO_ERR:
+		case NRFX_UART_EVT_ERROR:
+			msg = DEBUG_DRV_ERR;
+			xx_tx_drv_err++;
 			break;
 	}
+	if (msg)
+		xQueueSendFromISR(gUARTQueue, &msg, NULL);
 }
 
 static void debug_task(void *pvParameter) {
@@ -114,25 +102,28 @@ static void debug_task(void *pvParameter) {
 	msg = DEBUG_TASK_CREATED;
 	xQueueSend(gUARTQueue, &msg, 0);
 
+	nrfx_uart_rx_enable(&gDebuggerUart);
+
 	while (true) {
 		if (!xQueueReceive(gUARTQueue, (void*)&msg, portMAX_DELAY))
 			continue;
 
 		if (DEBUG_READABLE == msg) {
-			sReceivedRx = 0;
 			fxReceiveLoop();		
+		}
+		else if (DEBUG_DRV_ERR == msg) {
+			modLog("uart error\n");
 		}
 	}
 }
 		
 //---------
 void setupDebugger() {
-	uint32_t	msg = 0;
 	ret_code_t ret;
 
-	ret = nrf_serial_init(&gDebuggerUarte, &gDebuggerUartConfig, &gDebuggerSerialConfig);
-
 	gUARTQueue = xQueueCreate(DEBUG_QUEUE_LEN, DEBUG_QUEUE_ITEM_SIZE);
+
+	ret = nrfx_uart_init(&gDebuggerUart, &gDebuggerUartConfig, uart_handler);
 
 	xTaskCreate(debug_task, "debug", DEBUGGER_STACK/sizeof(StackType_t), NULL, 8, NULL);
 }
@@ -150,8 +141,8 @@ void modLog_transmit(const char *msg)
 	}
 	else
 #endif
-	while (0 != (c = c_read8(msg++)))
-		ESP_putc(c);
+		while (0 != (c = c_read8(msg++)))
+			ESP_putc(c);
 	ESP_putc('\r');
 	ESP_putc('\n');
 }
@@ -162,11 +153,14 @@ void ESP_putc(int c) {
 	uint8_t ch;
 
 	ch = c;
-	while (NRF_ERROR_BUSY == (ret = nrf_serial_write(&gDebuggerUarte, &ch, 1, NULL, NRF_SERIAL_MAX_TIMEOUT)))
+	while (nrfx_uart_tx_in_progress(&gDebuggerUart))
 		taskYIELD();
 
-        if (NRF_SUCCESS != ret)
-          oof++;
+	while (NRF_ERROR_BUSY == (ret = nrfx_uart_tx(&gDebuggerUart, &ch, 1)))
+		taskYIELD();
+
+	if (NRF_SUCCESS != ret)
+		oof++;
 }
 
 static int debugRead = 0;
@@ -174,19 +168,20 @@ int ESP_getc(void) {
 	uint8_t ch;
 	int ret;
 
-	while (NRF_ERROR_BUSY == (ret = nrf_serial_read(&gDebuggerUarte, &ch, 1, NULL, 0)))
-		taskYIELD();
+	if (nrfx_uart_rx_ready(&gDebuggerUart)) {
+		while (NRF_ERROR_BUSY == (ret = nrfx_uart_rx(&gDebuggerUart, &ch, 1)))
+			taskYIELD();
 
-	if (NRF_SUCCESS == ret)
-		return ch;
+		if (NRF_SUCCESS == ret)
+			return ch;
+	}
 
 	return -1;
 }
 
-uint8_t ESP_isReadable() { return 0; }
-uint8_t xESP_isReadable() {
+uint8_t ESP_isReadable() {
 	uint8_t ret;
-	ret = nrf_drv_uart_rx_ready(&gDebuggerUarte);
+	ret = nrfx_uart_rx_ready(&gDebuggerUart);
 	return ret;
 }
 #else
