ac134cfd52c268a30a3f753acbf619c539c71124
Michael Kellner <mkellner@moddable.tech>
2019-11-01 16:52:35 -0700

    updates for analog support

--- build/devices/nrf52/config/sdk_config.h
+++ build/devices/nrf52/config/sdk_config.h
@@ -3271,7 +3271,7 @@
 // <e> NRFX_SAADC_ENABLED - nrfx_saadc - SAADC peripheral driver
 //==========================================================
 #ifndef NRFX_SAADC_ENABLED
-#define NRFX_SAADC_ENABLED 0
+#define NRFX_SAADC_ENABLED 1
 #endif
 // <o> NRFX_SAADC_CONFIG_RESOLUTION  - Resolution
  
@@ -5309,7 +5309,7 @@
 // <e> SAADC_ENABLED - nrf_drv_saadc - SAADC peripheral driver - legacy layer
 //==========================================================
 #ifndef SAADC_ENABLED
-#define SAADC_ENABLED 0
+#define SAADC_ENABLED 1
 #endif
 // <o> SAADC_CONFIG_RESOLUTION  - Resolution
  
