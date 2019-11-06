2f96aafb645634d659d26dfb7c677ce1b2c6db4a
Brian Friedkin <brian@moddable.tech>
2019-11-05 17:41:46 -0800

    security manager work in progress

--- modules/network/ble/nrf52/modBLESM.c
+++ modules/network/ble/nrf52/modBLESM.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018  Moddable Tech, Inc.
+ * Copyright (c) 2016-2019  Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK Runtime.
  * 
@@ -22,12 +22,56 @@
 #include "xsHost.h"
 #include "mc.xs.h"
 #include "modBLE.h"
+#include "peer_manager.h"
+
+#define SEC_PARAM_MIN_KEY_SIZE 7
+#define SEC_PARAM_MAX_KEY_SIZE 16
 
 void xs_ble_sm_delete_all_bondings(xsMachine *the)
 {
+	pm_peers_delete();
 }
 
 void modBLESetSecurityParameters(uint8_t encryption, uint8_t bonding, uint8_t mitm, uint16_t ioCapability)
 {
+	uint8_t io_caps;
+	ble_gap_sec_params_t sec_param;
+	ret_code_t err_code;
+
+ 	switch(ioCapability) {
+ 		case NoInputNoOutput:
+ 			io_caps = BLE_GAP_IO_CAPS_NONE;
+ 			break;
+ 		case DisplayOnly:
+ 			io_caps = BLE_GAP_IO_CAPS_DISPLAY_ONLY;
+ 			break;
+ 		case KeyboardOnly:
+ 			io_caps = BLE_GAP_IO_CAPS_KEYBOARD_ONLY;
+ 			break;
+ 		case KeyboardDisplay:
+ 			io_caps = BLE_GAP_IO_CAPS_KEYBOARD_DISPLAY;
+ 			break;
+ 		case DisplayYesNo:
+ 			io_caps = BLE_GAP_IO_CAPS_DISPLAY_YESNO;
+ 			break;
+ 	} 	
+
+    c_memset(&sec_param, 0, sizeof(ble_gap_sec_params_t));
+    sec_param.bond           = bonding;
+    sec_param.mitm           = mitm;
+    sec_param.lesc           = (mitm && (io_caps == BLE_GAP_IO_CAPS_DISPLAY_YESNO));
+    sec_param.keypress       = 0;
+    sec_param.io_caps        = io_caps;
+    sec_param.oob            = 0;
+    sec_param.min_key_size   = SEC_PARAM_MIN_KEY_SIZE;
+    sec_param.max_key_size   = SEC_PARAM_MAX_KEY_SIZE;
+    if (bonding) {
+		sec_param.kdist_own.enc  = 1;
+		sec_param.kdist_own.id   = 1;
+		sec_param.kdist_peer.enc = 1;
+		sec_param.kdist_peer.id  = 1;
+    }
+
+	err_code = pm_sec_params_set(&sec_param);
 }
 
