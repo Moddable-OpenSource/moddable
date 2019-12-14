7c2bccc7c8e96181de6ab124b97697213c672258
Peter Hoddie <peter@moddable.tech>
2019-12-14 21:16:28 +0900

    Net.get("IP") for macOS

--- modules/network/net/mac/modNet.m
+++ modules/network/net/mac/modNet.m
@@ -20,6 +20,9 @@
 
 #include "xs.h"
 
+#include <ifaddrs.h>
+#include <netdb.h>
+
 #import <CoreWLAN/CoreWLAN.h>
 
 void xs_net_get(xsMachine *the)
@@ -44,7 +47,33 @@ void xs_net_get(xsMachine *the)
 	else if (0 == c_strcmp(prop, "BSSID")) {
 		CWInterface* wifi = [[CWWiFiClient sharedWiFiClient] interface];
 		NSString *bssid = wifi.bssid;
-		xsResult = xsString([bssid UTF8String]);
+		if (bssid)
+			xsResult = xsString([bssid UTF8String]);
+	}
+	else if (0 == c_strcmp(prop, "IP")) {
+		int localhost = 0;
+		struct ifaddrs *ifaddr, *ifa;
+		if (getifaddrs(&ifaddr) == -1)
+			return;
+		for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+			if (ifa->ifa_addr == NULL)
+				continue;
+			if (ifa->ifa_addr->sa_family == AF_INET) {
+				struct sockaddr_in* address = (struct sockaddr_in*)ifa->ifa_addr;
+				int ip = ntohl(address->sin_addr.s_addr);
+				char buffer[22];
+				snprintf(buffer, 22, "%u.%u.%u.%u", (ip & 0xff000000) >> 24, (ip & 0x00ff0000) >> 16, (ip & 0x0000ff00) >> 8, (ip & 0x000000ff));
+				if (!c_strcmp(buffer, "127.0.0.1"))
+					localhost = 1;
+				else {
+					xsResult = xsString(buffer);
+					break;
+				}
+			}
+		}
+		if (!xsTest(xsResult) && localhost)
+			xsResult = xsString("127.0.0.1");
+		freeifaddrs(ifaddr);
 	}
 }
 
