ec89ed7d4331cf2e8d867bb3df639bd7d91195a1
Patrick Soquet <ps@moddable.tech>
2020-01-09 13:03:10 +0100

    XS: version number

--- xs/sources/xsCommon.h
+++ xs/sources/xsCommon.h
@@ -93,9 +93,9 @@ typedef struct {
 #define XS_ATOM_SIGNATURE 0x5349474E /* 'SIGN' */
 #define XS_ATOM_SYMBOLS 0x53594D42 /* 'SYMB' */
 #define XS_ATOM_VERSION 0x56455253 /* 'VERS' */
-#define XS_MAJOR_VERSION 9
+#define XS_MAJOR_VERSION 10
 #define XS_MINOR_VERSION 0
-#define XS_PATCH_VERSION 3
+#define XS_PATCH_VERSION 0
 
 #define XS_DIGEST_SIZE 16
 #define XS_VERSION_SIZE 4
