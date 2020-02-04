4161d21ac31db621ca2847fa6a05d8e601a15632
Brian Friedkin <brian@moddable.tech>
2020-02-03 17:07:18 -0800

    standardize led and button pin definition format across all targets

--- build/devices/nrf52/targets/sparkfun/setup-target.js
+++ build/devices/nrf52/targets/sparkfun/setup-target.js
@@ -19,11 +19,8 @@
  */
 
 import config from "mc/config";
-//import Digital from "pins/digital";
 
 export default function (done) {
-//	if (config.autobacklight)
-//		Digital.write(config.backlight_pin, 1);
-
+	// Put startup code in here
 	done();
 }
