175875aed74a8615aa9c3b9848378699a5a6e609
Peter Hoddie <peter@moddable.tech>
2020-04-01 13:44:03 -0700

    more keys

--- xs/tools/xsl.c
+++ xs/tools/xsl.c
@@ -89,7 +89,7 @@ int main(int argc, char* argv[])
 		4 * 1024 * 1024, 	/* initialHeapCount */
 		1 * 1024 * 1024,	/* incrementalHeapCount */
 		1024,				/* stackCount */
-		2048+2048,			/* keyCount */
+		2048 * 4,			/* keyCount */
 		1993,				/* nameModulo */
 		127,				/* symbolModulo */
 		32 * 1024,			/* parserBufferSize */
