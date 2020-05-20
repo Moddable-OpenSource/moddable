ce06a22a76529c685675fe660b0799fc7f555593
Brian Friedkin <brian@moddable.tech>
2020-05-20 16:30:32 -0700

    kill serial2xsbug earlier in build targets to avoid abort error messages

--- tools/mcconfig/nmake.nrf52.mk
+++ tools/mcconfig/nmake.nrf52.mk
@@ -72,7 +72,7 @@ DO_COPY = for /F "tokens=1" %%i in ( $(TMP_DIR)\_drive.tmp ) do @copy $(BIN_DIR)
 DO_XSBUG = tasklist /nh /fi "imagename eq xsbug.exe" | find /i "xsbug.exe" > nul || (start $(MODDABLE_TOOLS_DIR)\xsbug.exe)
 KILL_SERIAL_2_XSBUG =-tasklist /nh /fi "imagename eq serial2xsbug.exe" | (find /i "serial2xsbug.exe" > nul) && taskkill /f /t /im "serial2xsbug.exe" >nul 2>&1
 WAIT_FOR_NEW_SERIAL = $(PLATFORM_DIR)\config\waitForNewSerialWindows.bat 1 $(UF2_VOLUME_NAME) $(TMP_DIR)\_port.tmp
-SERIAL_2_XSBUG = echo Starting serial2xsbug. Press control-c to exit. && for /F "tokens=1" %%i in ( $(TMP_DIR)\_port.tmp ) do $(MODDABLE_TOOLS_DIR)\serial2xsbug %%i 921600 8n1 -dtr
+SERIAL_2_XSBUG = echo Starting serial2xsbug. Press control-c to exit. && for /F "tokens=1" %%i in ( $(TMP_DIR)\_port.tmp ) do $(MODDABLE_TOOLS_DIR)\serial2xsbug %%i 921600 8N1 -dtr
 !ELSE
 DO_XSBUG =
 KILL_SERIAL_2_XSBUG =
@@ -550,8 +550,8 @@ LINKER_SCRIPT = $(PLATFORM_DIR)\config\xsproj.ld
 precursor: $(BLE) $(TMP_DIR) $(LIB_DIR) $(BIN_DIR)\xs_nrf52.hex
 
 all: precursor $(BIN_DIR)\xs_nrf52.uf2
-	$(WAIT_FOR_M4)
 	$(KILL_SERIAL_2_XSBUG)
+	$(WAIT_FOR_M4)
 	$(DO_XSBUG)
 	@echo Copying: $(BIN_DIR)\xs_nrf52.hex to $(UF2_VOLUME_NAME)
 	$(DO_COPY)
@@ -571,8 +571,8 @@ clean:
 	if exist $(LIB_DIR) rmdir /s/q $(LIB_DIR)
 
 xsbug:
-	$(WAIT_FOR_M4)
 	$(KILL_SERIAL_2_XSBUG)
+	$(WAIT_FOR_M4)
 	$(DO_XSBUG)
 	$(WAIT_FOR_NEW_SERIAL)
 	$(SERIAL_2_XSBUG)
