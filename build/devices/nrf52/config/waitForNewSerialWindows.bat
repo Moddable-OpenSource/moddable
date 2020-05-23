a73b224b5f4e93bf138b8a8043328135e842319e
Andy Carle <andy@moddable.tech>
2020-05-22 18:35:21 -0700

    tidying Windows batch files

--- build/devices/nrf52/config/waitForNewSerialWindows.bat
+++ build/devices/nrf52/config/waitForNewSerialWindows.bat
@@ -1,20 +1,24 @@
-@REM Wait for the Moddable Four volume to unmount.
-@echo | set /p moddableFourTemp=Installing to %2.
+echo off
+
+REM Wait for the Moddable Four volume to unmount.
+echo | set /p moddableFourTemp=Installing to %2.
 :DoWhile
-@wmic logicaldisk get caption, description, VolumeName | (find /i "%2")
-@IF %ERRORLEVEL% NEQ 0 (GOTO Done)
-@echo | set /p moddableFourTemp=.
-@timeout /T 1 /NOBREAK > nul
-@GOTO DoWhile
+wmic logicaldisk get caption, description, VolumeName | (find /i "%2")
+IF %ERRORLEVEL% NEQ 0 (GOTO Done)
+echo | set /p moddableFourTemp=.
+timeout /T 1 /NOBREAK > nul
+GOTO DoWhile
 :Done
-@echo .
+echo .
 
-@IF %1 == 0 (@echo Release build installed. && EXIT /b 0)
+IF %1 == 0 (echo Release build installed. && EXIT /b 0)
 
-@REM Wait for serial port with Vendor ID 0x1915 and Product ID 0x520F.
+REM Wait for serial port with Vendor ID 0x1915 and Product ID 0x520F.
 :SerialWhile
-@wmic path Win32_SerialPort where "PNPDeviceID like '%%VID_1915&PID_520F%%'" get DeviceID 2> nul | (find /i "COM" > %3)
-@IF %ERRORLEVEL% == 0 (GOTO SerialDone)
-@GOTO SerialWhile
+wmic path Win32_SerialPort where "PNPDeviceID like '%%VID_1915&PID_520F%%'" get DeviceID 2> nul | (find /i "COM" > %3)
+IF %ERRORLEVEL% == 0 (GOTO SerialDone)
+GOTO SerialWhile
 :SerialDone
-@echo Debug build installed.
+echo Debug build installed.
+
+set moddableFourTemp=
--- build/devices/nrf52/config/waitForVolumeWindows.bat
+++ build/devices/nrf52/config/waitForVolumeWindows.bat
@@ -1,10 +1,14 @@
-@echo Waiting for volume %1.
-@echo | set /p moddableFourTemp=Double-tap the reset button to enter programming mode.
+echo off
+
+echo Waiting for volume %1.
+echo | set /p moddableFourTemp=Double-tap the reset button to enter programming mode.
 :DoWhile
-@wmic logicaldisk get caption, description, VolumeName | (find /i "%1" > %2)
-@IF %ERRORLEVEL% == 0 (GOTO Done)
-@echo | set /p moddableFourTemp=.
-@timeout /T 1 /NOBREAK > nul
-@GOTO DoWhile
+wmic logicaldisk get caption, description, VolumeName | (find /i "%1" > %2)
+IF %ERRORLEVEL% == 0 (GOTO Done)
+echo | set /p moddableFourTemp=.
+timeout /T 1 /NOBREAK > nul
+GOTO DoWhile
 :Done
-@echo volume found
+echo volume found
+
+set  moddableFourTemp=
