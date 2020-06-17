2fd11972729158315baccb847fe844c42e7a4c7c
Andy Carle <andy@moddable.tech>
2020-06-16 17:00:08 -0700

    fix names for Monitor edge constants (issue #385)

--- build/devices/esp32/targets/m5stack_fire/setup-target.js
+++ build/devices/esp32/targets/m5stack_fire/setup-target.js
@@ -21,9 +21,9 @@ export default function (done) {
 	global.lights = new NeoPixel({});
 
 	global.button = {
-		a: new Monitor({pin: 39, mode: Digital.InputPullUp, edge: Monitor.RisingEdge | Monitor.FallingEdge}),
-		b: new Monitor({pin: 38, mode: Digital.InputPullUp, edge: Monitor.RisingEdge | Monitor.FallingEdge}),
-		c: new Monitor({pin: 37, mode: Digital.InputPullUp, edge: Monitor.RisingEdge | Monitor.FallingEdge}),
+		a: new Monitor({pin: 39, mode: Digital.InputPullUp, edge: Monitor.Rising | Monitor.Falling}),
+		b: new Monitor({pin: 38, mode: Digital.InputPullUp, edge: Monitor.Rising | Monitor.Falling}),
+		c: new Monitor({pin: 37, mode: Digital.InputPullUp, edge: Monitor.Rising | Monitor.Falling}),
 	};
 	button.a.onChanged = button.b.onChanged = button.c.onChanged = nop;
 
