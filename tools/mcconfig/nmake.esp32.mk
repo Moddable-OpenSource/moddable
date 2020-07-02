e364b9093aa1309dd1c87d42e022351a27b01b52
Brian Friedkin <brian@moddable.tech>
2020-07-02 11:20:57 -0700

    override MYNEWT_VAL_BLE_LL_CONN_INIT_MAX_TX_BYTES and set to 251 so that data length extension (enabled) can transfer up to 251 bytes in a single packet

--- tools/mcconfig/nmake.esp32.mk
+++ tools/mcconfig/nmake.esp32.mk
@@ -303,7 +303,8 @@ C_DEFINES = \
 	-DmxUseDefaultSharedChunks=1 \
 	-DmxRun=1 \
 	-DkCommodettoBitmapFormat=$(DISPLAY) \
-	-DkPocoRotation=$(ROTATION)
+	-DkPocoRotation=$(ROTATION) \
+	-DMYNEWT_VAL_BLE_LL_CONN_INIT_MAX_TX_BYTES=251
 
 !IF "$(DEBUG)"=="1"
 C_DEFINES = $(C_DEFINES) -DmxDebug=1
