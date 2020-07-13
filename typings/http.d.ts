2d801480ad5fe96876d4a06f32113ccea99ce786
Peter Hoddie <peter@moddable.tech>
2020-07-12 23:06:12 -0700

    small updates

--- typings/http.d.ts
+++ typings/http.d.ts
@@ -15,20 +15,31 @@ declare module "http" {
   );
   class Request {
     constructor(options: {
+      host?: string,
+      address?: string,
       port?: number,
       path?: string,
       method?: string,
-      headers?: string[],
+      headers?: (string | number)[],
       body?: boolean | string | ArrayBuffer,
       response?: typeof String | typeof ArrayBuffer
     });
     close(): void;
+    read(): number;
     read<T extends typeof String | typeof ArrayBuffer>(
       type: T,
       until?: number
     ): T extends typeof String ? string : ArrayBuffer;
     // TODO: better
     callback: (message: RequestStatus, val1?: any, val2?: any) => void;
+    
+    static requestFragment: number;
+    static status: number;
+    static header: number;
+    static headersComplete: number;
+    static responseFragment: number;
+    static responseComplete: number;
+    static error: number;
   }
   class Server {
     constructor(
@@ -39,6 +50,17 @@ declare module "http" {
     close();
     // TODO: better
     callback: (message: number, val1?: any, val2?: any) => void;
-  }
+  
+    static connection: number;
+    static status: number;
+    static header: number;
+    static headersComplete: number;
+    static requestFragment: number;
+    static requestComplete: number;
+    static prepareResponse: number;
+    static responseFragment: number;
+    static responseComplete: number;
+    static error: number;
+}
   export {Request, Server};
 }
