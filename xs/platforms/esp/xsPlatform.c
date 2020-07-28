f32433ff230fb3d80da7572869a8ea2eb6d66417
Peter Hoddie <peter@moddable.tech>
2020-07-28 14:57:36 -0700

    mmm -> mm

--- xs/platforms/esp/xsPlatform.c
+++ xs/platforms/esp/xsPlatform.c
@@ -58,7 +58,7 @@
 #define kSerialConnection ((void *)0x87654321)
 
 static void fx_putpi(txMachine *the, char separator, txBoolean trailingcrlf);
-static void doRemoteCommmand(txMachine *the, uint8_t *cmd, uint32_t cmdLen);
+static void doRemoteCommand(txMachine *the, uint8_t *cmd, uint32_t cmdLen);
 
 #if defined (mxDebug) && ESP32
 	SemaphoreHandle_t gDebugMutex;
@@ -625,7 +625,7 @@ void fxReceive(txMachine* the)
 						if (0 == the->wsLength) {
 							if (the->wsFin) {
 								// received full remote command
-								doRemoteCommmand(the, the->wsCmd + sizeof(uint32_t), the->wsCmdPtr - (the->wsCmd + sizeof(uint32_t)));
+								doRemoteCommand(the, the->wsCmd + sizeof(uint32_t), the->wsCmdPtr - (the->wsCmd + sizeof(uint32_t)));
 								c_free(the->wsCmd);
 								the->wsCmd = the->wsCmdPtr = NULL;
 							}
@@ -685,7 +685,7 @@ void fxReceive(txMachine* the)
 					the->debugOffset = f->count;
 				}
 				else
-					doRemoteCommmand(the, f->bytes, f->count);
+					doRemoteCommand(the, f->bytes, f->count);
 				c_free(f);
 				break;
 			}
@@ -988,7 +988,7 @@ static void doLoadModule(modTimer timer, void *refcon, int refconSize)
 	modLoadModule((txMachine *)*(uintptr_t *)refcon, sizeof(uintptr_t) + (uint8_t *)refcon);
 }
 
-void doRemoteCommmand(txMachine *the, uint8_t *cmd, uint32_t cmdLen)
+void doRemoteCommand(txMachine *the, uint8_t *cmd, uint32_t cmdLen)
 {
 	uint16_t resultID = 0;
 	int16_t resultCode = 0;
