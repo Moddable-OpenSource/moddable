ea129a289e7958010f54249b7d2b5882a4ff2542
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2020-08-14 06:57:50 +0000

    make smbus module independent

--- typings/pins/smbus.d.ts
+++ typings/pins/smbus.d.ts
@@ -1,6 +1,35 @@
-import type I2C from "pins/i2c";
 declare module "pins/smbus" {
-  class SMBus extends I2C {
+  type TypedArray =
+    | Int8Array
+    | Uint8Array
+    | Int16Array
+    | Uint16Array
+    | Int32Array
+    | Uint32Array
+    | Uint8ClampedArray
+    | Float32Array
+    | Float64Array;
+  class SMBus {
+    public constructor(dictionary: {
+      address: number;
+      scl?: number;
+      sda?: number;
+      hz?: number;
+      timeout?: number;
+    });
+    public constructor(port: number[], pin: number, mode: number);
+    public close(): void;
+    public read(count: number, buffer?: ArrayBuffer): void;
+    public write(
+      first: any,
+      ...valuesOrStop: (
+        | number
+        | string
+        | (number | string)[]
+        | TypedArray
+        | boolean
+      )[]
+    ): void;
     public readByte(register: number): number;
     public readWord(register: number): number;
     public readBlock(
