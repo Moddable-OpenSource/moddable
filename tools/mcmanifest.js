0cd6f06f97b4cd1679b106027005a610eae71dee
Brian Friedkin <brian@moddable.tech>
2020-08-24 17:07:08 -0700

    esp32 application-provided sdkconfig file(s) now merged on top of base Moddable SDK sdkconfig files

--- tools/mcmanifest.js
+++ tools/mcmanifest.js
@@ -116,47 +116,56 @@ export class MakeFile extends FILE {
 	generateConfigurationRules(tool) {
 		if (("esp32" != tool.platform) || !tool.environment.SDKCONFIGPATH) return;
 		
-		// Read base sdkconfig file
-		let baseConfigDirectory = tool.buildPath + tool.slash + "devices" + tool.slash + "esp32" + tool.slash + "xsProj" + tool.slash;
-		let baseConfigFile = baseConfigDirectory + "sdkconfig.defaults";
+		// Read base debug build sdkconfig.defaults file
+		let mergedConfig = [];
+		let regex = /[\r\n]+/gm;
+		let baseConfigDirectory = tool.buildPath + tool.slash + "devices" + tool.slash + "esp32" + tool.slash + "xsProj";
+		let baseConfigFile = baseConfigDirectory + tool.slash + "sdkconfig.defaults";
 		let baseConfig = tool.readFileString(baseConfigFile);
 		let baseConfigLength = baseConfig.length;
 
-		// Read app sdkconfig file
-		let sdkconfigFile = tool.environment.SDKCONFIGPATH + tool.slash + "sdkconfig.defaults";
-		if (tool.debug) {
-			if (1 == tool.isDirectoryOrFile(sdkconfigFile + ".debug"))
-				sdkconfigFile += ".debug";
-		}
-		else {
-			if (1 == tool.isDirectoryOrFile(sdkconfigFile + ".release"))
-				sdkconfigFile += ".release";
+		// For release builds merge base sdkconfig.defaults.release file
+		if (tool.debug === false) {
+			let releaseBaseConfigFile = baseConfigFile + ".release";
+			let entries = tool.readFileString(releaseBaseConfigFile);
+			mergedConfig = entries.split(regex);
+
+			// For instrumented release builds merge base "sdkconfig.inst" file
+			if (tool.instrument === true) {
+				let instConfigFile = baseConfigDirectory + tool.slash + "sdkconfig.inst";
+				let instConfig = tool.readFileString(instConfigFile);
+				mergedConfig = mergedConfig.concat(instConfig.split(regex));
+			}
 		}
 		
-		let appConfig;
-		if (baseConfigFile != sdkconfigFile) {
-			appConfig = tool.readFileString(sdkconfigFile);
-			appConfig = appConfig.split(/[\r\n]+/gm);
+		// Merge any application sdkconfig files
+		if (tool.environment.SDKCONFIGPATH != baseConfigDirectory) {
+			let appConfigFile = tool.environment.SDKCONFIGPATH + tool.slash + "sdkconfig.defaults";
+			if ((false === tool.debug) && (1 == tool.isDirectoryOrFile(appConfigFile + ".release")))
+				appConfigFile += ".release";
+			if (1 == tool.isDirectoryOrFile(appConfigFile)) {
+				let entries = tool.readFileString(appConfigFile);
+				mergedConfig = mergedConfig.concat(entries.split(regex));
+			}
+			if (tool.debug === false && tool.instrument === true) {
+				appConfigFile = tool.environment.SDKCONFIGPATH + tool.slash + "sdkconfig.inst";
+				if (1 == tool.isDirectoryOrFile(appConfigFile)) {
+					let entries = tool.readFileString(appConfigFile);
+					mergedConfig = mergedConfig.concat(entries.split(regex));
+				}
+			}
 		}
-		else
-			appConfig = [];
-
-		if (tool.instrument === true && tool.debug === false) {
-			let instConfigFile = baseConfigDirectory + "sdkconfig.inst";
-			let instConfig = tool.readFileString(instConfigFile);
-			appConfig = appConfig.concat(appConfig, instConfig.split(/[\r\n]+/gm));
-		}
-			
+		
 		let port = tool.getenv("UPLOAD_PORT");
 		if (port) {
 			if (port.charAt(0) != '"')
 				port = `"${port}"`;
-			appConfig.push("CONFIG_ESPTOOLPY_PORT=" + port);
+			mergedConfig.push("CONFIG_ESPTOOLPY_PORT=" + port);
 		}
 
 		// Merge differences
 		let appended = false;
-		appConfig.forEach(option => {
+		mergedConfig.forEach(option => {
 			if (option.length && ('#' != option.charAt(0))) {
 				let parts = option.split('=');
 				let name = parts[0];
@@ -231,7 +240,7 @@ export class MakeFile extends FILE {
 		}
 		
 		// Write the result, if it has changed
-		let buildConfigFile = baseConfigDirectory + "sdkconfig.mc";
+		let buildConfigFile = baseConfigDirectory + tool.slash + "sdkconfig.mc";
 		tool.setenv("SDKCONFIG_FILE", buildConfigFile);
 		if (tool.isDirectoryOrFile(buildConfigFile) == 1){
 			const oldConfig = tool.readFileString(buildConfigFile);
