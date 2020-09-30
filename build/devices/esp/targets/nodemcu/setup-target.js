e6d5833ab4cea991c86e20937de7fdfb20f6cb82
Peter Hoddie <peter@moddable.tech>
2020-09-29 17:21:40 -0700

    button & led for Moddable One, Moddable Two, and Moddable Three hosts. Add NodeMCU targets for ESP8266 & ESP32 .

---
+++ build/devices/esp/targets/nodemcu/setup-target.js
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2018-2020  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ *
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import Button from "button";
+import LED from "led";
+
+class A  {
+	constructor(options) {
+		return new LED({
+			...options,
+			pin: 16,
+			invert: true
+		});
+	}
+};
+
+class B {
+	constructor(options) {
+		return new LED({
+			...options,
+			pin: 2,
+			invert: true
+		});
+	}
+};
+
+globalThis.Host = {
+	LED: {
+		Default: A,
+		A,
+		B,
+	},
+	Button: {
+		Default: class {
+			constructor(options) {
+				return new Button({
+					...options,
+					pin: 0,
+					invert: true
+				});
+			}
+		}
+	}
+};
+Object.freeze(Host, true);
--- build/devices/esp32/targets/moddable_two/setup-target.js
+++ build/devices/esp32/targets/moddable_two/setup-target.js
@@ -1,6 +1,8 @@
 import Digital from "pins/digital";
 import PWM from "pins/pwm";
 import config from "mc/config";
+import Button from "button";
+import LED from "led";
 
 class Backlight extends PWM {
 	constructor() {
@@ -13,14 +15,39 @@ class Backlight extends PWM {
 			value = 0;
 		else if (value > 100)
 			value = 100;
-		super.write((value / 100) * 1023);
+		value = (value / 100) * 1023;
+		super.write(value);
 	}
 }
 
+globalThis.Host = {
+	Backlight,
+	LED: {
+		Default: class {
+			constructor(options) {
+				return new LED({
+					...options,
+					pin: 2
+				});
+			}
+		}
+	},
+	Button: {
+		Default: class {
+			constructor(options) {
+				return new Button({
+					...options,
+					pin: 0,
+					invert: true
+				});
+			}
+		}
+	}
+}
+Object.freeze(globalThis.Host, true);
+
 export default function (done) {
-	if ("none" !== config.brightness)
-		globalThis.backlight = new Backlight;
-	else
+	if ("none" === config.brightness)
 		Digital.write(config.backlight, 0);
 
 	done();
