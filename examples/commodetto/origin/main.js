94cb26980ce45d7588f8bfaabdb96a1416750c0e
Lizzie Prader <lizzie@moddable.tech>
2020-10-02 15:33:49 -0700

    boilerplate update + screen size warnings in commodetto apps

--- examples/commodetto/origin/main.js
+++ examples/commodetto/origin/main.js
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2016-2017  Moddable Tech, Inc.
+ * Copyright (c) 2016-2020 Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK.
  * 
  *   This work is licensed under the
  *       Creative Commons Attribution 4.0 International License.
  *   To view a copy of this license, visit
- *       <https://creativecommons.org/licenses/by/4.0>.
+ *       <https://creativecommons.org/licenses/by/4.0>
  *   or send a letter to Creative Commons, PO Box 1866,
  *   Mountain View, CA 94042, USA.
  *
@@ -15,6 +15,8 @@
 import Poco from "commodetto/Poco";
 
 let render = new Poco(screen);
+if (render.height != 240 || render.width != 320)
+	trace("WARNING: This application was designed to run on a 320x240 screen.\n");
 
 let black = render.makeColor(0, 0, 0);
 let blue = render.makeColor(0, 0, 255);
--- examples/commodetto/rotated/main.js
+++ examples/commodetto/rotated/main.js
@@ -1,19 +1,15 @@
 /*
- * Copyright (c) 2016-2017  Moddable Tech, Inc.
+ * Copyright (c) 2016-2020 Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK.
  * 
  *   This work is licensed under the
  *       Creative Commons Attribution 4.0 International License.
  *   To view a copy of this license, visit
- *       <https://creativecommons.org/licenses/by/4.0>.
+ *       <https://creativecommons.org/licenses/by/4.0>
  *   or send a letter to Creative Commons, PO Box 1866,
  *   Mountain View, CA 94042, USA.
  *
- */
-/*
- *     This app should be built with rotation set to 90 degrees:
- *     mcconfig -d -m -p esp -r 90
  */
 
 import parseBMF from "commodetto/parseBMF";
@@ -22,6 +18,8 @@ import Poco from "commodetto/Poco";
 import Resource from "Resource";
 
 let render = new Poco(screen, { rotation:90 });
+if (render.height != 240 || render.width != 320)
+	trace("WARNING: This application was designed to run on a 320x240 screen.\n");
 
 let black = render.makeColor(0, 0, 0);
 let white = render.makeColor(255, 255, 255);
