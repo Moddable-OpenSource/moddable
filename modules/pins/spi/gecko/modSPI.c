aa2e6b3663f37bec380bae9144955833a9f6680d
svenni <svenni@server.local>
2020-11-14 01:33:40 +0100

    Comment consistency

--- modules/pins/spi/gecko/modSPI.c
+++ modules/pins/spi/gecko/modSPI.c
@@ -128,17 +128,17 @@ void modSPIInit(modSPIConfiguration config)
 	
 	switch (config->mode) {
 		case 3:
-			spiInit.clockMode = usartClockMode2;   /* CPOL = 1, CPHA = 1 */
+			spiInit.clockMode = usartClockMode2;   // CPOL = 1, CPHA = 1
 			break;
 		case 2:
-			spiInit.clockMode = usartClockMode3;   /* CPOL = 1, CPHA = 0 */
+			spiInit.clockMode = usartClockMode3;   // CPOL = 1, CPHA = 0
 			break;
 		case 1:
-			spiInit.clockMode = usartClockMode1;   /* CPOL = 0, CPHA = 1 */
+			spiInit.clockMode = usartClockMode1;   // CPOL = 0, CPHA = 1
 			break;
 		case 0:
 		default:
-			spiInit.clockMode = usartClockMode0;   /* CPOL = 0, CPHA = 0 */
+			spiInit.clockMode = usartClockMode0;   // CPOL = 0, CPHA = 0
 			break;
 	}
 	
