af137eb724a9477b26071a210bcb1b1986826a58
Patrick Soquet <ps@moddable.tech>
2020-12-10 08:28:23 +0100

    xsbug: serial experiment

---
+++ modules/io/serial/win/serial.c
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2016-2020  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ *
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "xsAll.h"
+#include "mc.xs.h"
+
+/*
+	to do:
+
+		everything
+
+*/
+
+void xs_serial_destructor(void *data)
+{
+}
+
+void xs_serial_constructor(xsMachine *the)
+{
+	xsUnknownError("not implemented");
+}
+
+void xs_serial_close(xsMachine *the)
+{
+}
+
+void xs_serial_read(xsMachine *the)
+{
+}
+
+void xs_serial_write(xsMachine *the)
+{
+}
+
+void xs_serial_set(xsMachine *the)
+{
+}
+
+void xs_serial_format_get(xsMachine *the)
+{
+}
+
+void xs_serial_format_set(xsMachine *the)
+{
+}
+
+void xs_serial_purge(xsMachine *the)
+{
+}
