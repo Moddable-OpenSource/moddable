84fd9e47ef912eb6a60ad8b706bb92f2be6071c7
Peter Hoddie <peter@moddable.tech>
2020-12-14 13:35:46 -0800

    fix timers when using works on QCA4020. add check to avoid mistake in future.

--- xs/platforms/qca4020/xsHost.h
+++ xs/platforms/qca4020/xsHost.h
@@ -230,6 +230,10 @@ typedef void (*modMessageDeliver)(void *the, void *refcon, uint8_t *message, uin
     void modMachineTaskWake(xsMachine *the);
 #endif
 
+#define MOD_TASKS (true)
+
+#define modTaskGetCurrent() ((uintptr_t)xTaskGetCurrentTaskHandle())
+
 /*
 	instrumentation
 */
