d7e466c3903c09384f03f099ea9aaa1dc0702d80
Brian Friedkin <brian@moddable.tech>
2020-12-18 11:34:41 -0800

    reduce flash data storage used internally by nrf52 sdk for ble bonding by two pages so that it doesn't overlap with flash area used to store our preferences

--- build/devices/nrf52/config/sdk_config.h
+++ build/devices/nrf52/config/sdk_config.h
@@ -6358,7 +6358,7 @@
 // <i> As a result the reserved space can be used by other modules.
 
 #ifndef FDS_VIRTUAL_PAGES_RESERVED
-#define FDS_VIRTUAL_PAGES_RESERVED 0
+#define FDS_VIRTUAL_PAGES_RESERVED 2
 #endif
 
 // </h> 
