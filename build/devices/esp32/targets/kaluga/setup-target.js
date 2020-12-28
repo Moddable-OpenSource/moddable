ef230813ea203a4f8f9f8183b5a570ce7f5519a6
Andy Carle <andy@moddable.tech>
2020-12-28 14:03:13 -0800

    kaluga sub-platform start

---
+++ build/devices/esp32/targets/kaluga/setup-target.js
@@ -0,0 +1,56 @@
+//import Digital from "pins/digital";
+//import PWM from "pins/pwm";
+import config from "mc/config";
+//import Button from "button";
+//import LED from "led";
+
+// class Backlight extends PWM {
+// 	constructor(brightness = 100) {
+// 		super({pin: config.backlight});
+// 		this.write(brightness);
+// 	}
+// 	write(value) {
+// 		value = 100 - value;		// PWM is inverted
+// 		if (value <= 0)
+// 			value = 0;
+// 		else if (value >= 100)
+// 			value = 1023;
+// 		else
+// 			value = (value / 100) * 1023;
+// 		super.write(value);
+// 	}
+// }
+
+globalThis.Host = Object.freeze({
+	// Backlight,
+	// LED: {
+	// 	Default: class {
+	// 		constructor(options) {
+	// 			return new LED({
+	// 				...options,
+	// 				pin: 2
+	// 			});
+	// 		}
+	// 	}
+	// },
+	// Button: {
+	// 	Default: class {
+	// 		constructor(options) {
+	// 			return new Button({
+	// 				...options,
+	// 				pin: 0,
+	// 				invert: true
+	// 			});
+	// 		}
+	// 	}
+	// }
+}, true);
+
+export default function (done) {
+	// if ((undefined === config.brightness) || ("none" === config.brightness))
+	// 	Digital.write(config.backlight, 0);
+	// else
+	// 	globalThis.backlight = new Backlight(parseInt(config.brightness));
+
+	done();
+}
