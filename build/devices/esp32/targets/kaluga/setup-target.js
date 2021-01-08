fba348b82e4546239f57466ecd2a29b296533b22
Peter Hoddie <peter@moddable.tech>
2021-01-08 13:50:10 -0800

    some clean-up

--- build/devices/esp32/targets/kaluga/setup-target.js
+++ build/devices/esp32/targets/kaluga/setup-target.js
@@ -10,22 +10,23 @@ class ButtonArray {
 	#callback;
 	#pushed;
 	constructor(options){
-		let delay = 50;
+		const delay = options.delay ?? 50;
 		this.#callback = options.onPush;
-		if (options.delay !== undefined) delay = options.delay;
 		Timer.repeat( id => {
 			const value = Analog.read(config.buttonArray);
-			if (value > BUTTON_VALUES[0]){
-				if (this.#pushed === undefined) return;
-				this.#callback.call(this, this.#pushed, 0);
+			if (value > BUTTON_VALUES[0]) {
+				if (this.#pushed === undefined)
+					return;
+				this.#callback(this.#pushed, 0);
 				this.#pushed = undefined;
 			}
-			for (let i = 5; i >= 0; i--){
-				if (value < BUTTON_VALUES[i]){
+			for (let i = 5; i >= 0; i--) {
+				if (value < BUTTON_VALUES[i]) {
 					if (i !== this.#pushed){
-						if (this.#pushed !== undefined) this.#callback.call(this, this.#pushed, 0);
+						if (this.#pushed !== undefined)
+							this.#callback(this.#pushed, 0);
 						this.#pushed = i;
-						this.#callback.call(this, i, 1);
+						this.#callback(i, 1);
 					}
 					break;
 				}
@@ -37,7 +38,7 @@ class ButtonArray {
 globalThis.Host = Object.freeze({
 	ButtonArray,
 	NeoPixel: class {
-		constructor(options = {}){
+		constructor(options) {
 			return new NEOPIXEL({
 				...options,
 				length: 1, 
@@ -48,32 +49,33 @@ globalThis.Host = Object.freeze({
 	}
 }, true);
 
+const phases = Object.freeze([
+	//red, purple, blue, cyan, green, orange, white, black
+	[1, 0, -1, 0, 0, 1, 0, -1],
+	[0, 0, 0, 1, 0, 0, 0, -1],
+	[0, 1, 0, 0, -1, 0, 1, -1]
+], true);
+
 export default function (done) {
 	if (config.rainbow){
-		globalThis.neopixel = new NEOPIXEL({length: 1, pin: config.neopixel, order: "GRB"});
-		const np = globalThis.neopixel;
+		const np = new Host.NeoPixel;
+		globalThis.neopixel = np;
 		const STEP = 3;
 		
 		let rgb = [0, 0, 0];
 		let phase = 0;
-		let phases = [
-			//red, purple, blue, cyan, green, orange, white, black
-			[1, 0, -1, 0, 0, 1, 0, -1],
-			[0, 0, 0, 1, 0, 0, 0, -1],
-			[0, 1, 0, 0, -1, 0, 1, -1]
-		]
 
 		np.rainbowTimer = Timer.repeat(() => {
-			let advance = false;
-			for (let i = 0; i < 3; i++){
+			let advance;
+			for (let i = 0; i < 3; i++) {
 				const direction = phases[i][phase];
 				rgb[i] += direction * STEP;
-				if (direction){
+				if (direction) {
 					if (rgb[i] >= 255){
 						rgb[i] = 255;
 						advance = true;
 					}
-					if (rgb[i] <= 0){
+					else if (rgb[i] <= 0){
 						rgb[i] = 0;
 						advance = true;
 					}
@@ -82,11 +84,10 @@ export default function (done) {
 			if (advance)
 				if (++phase >= phases[0].length) phase = 0;
 	
-			np.setPixel(0, np.makeRGB(rgb[0], rgb[1], rgb[2]));
-			np.update();
+			neopixel.setPixel(0, neopixel.makeRGB(rgb[0], rgb[1], rgb[2]));
+			neopixel.update();
 		}, 33);
 	}
-
 
 	done();
 }
