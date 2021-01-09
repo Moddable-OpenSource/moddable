769e80867811a1daa06b466f436894455cd0e78e
Andy Carle <andy@moddable.tech>
2021-01-08 22:51:36 -0800

    Kaluga Host.Button rework

--- build/devices/esp32/targets/kaluga/setup-target.js
+++ build/devices/esp32/targets/kaluga/setup-target.js
@@ -7,33 +7,151 @@ const BUTTON_TOLERANCE = 10;
 const BUTTON_VALUES = Object.freeze([750 + BUTTON_TOLERANCE, 615 + BUTTON_TOLERANCE, 515 + BUTTON_TOLERANCE, 347 + BUTTON_TOLERANCE, 255 + BUTTON_TOLERANCE, 119 + BUTTON_TOLERANCE]);
 
 class ButtonArray {
-	#callback;
+	#buttons = {};
+	#delay;
 	#pushed;
-	constructor(options){
-		const delay = options.delay ?? 50;
-		this.#callback = options.onPush;
-		Timer.repeat( id => {
+	#timerID;
+
+	constructor() {
+		this.#delay = config.buttonArrayDelay ?? 50;
+	}
+
+	register(button, buttonNumber) {
+		if (this.#buttons[buttonNumber])
+			throw new Error(`Button ${buttonNumber} has already been created.`);
+
+		if (this.#timerID === undefined)
+			this.#startTimer();
+
+		this.#buttons[buttonNumber] = button;
+	}
+
+	unregister(buttonNumber) {
+		delete this.#buttons[buttonNumber];
+		if (Object.keys(this.#buttons).length == 0)
+			this.#stopTimer();
+	}
+
+	#startTimer() {
+		this.#timerID = Timer.repeat( () => {
 			const value = Analog.read(config.buttonArray);
 			if (value > BUTTON_VALUES[0]) {
 				if (this.#pushed === undefined)
 					return;
-				this.#callback(this.#pushed, 0);
+				if (this.#buttons[this.#pushed])
+					this.#buttons[this.#pushed].value = 0;
 				this.#pushed = undefined;
 			}
 			for (let i = 5; i >= 0; i--) {
 				if (value < BUTTON_VALUES[i]) {
-					if (i !== this.#pushed){
+					if (i !== this.#pushed) {
 						if (this.#pushed !== undefined)
-							this.#callback(this.#pushed, 0);
+							if (this.#buttons[this.#pushed])
+								this.#buttons[this.#pushed].value = 0;
 						this.#pushed = i;
-						this.#callback(i, 1);
+						if (this.#buttons[i])
+							this.#buttons[i].value = 1;
 					}
 					break;
 				}
 			}
-		}, delay);
+		}, this.#delay);
+	}
+
+	#stopTimer() {
+		Timer.clear(this.#timerID);
+		this.#timerID = undefined;
 	}
 }
+
+let buttonArray;
+class KalugaButton {
+	#buttonNumber;
+	#onPush;
+	#value = 0;
+	
+	constructor(options) {
+		this.#buttonNumber = options.buttonNumber;
+		this.#onPush = options.onPush;
+
+		if (undefined === buttonArray)
+			buttonArray = new ButtonArray();
+
+		buttonArray.register(this, options.buttonNumber);
+	}
+
+	set value(value) {
+		this.#value = value;
+		if (this.#onPush)
+			this.#onPush(value);
+	}
+
+	close() {
+		buttonArray.unregister(this.#buttonNumber);
+	}
+
+	read() {
+		return this.#value;
+	}
+
+	get pressed() {
+		return this.#value ? true : false;
+	}
+}
+
+class A {
+	constructor(options) {
+		return new KalugaButton({
+			...options,
+			buttonNumber: 0
+		});
+	}
+};
+
+class B {
+	constructor(options) {
+		return new KalugaButton({
+			...options,
+			buttonNumber: 1
+		});
+	}
+};
+
+class C {
+	constructor(options) {
+		return new KalugaButton({
+			...options,
+			buttonNumber: 2
+		});
+	}
+};
+
+class D {
+	constructor(options) {
+		return new KalugaButton({
+			...options,
+			buttonNumber: 3
+		});
+	}
+};
+
+class E {
+	constructor(options) {
+		return new KalugaButton({
+			...options,
+			buttonNumber: 4
+		});
+	}
+};
+
+class F {
+	constructor(options) {
+		return new KalugaButton({
+			...options,
+			buttonNumber: 5
+		});
+	}
+};
 
 class NeoPixelLED extends NeoPixel {
 	#value = 0;
@@ -59,7 +177,15 @@ class NeoPixelLED extends NeoPixel {
 }
 
 globalThis.Host = Object.freeze({
-	ButtonArray,
+	Button: {
+		Default: A,
+		A,
+		B,
+		C,
+		D,
+		E,
+		F
+	},
 	LED: {
 		Default: class {
 			constructor(options) {
@@ -95,11 +221,11 @@ export default function (done) {
 				const direction = phases[i][phase];
 				rgb[i] += direction * STEP;
 				if (direction) {
-					if (rgb[i] >= 255){
+					if (rgb[i] >= 255) {
 						rgb[i] = 255;
 						advance = true;
 					}
-					else if (rgb[i] <= 0){
+					else if (rgb[i] <= 0) {
 						rgb[i] = 0;
 						advance = true;
 					}
