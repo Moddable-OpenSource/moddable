2e89d3338cd75b78b5294f89db7c1a77d1ef7c3d
Brian Friedkin <brian@moddable.tech>
2021-01-18 12:03:17 -0800

    disable softdevice before wake on timer to eliminate unwanted  interrupts

--- build/devices/nrf52/modules/sleep/modSleep.c
+++ build/devices/nrf52/modules/sleep/modSleep.c
@@ -61,7 +61,6 @@ enum {
 static void getRAMSlaveAndSection(uint32_t address, uint32_t *slave, uint32_t *section);
 
 static void sleep_wake_on_timer();
-static void sleep_wake_on_timer_sd();
 
 // The gRamRetentionBuffer contains 32-bit retained value slots
 // When values are retained, the slot contents are as follows:
@@ -165,6 +164,10 @@ void xs_sleep_deep(xsMachine *the)
 		// Disable LFCLK started interrupt
 		nrf_clock_int_disable(CLOCK_INTENSET_LFCLKSTARTED_Msk);
                           
+		// Disable SoftDevice
+		if (nrf52_softdevice_enabled())
+			nrf_sdh_disable_request();
+			
 		// Stop the RTC and clear the RTC counter
 		nrf_rtc_task_trigger(portNRF_RTC_REG, NRF_RTC_TASK_STOP);
 		nrf_rtc_task_trigger(portNRF_RTC_REG, NRF_RTC_TASK_CLEAR);
@@ -189,11 +192,8 @@ void xs_sleep_deep(xsMachine *the)
 		// Ensure memory access has completed
 		__DSB();
 
-		// Complete process depending on whether or not the SoftDevice is enabled
-		if (nrf52_softdevice_enabled())
-			sleep_wake_on_timer_sd();
-		else
-			sleep_wake_on_timer();		
+		// Wake on timer
+		sleep_wake_on_timer();		
 	}
 	else {
 		// System OFF sleep, wake on reset or preconfigured analog/digital wake-up trigger
@@ -343,65 +343,6 @@ void sleep_wake_on_timer()
 	// Reset device
 	*((uint32_t*)DFU_DBL_RESET_MEM) = 0;
 	NVIC_SystemReset();
-}
-
-void sleep_wake_on_timer_sd()
-{
-	// Power off all ram sections except for retention memory (slave 2, section 0)
-	sd_power_ram_power_clr(0, 0x03);
-	sd_power_ram_power_clr(1, 0x03);
-	sd_power_ram_power_clr(2, 0x02);
-	sd_power_ram_power_clr(3, 0x03);
-	sd_power_ram_power_clr(4, 0x03);
-	sd_power_ram_power_clr(5, 0x03);
-	sd_power_ram_power_clr(6, 0x03);
-	sd_power_ram_power_clr(7, 0x03);
-	sd_power_ram_power_clr(8, 0x3F);
-
-	// Wait for RTC compare or configured digital/analog interrupt sources
-	do {
-		__WFE();
-	} while (0 == (NVIC->ISPR[0] | NVIC->ISPR[1]));
-
-	// Read RTC again and update saved time of day
-	((c_timeval *)MOD_TIME_RESTORE_MEM)->tv_sec += ((portNRF_RTC_REG->COUNTER * 1000) >> 10) / 1000;
-	((uint32_t *)MOD_TIME_RESTORE_MEM)[2] = kRamRetentionValueMagic;
-
-    // If no RTC event pending, check for wake-up from configured digital/analog interrupt sources
-    if (!nrf_rtc_event_pending(portNRF_RTC_REG, NRF_RTC_EVENT_COMPARE_0)) {
-    	if (0 != NRF_P0->LATCH) {
-			((uint32_t *)MOD_WAKEUP_REASON_MEM)[0] = MOD_GPIO_WAKE_MAGIC;
-			((uint32_t *)MOD_WAKEUP_REASON_MEM)[1] = NRF_P0->LATCH;
-			NRF_P0->LATCH = 0xFFFFFFFF;	// clear
-    	}
-    	else if (nrf_lpcomp_event_check(NRF_LPCOMP_EVENT_UP) && nrf_lpcomp_int_enable_check(LPCOMP_INTENSET_UP_Msk)) {
-			((uint32_t *)MOD_WAKEUP_REASON_MEM)[0] = MOD_ANALOG_WAKE_MAGIC;
-			NRF_LPCOMP->ENABLE = LPCOMP_ENABLE_ENABLE_Disabled << LPCOMP_ENABLE_ENABLE_Pos;
-    	}
-    	else if (nrf_lpcomp_event_check(NRF_LPCOMP_EVENT_DOWN) && nrf_lpcomp_int_enable_check(LPCOMP_INTENSET_DOWN_Msk)) {
-			((uint32_t *)MOD_WAKEUP_REASON_MEM)[0] = MOD_ANALOG_WAKE_MAGIC;
-			NRF_LPCOMP->ENABLE = LPCOMP_ENABLE_ENABLE_Disabled << LPCOMP_ENABLE_ENABLE_Pos;
-    	}
-    	else if (nrf_lpcomp_event_check(NRF_LPCOMP_EVENT_CROSS) && nrf_lpcomp_int_enable_check(LPCOMP_INTENSET_CROSS_Msk)) {
-			((uint32_t *)MOD_WAKEUP_REASON_MEM)[0] = MOD_ANALOG_WAKE_MAGIC;
-			NRF_LPCOMP->ENABLE = LPCOMP_ENABLE_ENABLE_Disabled << LPCOMP_ENABLE_ENABLE_Pos;
-    	}
-    }
-
-	// Power on all RAM (seems to be required in order for reset to work)
-	sd_power_ram_power_set(0, 0x03);
-	sd_power_ram_power_set(1, 0x03);
-	sd_power_ram_power_set(2, 0x02);
-	sd_power_ram_power_set(3, 0x03);
-	sd_power_ram_power_set(4, 0x03);
-	sd_power_ram_power_set(5, 0x03);
-	sd_power_ram_power_set(6, 0x03);
-	sd_power_ram_power_set(7, 0x03);
-	sd_power_ram_power_set(8, 0x3F);
-
-	// Reset device
-	*((uint32_t*)DFU_DBL_RESET_MEM) = 0;
-	sd_nvic_SystemReset();
 }
 
 void getRAMSlaveAndSection(uint32_t address, uint32_t *slave, uint32_t *section)
