4ae91266ad4a9a4370e335d3904834ccccc3c5dc
Andy Carle <andy@moddable.tech>
2021-04-22 21:33:58 -0700

    esp32: add optional manifest define to set PWM frequency

--- modules/pins/pwm/esp32/pwm.c
+++ modules/pins/pwm/esp32/pwm.c
@@ -40,9 +40,13 @@
 	#define ESP_SPEED_MODE LEDC_HIGH_SPEED_MODE
 #endif
 
+#ifndef MODDEF_PWM_LEDC_FREQUENCY
+	#define MODDEF_PWM_LEDC_FREQUENCY 1024
+#endif
+
 static const ledc_timer_config_t gTimer = {
 	.duty_resolution = LEDC_TIMER_10_BIT,
-	.freq_hz = 1024,
+	.freq_hz = MODDEF_PWM_LEDC_FREQUENCY,
 	.speed_mode = ESP_SPEED_MODE,
 	.timer_num = MODDEF_PWM_LEDC_TIMER
 };
