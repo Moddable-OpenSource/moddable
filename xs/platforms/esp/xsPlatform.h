14a67a2b98192acce20ab7ad4d9ce6526b1bb32e
Andy Carle <andy@moddable.tech>
2021-06-02 18:45:24 -0600

    add C_PATH_MAX to ESP target

--- xs/platforms/esp/xsPlatform.h
+++ xs/platforms/esp/xsPlatform.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017  Moddable Tech, Inc.
+ * Copyright (c) 2016-2021 Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK Runtime.
  * 
@@ -115,6 +115,7 @@ extern "C" {
 	#undef PATH_MAX
 #endif
 #define PATH_MAX (256)
+#define C_PATH_MAX PATH_MAX
 
 #define mxGetKeySlotID(SLOT) (SLOT)->ID
 #define mxGetKeySlotKind(SLOT) (SLOT)->kind
