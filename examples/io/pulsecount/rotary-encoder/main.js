ce245fd3fe096cb2ef5e1b374ec5ac4416a31292
Andy Carle <andy@moddable.tech>
2021-06-11 15:10:56 -0700

    io pulsecount rotary encoder example

---
+++ examples/io/pulsecount/rotary-encoder/main.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ *
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <https://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+
+const encoder = new device.io.PulseCount({
+   signal: 13,
+   control: 14,
+   onReadable() {
+      const count = this.read();
+      trace(`count: ${count}\n`);
+   }
+});
