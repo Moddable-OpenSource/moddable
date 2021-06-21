fadd857a231500b62cb5084596118d61d4ae21cf
Patrick Soquet <ps@moddable.tech>
2021-06-21 07:55:04 +0200

    XS: safer capture error stack

--- xs/sources/xsError.c
+++ xs/sources/xsError.c
@@ -138,13 +138,13 @@ void fxCaptureErrorStack(txMachine* the, txSlot* internal, txSlot* frame)
 				slot->ID = environment->ID;
 				slot->value.key.string = C_NULL;
 				slot->value.key.sum = environment->value.environment.line;
-				if ((name->kind == XS_STRING_KIND) || (name->kind == XS_STRING_X_KIND)) {
+				if (name && ((name->kind == XS_STRING_KIND) || (name->kind == XS_STRING_X_KIND))) {
 					if (name->kind == XS_STRING_X_KIND)
 						slot->kind = XS_KEY_X_KIND;
 					slot->value.key.string = name->value.string;
 				}
 				else {
-					txSlot* code = mxFunctionInstanceCode(function->value.reference);
+					txSlot* code = mxFunctionInstanceCode(function);
 					if (code->ID != XS_NO_ID) {
 						txSlot* key = fxGetKey(the, code->ID);
 						if (key) {
