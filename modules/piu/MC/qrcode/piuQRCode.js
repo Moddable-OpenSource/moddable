7e336db567f2b2519658bb4eda6cd2a9f5c43f01
Patrick Soquet <ps@moddable.tech>
2021-07-13 10:56:20 +0200

    Piu QR Code string accessor

--- modules/piu/MC/qrcode/piuQRCode.js
+++ modules/piu/MC/qrcode/piuQRCode.js
@@ -4,6 +4,8 @@ const qrCode = {
 	__proto__: Content.prototype,
 	_create($, it) @ "PiuQRCode_create",
 	QRCodeBuffer,
+	get string() @ "PiuQRCode_get_string",
+	set string(it) @ "PiuQRCode_set_string",
 };
 export const QRCode = Template(qrCode);
 Object.freeze(qrCode);
