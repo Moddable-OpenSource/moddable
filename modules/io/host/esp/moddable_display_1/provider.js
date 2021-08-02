8fefee0dafceb53fe79bc48237266b8380d15d0d
Michael Kellner <mkellner@moddable.com>
2021-08-02 10:41:53 -0700

    driverRotation for display

--- modules/io/host/esp/moddable_display_1/provider.js
+++ modules/io/host/esp/moddable_display_1/provider.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020  Moddable Tech, Inc.
+ * Copyright (c) 2021  Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK Runtime.
  *
@@ -18,31 +18,49 @@
  *
  */
 
-import Button from "button";
-import LED from "led";
+import Analog from "embedded:io/analog";
+import Digital from "embedded:io/digital";
+import DigitalBank from "embedded:io/digitalbank";
+import I2C from "embedded:io/i2c";
+import PWM from "embedded:io/pwm";
+import Serial from "embedded:io/serial";
+import SMBus from "embedded:io/smbus";
+import SPI from "embedded:io/spi";
 
-globalThis.Host = {
-	LED: {
-		Default: class {
-			constructor(options) {
-				return new LED({
-					...options,
-					pin: 2,
-					invert: true
-				});
-			}
+const device = {
+	I2C: {
+		default: {
+			io: I2C,
+			data: 5,
+			clock: 4
 		}
 	},
-	Button: {
-		Default: class {
-			constructor(options) {
-				return new Button({
-					...options,
-					pin: 0,
-					invert: true
-				});
-			}
+	Serial: {
+		default: {
+			io: Serial,
 		}
+	},
+	SPI: {
+		default: {
+			io: SPI,
+			clock: 14,
+			in: 13,
+			out: 12,
+			port: "HSPI"
+		}
+	},
+	Analog: {
+		default: {
+			io: Analog
+		}
+	},
+	io: {Analog, Digital, DigitalBank, I2C, PWM, Serial, SMBus, SPI},
+	pins: {
+		button: 0,
+		led: 2,
+		displayDC: 2,
+		displaySelect: 15
 	}
 };
-Object.freeze(Host, true);
+
+export default device;
