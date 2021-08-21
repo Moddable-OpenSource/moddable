02a17c7e70a5f6e5f9355a86835ef1803fa1f49f
Peter Hoddie <peter@moddable.tech>
2021-08-20 21:47:39 -0700

    more ethernet support in getNIF

--- modules/network/net/esp/modNet.c
+++ modules/network/net/esp/modNet.c
@@ -49,20 +49,22 @@ getNIF(xsMachine *the)
 	uint8_t wantsAP = 0, wantsStation = 0;
 
 	if (xsToInteger(xsArgc) > 1) {
-		wantsAP = 0 == c_strcmp(xsToString(xsArg(1)), "ap");
-		wantsStation = 0 == c_strcmp(xsToString(xsArg(1)), "station");
+		const char *nif = xsToString(xsArg(1));
+		wantsAP = 0 == c_strcmp(nif, "ap");
+		wantsStation = 0 == c_strcmp(nif, "station");
 #if ESP32
-		if (0 == c_strcmp(xsToString(xsArg(1)), "ethernet"))
+		if (0 == c_strcmp(nif, "ethernet"))
 			return TCPIP_ADAPTER_IF_ETH;
-		if (!wantsAP && !wantsStation) {	// for multihomed configurations: if argument is IP address, find adapter that matches.
+		if (!wantsAP && !wantsStation) {	// if argument is IP address, find adapter that matches
 			ip_addr_t dst;
-			if (ipaddr_aton(xsToString(xsArg(1)), &dst)) {
+			if (ipaddr_aton(nif, &dst)) {
+				uint8_t ifc;
 				dst.u_addr.ip4.addr &= 0x00ffffff;		//@@ this only works for IPv4
-				tcpip_adapter_ip_info_t infoSTA = {0}, infoAP = {0};
-				if ((ESP_OK == tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_STA, &infoSTA)) && ((infoSTA.ip.addr & 0x00ffffff) == dst.u_addr.ip4.addr))
-					return TCPIP_ADAPTER_IF_STA;
-				if ((ESP_OK == tcpip_adapter_get_ip_info(TCPIP_ADAPTER_IF_AP, &infoAP)) && ((infoAP.ip.addr & 0x00ffffff) == dst.u_addr.ip4.addr))
-					return TCPIP_ADAPTER_IF_AP;
+				for (ifc = 0; ifc <= TCPIP_ADAPTER_IF_MAX; ifc++) {
+					tcpip_adapter_ip_info_t info = {0};
+					if ((ESP_OK == tcpip_adapter_get_ip_info(ifc, &info)) && ((info.ip.addr & 0x00ffffff) == dst.u_addr.ip4.addr))
+						return ifc;
+				}
 			}
 		}
 #endif
