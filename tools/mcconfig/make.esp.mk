7f05cb4409aab7af4b026ae3fd9bf6fcbaf2e87a
Peter Hoddie <peter@moddable.tech>
2021-12-15 10:32:32 -0800

    fix default on macOS Monterey

--- tools/mcconfig/make.esp.mk
+++ tools/mcconfig/make.esp.mk
@@ -33,7 +33,9 @@ PLATFORM_DIR = $(MODDABLE)/build/devices/esp
 UPLOAD_SPEED ?= 921600
 DEBUGGER_SPEED ?= 921600
 ifeq ($(HOST_OS),Darwin)
-	ifeq ($(findstring _11.,_$(shell sw_vers -productVersion)),_11.)
+	ifeq ($(findstring _12.,_$(shell sw_vers -productVersion)),_12.)
+		UPLOAD_PORT ?= /dev/cu.usbserial-0001
+	else ifeq ($(findstring _11.,_$(shell sw_vers -productVersion)),_11.)
 		UPLOAD_PORT ?= /dev/cu.usbserial-0001
 	else
 		UPLOAD_PORT ?= /dev/cu.SLAB_USBtoUART
--- tools/mcrun/make.esp.mk
+++ tools/mcrun/make.esp.mk
@@ -19,7 +19,9 @@
 
 HOST_OS := $(shell uname)
 ifeq ($(HOST_OS),Darwin)
-	ifeq ($(findstring _11.,_$(shell sw_vers -productVersion)),_11.)
+	ifeq ($(findstring _12.,_$(shell sw_vers -productVersion)),_12.)
+		UPLOAD_PORT ?= /dev/cu.usbserial-0001
+	else ifeq ($(findstring _11.,_$(shell sw_vers -productVersion)),_11.)
 		UPLOAD_PORT ?= /dev/cu.usbserial-0001
 	else
 		UPLOAD_PORT ?= /dev/cu.SLAB_USBtoUART
