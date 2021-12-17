456f0b1df1b8101543e33d6094b450c6b67acf27
Peter Hoddie <peter@moddable.tech>
2021-12-17 10:54:51 -0800

    fix obscure memory moving bug in pocoGetBitmapPixels

--- modules/commodetto/commodettoPoco.c
+++ modules/commodetto/commodettoPoco.c
@@ -897,14 +897,16 @@ PocoPixel *pocoGetBitmapPixels(xsMachine *the, Poco poco, CommodettoBitmap cb, i
 	xsSlot buffer;
 	void *data;
 	xsUnsignedValue dataSize;
+	int32_t offset;
 
 	if (cb->havePointer)
 		return (PocoPixel *)cb->bits.data;
 	
+	offset = cb->bits.offset;
 	xsmcGet(buffer, xsArg(arg), xsID_buffer);
 	xsmcGetBufferReadable(buffer, &data, &dataSize);
 	PocoDisableGC(poco);
-	return (PocoPixel *)(cb->bits.offset + (char *)data);
+	return (PocoPixel *)(offset + (char *)data);
 }
 
 void xs_rectangle_get_x(xsMachine *the)
