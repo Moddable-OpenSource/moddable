a07b7afae06dc9dac6bbc1f2404f67a5ad6f356c
Patrick Soquet <ps@moddable.tech>
2022-01-03 10:21:22 +0100

    module modules

--- modules/base/modules/modules.c
+++ modules/base/modules/modules.c
@@ -6,23 +6,16 @@ extern void* xsPreparationAndCreation(xsCreation **creation);
 void xs_modules_host(xsMachine *the)
 {
 	txPreparation *preparation = xsPreparationAndCreation(NULL);
-	txInteger scriptCount = preparation->scriptCount, i = 0;
-	txScript* script = preparation->scripts;
-
 	xsResult = xsNewArray(0);
-
-	while (scriptCount--) {
-		char path[C_PATH_MAX];
-		char *dot;
-
-		c_strcpy(path, script->path);
-		dot = c_strchr(path, '.');
-		if (dot)
-			*dot = 0;
-
-		xsSetIndex(xsResult, i++, xsString(path));
-
-		script++;
+	if (preparation) {
+		txInteger scriptCount = preparation->scriptCount, i = 0;
+		txScript* script = preparation->scripts;
+		while (scriptCount--) {
+			char path[C_PATH_MAX];
+			c_strcpy(path, script->path);
+			xsSetIndex(xsResult, i++, xsString(path));
+			script++;
+		}
 	}
 }
 
@@ -30,40 +23,25 @@ void xs_modules_host(xsMachine *the)
 
 void xs_modules_archive(xsMachine *the)
 {
-	uint8_t *p, *q;
-	int i = 0;
-	char path[128];
-	txPreparation *preparation;
-
+	uint8_t *p = the->archive;
 	xsResult = xsNewArray(0);
-
-	preparation = xsPreparationAndCreation(NULL);
-	if (!preparation)
-		return;
-	c_memcpy(path, preparation->base, preparation->baseLength);
-
-	p = the->archive;
-	if (!p) return;
-
-	p += mxArchiveHeaderSize;
-	// NAME
-	p += c_read32be(p);
-	// SYMB
-	p += c_read32be(p);
-	// MODS
-	q = p + c_read32be(p);
-	p += sizeof(Atom);
-
-	while (p < q) {
-		int atomSize = c_read32be(p);
-
-		c_strcpy(path + preparation->baseLength, (txString)(p + sizeof(Atom)));
-		path[atomSize - sizeof(Atom) - 4] = 0;
-
-		xsSetIndex(xsResult, i++, xsString(path + preparation->baseLength));
-
-		p += atomSize;
+	if (p) {
+		uint8_t *q;
+		int i = 0;
+		p += mxArchiveHeaderSize;
+		// NAME
 		p += c_read32be(p);
+		// SYMB
+		p += c_read32be(p);
+		// MODS
+		q = p + c_read32be(p);
+		p += sizeof(Atom);
+		while (p < q) {
+			int atomSize = c_read32be(p);
+			xsSetIndex(xsResult, i++, xsString((txString)(p + sizeof(Atom))));
+			p += atomSize;
+			p += c_read32be(p);
+		}
 	}
 }
 
