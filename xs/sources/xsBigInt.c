5129440b146b47f0bce7dcdb62ea35735ac60520
Patrick Soquet <ps@moddable.tech>
2022-01-25 17:16:00 +0100

    XS: #797

--- xs/sources/xsBigInt.c
+++ xs/sources/xsBigInt.c
@@ -1570,9 +1570,11 @@ txBigInt *fxBigInt_exp(txMachine* the, txBigInt *r, txBigInt *a, txBigInt *b)
 		txU4 c = fxBigInt_bitsize(a);
 		txBigInt *t = fxBigInt_umul1(the, NULL, b, c);
 		t = fxBigInt_ulsr1(the, t, t, 5);
+#ifdef mxRun
 		if ((t->size > 1) || (t->data[0] > 0xFFFF))
-			fxAbort(the, XS_NOT_ENOUGH_MEMORY_EXIT);
-		c = 2 + t->data[0]; //@@
+			mxRangeError("too big exponent");
+#endif
+		c = 2 + t->data[0];
 		fxBigInt_free(the, t);
         if (r == NULL)
 			r = fxBigInt_alloc(the, c);
