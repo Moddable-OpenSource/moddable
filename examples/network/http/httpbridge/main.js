e0e909db13f4aec3c7c291ceee42f292a286afd3
wilberforce <wilber@wilberforce.co.nz>
2022-02-09 13:37:25 +1300

    Add App with methods mapping ws actions

--- examples/network/http/httpbridge/main.js
+++ examples/network/http/httpbridge/main.js
@@ -1,71 +1,71 @@
 import { WebServer as HTTPServer } from "bridge/webserver";
 import { BridgeWebsocket } from "bridge/websocket";
-import { BridgeHttpZip  } from "bridge/httpzip";
+import { BridgeHttpZip } from "bridge/httpzip";
 
 const http = new HTTPServer({
-	port: 80
+  port: 80,
 });
 
-let ws = new BridgeWebsocket('/api');
-//let ws = new WebSocketServer('/api');
-http.use( ws );
-http.use( new BridgeHttpZip('site.zip'));
+let ws = new BridgeWebsocket("/api");
+http.use(ws);
+http.use(new BridgeHttpZip("site.zip"));
 
 import { _model } from "model";
 
 let observer = {
-	set: function (obj, prop, value) {
-	  trace(`set prop: ${prop} ${ JSON.stringify(value)}\n`);
-	  obj[prop] = value;
-	  return true;
-	}
-  };
+  set: function (obj, prop, value) {
+    trace(`set prop: ${prop} ${JSON.stringify(value)}\n`);
+    obj[prop] = value;
+    return true;
+  },
+};
 
-  /*
-  class thingy {
-	minus() {
-		trace(`minus\n`);
-	}
+class App {
+  minus(value) {
+    model.satisfaction = Math.max(0, model.satisfaction - 1);
+    return model;
   }
-  */
-  
-const model = new Proxy(_model, observer);
-
-ws.callback = function cb(websock,message, value) {
-	switch (message) {		
-		case BridgeWebsocket.connect:
-			break;
-
-		case BridgeWebsocket.handshake:
-			websock.broadcast( model )
-			break;			
-
-		case BridgeWebsocket.receive:
-			try {
-				trace(`Main WebSocket receive: ${value}\n`);
-				value = JSON.parse(value);
-				
-				if ( value?.action === 'shutdown'){
-					ws.close();
-					http.close();
-				}
-				if ( value?.action === 'minus'){
-					model.satisfaction=Math.max(0,model.satisfaction-1)
-					value=model;
-					model.minus();
-				}				
-				if ( value?.action === 'plus'){
-					model.satisfaction=Math.min(10,model.satisfaction+1)
-					value=model;
-				}
-				if ( value.hasOwnProperty('language') ) {
-					model.language = value.language;
-				}
-
-				websock.broadcast(value);
-			}
-			catch (e) {
-				trace(`WebSocket parse received data error: ${e}\n`);
-			}
-	}
+  plus(value) {
+    model.satisfaction = Math.min(10, model.satisfaction + 1);
+    return model;
+  }
+  shutdown() {
+    ws.close();
+    http.close();
+  }
+  language() {
+    model.language = value.language;
+    return model;
+  }
 }
+
+const app = new App();
+
+let model = { ..._model };
+
+ws.callback = function cb(websock, message, value) {
+  switch (message) {
+    case BridgeWebsocket.connect:
+      break;
+
+    case BridgeWebsocket.handshake:
+      websock.broadcast(model);
+      break;
+
+    case BridgeWebsocket.receive:
+      try {
+        trace(`Main WebSocket receive: ${value}\n`);
+        value = JSON.parse(value);
+
+        let action = value?.action;
+
+        if (typeof app[action] === "function") {
+          value = app[action](value);
+        }
+
+        websock.broadcast(value);
+      } catch (e) {
+        trace(`WebSocket parse received data error: ${e}\n`);
+      }
+  }
+};
