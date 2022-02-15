d74db728fcb257d25d47a16ebeda07083e044b72
Patrick Soquet <ps@moddable.tech>
2022-02-15 10:08:58 +0100

    figures: use modules

--- examples/piu/outline/figures/main.js
+++ examples/piu/outline/figures/main.js
@@ -1,6 +1,7 @@
+import Modules from "modules";
+import {Outline} from "commodetto/outline";
 import {} from "piu/MC";
 import {} from "piu/shape";
-import {Outline} from "commodetto/outline";
 
 const shapeSkin = new Skin({ fill:rgba(255, 255, 255, 0.5), stroke:"white" });
 
@@ -9,20 +10,21 @@ let ShapeApplication = Application.template($ => ({
 	style:new Style({ font:"semibold 18px Open Sans", color:"white", horizontal:"center" }),
 	contents: [
 		Shape($, { left:0, right:0, top:0, bottom:0, skin:shapeSkin } ),
-		Label($, { left:0, right:0, bottom:0, height:40, string:"" } ),
+		Label($, { left:0, right:0, bottom:0, height:40, string:"no mods" } ),
 	],
 	behavior: {
-		async addMod(application) {
-			try {
-				let module = await import("mod");
-				let Behavior = module.default;
-				let shape = application.first;
-				shape.behavior = new Behavior();
-				shape.behavior.onCreate(shape);
-			}
-			catch(e) {
-				trace(`${e.stack}\n`);
-				application.last.string = "no mods";
+		addMod(application) {
+			if (Modules.has("mod")) {
+				try {
+					let Behavior = Modules.importNow("mod");
+					let shape = application.first;
+					shape.behavior = new Behavior();
+					shape.behavior.onCreate(shape);
+				}
+				catch(e) {
+					trace(`${e.stack}\n`);
+					application.last.string = "no mods";
+				}
 			}
 		},
 		onCreate(application) {
