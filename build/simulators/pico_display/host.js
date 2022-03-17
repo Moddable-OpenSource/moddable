39166550065774843c19943fe4fc4ac1423c70ba
Peter Hoddie <peter@moddable.tech>
2022-03-17 14:55:10 -0700

    pico display simualtor

---
+++ build/simulators/pico_display/host.js
@@ -0,0 +1,55 @@
+import Button from "button";
+import Control from "control";
+
+class A {
+	constructor(options) {
+		return new Button({
+			...options,
+			buttonKey: 'aButton',
+		});
+	}
+}
+class B {
+	constructor(options) {
+		return new Button({
+			...options,
+			buttonKey: 'bButton',
+		});
+	}
+}
+class X {
+	constructor(options) {
+		return new Button({
+			...options,
+			buttonKey: 'xButton',
+		});
+	}
+}
+class Y {
+	constructor(options) {
+		return new Button({
+			...options,
+			buttonKey: 'yButton',
+		});
+	}
+}
+
+class RGBLED extends Control {
+	close() {
+		this.postJSON({led: {red: 0, green: 0, blue: 0}});
+	}
+	write(led) {
+		this.postJSON({led});
+	}
+}
+
+globalThis.Host = Object.freeze({
+	Button: {
+		Default: A,
+		A,
+		B,
+		X,
+		Y
+	},
+	LED: { Default: RGBLED },
+}, true);
