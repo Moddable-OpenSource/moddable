86a934ff7182196bf8dac21a2d536154b7542fc6
Patrick Soquet <ps@moddable.tech>
2022-03-22 08:43:23 +0100

    XS: remapping mods

--- examples/js/mods/test/mod.js
+++ examples/js/mods/test/mod.js
@@ -1,7 +1,10 @@
+import Resource from "Resource";
+
 export default function(x, y) {
 	let a = new Array(2);
 	a[0] = x;
 	a[1] = y;
+	let r = new Resource("data.txt", import.meta.archive);
 	debugger
 	return a;
 }
