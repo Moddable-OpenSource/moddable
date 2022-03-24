42b5ac2cc3babbc584c398af3e9262d17142501c
Patrick Soquet <ps@moddable.tech>
2022-03-24 18:31:14 +0100

    XS: remapping mods

--- examples/js/mods/ArchiveFileMapping.js
+++
@@ -1,5 +0,0 @@
-export default class ArchiveFileMapping @ "ArchiveFileMappingDelete" {
-	constructor(path) @ "ArchiveFileMappingCreate"
-	close()  @ "ArchiveFileMapping_close"
-	get archive() @ "ArchiveFileMapping_get_archive"
-}
