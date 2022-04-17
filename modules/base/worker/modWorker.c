1323f7279aa928c647ecf2bd0c8fa8bef0795705
Michael Kellner <mkellner@intlweb.com>
2022-04-17 12:36:05 -0700

    reduce size of worker stack

--- modules/base/worker/modWorker.c
+++ modules/base/worker/modWorker.c
@@ -221,7 +221,7 @@ static void workerConstructor(xsMachine *the, xsBooleanValue shared)
 	xTaskCreate(workerLoop, worker->module, kStack, worker, 10, &worker->task);
 #elif nrf52
 	#define kWorkerPriority 6	// was 1
-	#define kStack ((10 * 1024) / sizeof(StackType_t))
+	#define kStack ((7 * 1024) / sizeof(StackType_t))
 
 	xTaskCreate(workerLoop, worker->module, kStack, worker, kWorkerPriority, &worker->task);
 #endif
