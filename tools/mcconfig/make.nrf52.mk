9e6dbe8ab8e3f84cb41f74d915400134792ef810
Michael Kellner <mkellner@intlweb.com>
2022-04-21 13:19:23 -0700

    use v8 gcc

--- tools/mcconfig/make.nrf52.mk
+++ tools/mcconfig/make.nrf52.mk
@@ -95,10 +95,10 @@ else
 	endif
 endif
 
-# NRF52_GNU_VERSION ?= 8.2.1
-# NRF52_GCC_ROOT ?= $(NRF_ROOT)/gcc-arm-none-eabi-8-2018-q4-major
-NRF52_GNU_VERSION ?= 7.2.1
-NRF52_GCC_ROOT ?= $(NRF_ROOT)/gcc-arm-none-eabi-7-2017-q4-major
+NRF52_GNU_VERSION ?= 8.2.1
+NRF52_GCC_ROOT ?= $(NRF_ROOT)/gcc-arm-none-eabi-8-2018-q4-major
+# NRF52_GNU_VERSION ?= 7.2.1
+# NRF52_GCC_ROOT ?= $(NRF_ROOT)/gcc-arm-none-eabi-7-2017-q4-major
 
 NRFJPROG ?= $(NRF_ROOT)/nrfjprog/nrfjprog
 UF2CONV ?= $(NRF_ROOT)/uf2conv.py
