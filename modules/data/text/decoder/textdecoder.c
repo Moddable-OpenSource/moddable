e9ce0ea516dae3b38b206bf287e62c2e64255596
Peter Hoddie <peter@moddable.tech>
2022-04-28 13:02:39 -0700

    c_memset

--- modules/data/text/decoder/textdecoder.c
+++ modules/data/text/decoder/textdecoder.c
@@ -58,7 +58,7 @@ void xs_textdecoder(xsMachine *the)
 	xsResult = xsNewHostInstance(xsResult);
 	xsThis = xsResult;
 	xsmcSetHostDestructor(xsThis, NULL);
-	memset(&decoder, 0, sizeof(decoder));
+	c_memset(&decoder, 0, sizeof(decoder));
 #endif
 
 	decoder.ignoreBOM = false;
