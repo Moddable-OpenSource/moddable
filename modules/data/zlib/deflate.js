d8997e2f15f3e52f9695468520690c8dc8711e1c
Peter Hoddie <peter@moddable.tech>
2022-05-30 09:45:48 -0700

    allow onData and onEnd to be set, clean-up

--- modules/data/zlib/deflate.js
+++ modules/data/zlib/deflate.js
@@ -25,35 +25,39 @@ class Deflate @ "xs_deflate_destructor" {
 
 	constructor(options = {}) {
 		build.call(this, options);
+		this.onData = onData;
+		this.onEnd = onEnd;
 	}
 	close() @ "xs_deflate_close";
 
 	push(buffer, end) @ "xs_deflate_push";
-	onData(chunk) {
-		this.chunks.push(chunk);
-	}
-	onEnd(err) {
-		this.err = err;
-		if (err) {
-			delete this.chunks;
-			return;
-		}
-
-		// join chunks
-		let total = 0;
-		for (let i = 0, chunks = this.chunks, length = chunks.length; i < length; i++)
-			total += chunks[i].byteLength;
-
-		this.result = new Uint8Array(total);
-		let offset = 0;
-		for (let i = 0, chunks = this.chunks, length = chunks.length; i < length; i++) {
-			this.result.set(new Uint8Array(this.chunks[i]), offset);
-			offset += chunks[i].byteLength;
-		}
-		delete this.chunks;
-	}
 }
 
 function build(options) @ "xs_deflate";
+
+function onData(chunk) {
+	this.chunks.push(chunk);
+}
+
+function onEnd(err) {
+	this.err = err;
+	if (err) {
+		delete this.chunks;
+		return;
+	}
+
+	// join chunks
+	let total = 0;
+	for (let i = 0, chunks = this.chunks, length = chunks.length; i < length; i++)
+		total += chunks[i].byteLength;
+
+	this.result = new Uint8Array(total);
+	let offset = 0;
+	for (let i = 0, chunks = this.chunks, length = chunks.length; i < length; i++) {
+		this.result.set(new Uint8Array(this.chunks[i]), offset);
+		offset += chunks[i].byteLength;
+	}
+	delete this.chunks;
+}
 
 export default Deflate;
--- modules/data/zlib/inflate.js
+++ modules/data/zlib/inflate.js
@@ -18,46 +18,48 @@
  *
  */
  
-
 class Inflate @ "xs_inflate_destructor" {
 	chunks = [];
 	strm = {};
 
 	constructor(options = {}) {
-		this.build(options);
+		build.call(this, options);
+		this.onData = onData;
+		this.onEnd = onEnd;
 	}
-	build(options) @ "xs_inflate";
 	close() @ "xs_inflate_close";
 
 	push(buffer, end) {
-		this.strm.avail_in = this._push(buffer, end);
+		this.strm.avail_in = push.call(this, buffer, end);
 	}
+}
 
-	_push(buffer, end) @ "xs_inflate_push";
+function build(options) @ "xs_inflate";
+function push(buffer, end) @ "xs_inflate_push";
 
-	onData(chunk) {
-		this.chunks.push(chunk);
-	}
-	onEnd(err) {
-		const chunks = this.chunks, length = chunks.length;
-		delete this.chunks;
+function onData(chunk) {
+	this.chunks.push(chunk);
+}
 
-		this.err = err;
-		if (err)
-			return;
+function onEnd(err) {
+	const chunks = this.chunks, length = chunks.length;
+	delete this.chunks;
 
-		// join chunks
-		let total = 0;
-		for (let i = 0; i < length; i++)
-			total += chunks[i].byteLength;
+	this.err = err;
+	if (err)
+		return;
 
-		this.result = new Uint8Array(total);
-		let offset = 0;
-		for (let i = 0; i < length; i++) {
-			const chunk = chunks[i];
-			this.result.set(new Uint8Array(chunk), offset);
-			offset += chunk.byteLength;
-		}
+	// join chunks
+	let total = 0;
+	for (let i = 0; i < length; i++)
+		total += chunks[i].byteLength;
+
+	this.result = new Uint8Array(total);
+	let offset = 0;
+	for (let i = 0; i < length; i++) {
+		const chunk = chunks[i];
+		this.result.set(new Uint8Array(chunk), offset);
+		offset += chunk.byteLength;
 	}
 }
 
