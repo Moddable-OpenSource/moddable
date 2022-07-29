871091839e8394418393402ab9c89ca8fff458d8
Peter Hoddie <peter@moddable.tech>
2022-07-28 17:05:28 -0700

    flash partition byteLength does not have to be power of two

--- tests/modules/files/flash/byteLength.js
+++ tests/modules/files/flash/byteLength.js
@@ -9,7 +9,6 @@ let f = new Flash($TESTMC.config.flashParition);
 let byteLength = f.byteLength;
 assert.sameValue("number", typeof byteLength, "byteLength is number");
 assert(byteLength > 0, "byteLength is positive");
-assert.sameValue(0, byteLength & (byteLength - 1), "byteLength is power of 2");
 assert.sameValue(0, byteLength % f.blockSize, "byteLength is multiple of blockSize");
 
 assert.throws(TypeError, () => f.byteLength = byteLength, "byteLength is read-only");
