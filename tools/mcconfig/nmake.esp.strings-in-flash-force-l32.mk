912903c96e74d045e2ad90df05ef01ffeeb433d5
Peter Hoddie <peter@moddable.tech>
2022-08-10 09:59:03 -0700

    structuredClone and deepEqual for esp8266

---
+++ tools/mcconfig/nmake.esp.strings-in-flash-force-l32.mk
@@ -0,0 +1,5 @@
+	@echo # cc $(@F) (strings in flash + force-l32)
+	$(CC) $(C_DEFINES) $(C_INCLUDES) $(C_FLAGS) -mforce-l32 $< -o $@.unmapped
+	$(TOOLS_BIN)\xtensa-lx106-elf-objcopy --rename-section .rodata.str1.1=.irom0.str.1 --rename-section .text=irom0.code $@.unmapped $@ 
+	$(AR) $(AR_OPTIONS) $(APP_ARCHIVE) $@
+
