4131e14baca42846333984124c5e214ae21b3734
Andy Carle <andy@moddable.tech>
2022-08-31 13:48:08 -0600

    shorten device name to allow more room for advertising data

--- examples/network/ble/test-server/main.js
+++ examples/network/ble/test-server/main.js
@@ -22,7 +22,7 @@ const UART_SERVICE_UUID = uuid`6E400001-B5A3-F393-E0A9-E50E24DCCA9E`;
 
 class TestServer extends BLEServer {
 	onReady() {
-		this.deviceName = "Moddable Test Server";
+		this.deviceName = "Moddable Server";
 		this.onDisconnected();
 	}
 	onConnected() {
