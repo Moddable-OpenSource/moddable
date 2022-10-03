2c8e96ebc2d3b32fe7380cd35ab6f0294122e44f
Michael Kellner <mkellner@intlweb.com>
2022-10-03 14:12:24 -0700

    pico i2c

--- build/devices/pico/targets/pico_w/host/provider.js
+++ build/devices/pico/targets/pico_w/host/provider.js
@@ -1,8 +1,18 @@
 import Digital from "embedded:io/digital";
 import DigitalBank from "embedded:io/digitalbank";
+import I2C from "embedded:io/i2c";
+import SMBus from "embedded:io/smbus";
 
 const device = {
-	io: {Digital, DigitalBank},
+	I2C: {
+		default: {
+			io: I2C,
+			data: 2,
+			clock: 3,
+			port: 1
+		}
+	},
+	io: { Digital, DigitalBank, I2C, SMBus },
 	pin: {
 		button: 15,
 		led: 32
