663ca98e1cf63f428ae35ebc3092ce4999604da4
Michael Kellner <mkellner@intlweb.com>
2022-10-24 20:46:54 -0700

    2MB version of AI Thinker ESP32 C3-32S-Kit, allow LEDs work on pins 18 and 19 which defaults to USB function

--- modules/pins/digital/esp32/modGPIO.c
+++ modules/pins/digital/esp32/modGPIO.c
@@ -56,6 +56,8 @@ void modGPIOUninit(modGPIOConfiguration config)
 
 int modGPIOSetMode(modGPIOConfiguration config, uint32_t mode)
 {
+	gpio_reset_pin(config->pin);
+
 	switch (mode) {
 		case kModGPIOOutput:
 		case kModGPIOOutputOpenDrain:
