52144c1815f6f7d66659df66c4fb8111aac4dc08
STC <stc1988@users.noreply.github.com>
2022-11-06 21:58:49 +0900

    Add support for M5Atom U

---
+++ build/devices/esp32/targets/m5atom_u/setup-target.js
@@ -0,0 +1,12 @@
+import M5Button from "m5button";
+import NeoPixel from "neopixel";
+
+export default function (done) {
+	globalThis.button = {
+		a: new M5Button(39)
+	};
+
+	globalThis.lights = new NeoPixel({});
+
+	done?.();
+}
