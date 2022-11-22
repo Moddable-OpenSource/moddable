758b624dc985362c77cd283739bec7f87bc28ac2
Peter Hoddie <peter@moddable.tech>
2022-11-21 16:49:51 -0800

    poco doesn't below here

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -1,11 +1,9 @@
 import System_ from "embedded:io/system"
 import Device from "embedded:provider/builtin"
-import { PocoPrototype } from "commodetto/Poco"
 
 export {}
 declare global {
   // NOTE: `System` is non-standard and temporary to support the IO examples. Breaking changes are possible.
   var System: typeof System_;
   var device: typeof Device;
-  var poco: PocoPrototype;
 }
