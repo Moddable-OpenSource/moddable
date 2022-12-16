f45c053396acc844333d543f07513771071e6d15
Andy Carle <andy@moddable.tech>
2022-12-15 20:37:30 -0700

    ECMA-419 driver for hc-sr04 ultrasonic range sensor

---
+++ modules/drivers/sensors/hc-sr04/hc-sr04.js
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2022 Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ * 
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+/*
+    HC-SR04 Sonic Range Finder
+*/
+
+import Timer from "timer";
+
+class Sensor {
+  #input;
+  #output;
+  #timer;
+  #reading;
+  #onAlert;
+
+  constructor(options){
+    const {outputIO, inputIO, onAlert} = options;
+
+    this.#onAlert = onAlert;
+
+    this.#output = new outputIO({
+      mode:outputIO.Output,
+      pin: options.output
+    });
+
+    this.#input = new inputIO({
+      pin: options.input,
+      mode: inputIO.RisingToFalling,
+      pullUpDown: inputIO.PullDown,
+      onReadable: () => this.#onEcho()
+    });
+
+    this.configure({sampleRate: 500});
+  }
+  
+  configure(options = {}){
+    const {sampleRate} = options;
+
+    if (undefined !== sampleRate) {
+      if (this.#timer !== undefined) {
+        Timer.clear(this.#timer);
+        this.#timer = undefined;
+      }
+        
+      if (sampleRate !== 0) {
+        this.#timer = Timer.repeat(() => {
+          this.#output.write(1);
+          this.#output.write(0);          
+        }, sampleRate);
+      }
+    }
+  }
+
+  sample(){
+    return this.#reading;
+  }
+
+  close() {
+    if (this.#timer !== undefined) {
+      Timer.clear(this.#timer);
+      this.#timer = undefined;
+    }
+    this.#input?.close()
+    this.#output?.close();
+    this.#input = this.#output = undefined;
+  }
+
+  #onEcho() {
+    const value = this.#input.read();
+
+    if (value > 35000) {
+      this.#reading = Number.POSITIVE_INFINITY;
+    } else {
+      this.#reading = value / 58;
+      this.#onAlert?.(this.#reading);
+    }
+  }
+
+}
+
+export default Sensor;
