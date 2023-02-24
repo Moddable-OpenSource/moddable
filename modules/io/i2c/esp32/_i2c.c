de4f75d0620feb52cbf082ec7d7468c9320c6026
Peter Hoddie <peter@moddable.tech>
2023-02-23 22:08:18 -0800

    release i2c semaphore on reuse

--- modules/io/i2c/esp32/_i2c.c
+++ modules/io/i2c/esp32/_i2c.c
@@ -279,8 +279,10 @@ uint8_t i2cActivate(I2C i2c)
 	xSemaphoreTake(gI2CMutex, portMAX_DELAY);
 
 	if ((i2c == gI2CActive) ||
-		(gI2CActive && (gI2CActive->data == i2c->data) && (gI2CActive->clock == i2c->clock) && (gI2CActive->hz == i2c->hz) && (gI2CActive->port == i2c->port) && (gI2CActive->pullup == i2c->pullup)))
+		(gI2CActive && (gI2CActive->data == i2c->data) && (gI2CActive->clock == i2c->clock) && (gI2CActive->hz == i2c->hz) && (gI2CActive->port == i2c->port) && (gI2CActive->pullup == i2c->pullup))) {
+		xSemaphoreGive(gI2CMutex);
 		return 1;
+	}
 
 	if (gI2CActive) {
 		i2c_driver_delete(gI2CActive->port);
