c8f4901e331c3a8e8f8ceaf3730f5a0417f092d2
Peter Hoddie <peter@moddable.tech>
2023-03-13 16:32:20 -0700

    XS: fuzzilli 115

--- xs/sources/xsMemory.c
+++ xs/sources/xsMemory.c
@@ -1165,11 +1165,15 @@ void fxMarkValue(txMachine* the, txSlot* theSlot)
 		
 	case XS_ACCESSOR_KIND:
 		aSlot = theSlot->value.accessor.getter;
-		if (aSlot && !(aSlot->flag & XS_MARK_FLAG))
+		if (aSlot && !(aSlot->flag & XS_MARK_FLAG)) {
+			fxCheckCStack(the);
 			fxMarkInstance(the, aSlot, fxMarkValue);
+		}
 		aSlot = theSlot->value.accessor.setter;
-		if (aSlot && !(aSlot->flag & XS_MARK_FLAG))
+		if (aSlot && !(aSlot->flag & XS_MARK_FLAG)) {
+			fxCheckCStack(the);
 			fxMarkInstance(the, aSlot, fxMarkValue);
+		}
 		break;
 	case XS_HOME_KIND:
 		aSlot = theSlot->value.home.object;
