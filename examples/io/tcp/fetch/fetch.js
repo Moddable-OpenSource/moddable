dc1b2f42d9d1f349433ed121ade795af23e5eead
Peter Hoddie <peter@moddable.tech>
2023-04-06 08:42:13 -0700

    fix handling of content-length 0 response #1080

--- examples/io/tcp/fetch/fetch.js
+++ examples/io/tcp/fetch/fetch.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022  Moddable Tech, Inc.
+ * Copyright (c) 2021-2023  Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK Runtime.
  * 
@@ -239,7 +239,7 @@ function fetch(href, info = {}) {
 					}
 				},
 				onDone(error) {
-					resolveBody(buffer);
+					resolveBody(buffer ?? new ArrayBuffer);
 				}
 			});
 		});
