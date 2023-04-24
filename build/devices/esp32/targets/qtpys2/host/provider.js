56f896018f9f550fbb9a01e1799f33e46b27c6d2
Michael Kellner <mkellner@intlweb.com>
2023-04-23 20:48:24 -0700

    adafruit qtpys2

---
+++ build/devices/esp32/targets/qtpys2/host/provider.js
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2022  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ *
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import Analog from "embedded:io/analog";
+import Digital from "embedded:io/digital";
+import DigitalBank from "embedded:io/digitalbank";
+import I2C from "embedded:io/i2c";
+// import PulseCount from "embedded:io/pulsecount";
+import PWM from "embedded:io/pwm";
+import Serial from "embedded:io/serial";
+import SMBus from "embedded:io/smbus";
+import SPI from "embedded:io/spi";
+
+const device = {
+	I2C: {
+		default: {
+			io: I2C,
+			port: 1,
+			data: 41,
+			clock: 40
+		}
+	},
+	Serial: {
+		default: {
+			io: Serial,
+			port: 0,
+			receive: 16,
+			transmit: 5
+		}
+	},
+	SPI: {
+		default: {
+			io: SPI,
+			port: 1,
+			clock: 36,
+			in: 37,
+			out: 35
+		}
+	},
+	Analog: {
+		default: {
+			io: Analog,
+			pin: 18
+		}
+	},
+	io: { Analog, Digital, DigitalBank, I2C, PWM, Serial, SMBus, SPI },
+	pin: {
+		button: 0
+	}
+};
+
+export default device;
