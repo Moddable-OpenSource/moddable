283dd767766648b56f92c484e9da9f52bd88e4de
Peter Hoddie <peter@moddable.tech>
2023-04-30 16:21:56 -0700

    flashParition -> flashPartition

--- tests/modules/files/flash/erase.js
+++ tests/modules/files/flash/erase.js
@@ -5,7 +5,7 @@ flags: [module]
 
 import Flash from "flash";
 
-const f = new Flash($TESTMC.config.flashParition);
+const f = new Flash($TESTMC.config.flashPartition);
 const {byteLength, blockSize} = f;
 const blocks = byteLength / blockSize;
 
