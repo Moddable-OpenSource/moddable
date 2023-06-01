400775f8e6fca46cf91c6d24d5baa43263297c8c
Peter Hoddie <peter@moddable.tech>
2023-06-01 13:30:02 -0700

    consistently block write on input and read on output #1129

--- modules/io/digital/pico/digitalbank.c
+++ modules/io/digital/pico/digitalbank.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2022  Moddable Tech, Inc.
+ * Copyright (c) 2019-2023  Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK Runtime.
  *
@@ -22,9 +22,6 @@
 	DigitalBank - 
 
 	To do:
-
-		read not blocked on input instances, write not blocked on output instances
-		ESP8266 implementation assumes a single VM
 
 */
 
@@ -322,7 +319,7 @@ void xs_digitalbank_read(xsMachine *the)
 	uint32_t result = 0;
 
 	if (!digital->isInput)
-		xsUnknownError("unimplemented");
+		xsUnknownError("can't read output");
 
 	if (0 == digital->bank) {
 		result = gpio_get_all();
@@ -344,7 +341,7 @@ void xs_digitalbank_write(xsMachine *the)
 	uint32_t value;
 
 	if (digital->isInput)
-		xsUnknownError("unimplemented");
+		xsUnknownError("can't write input");
 
 	value = xsmcToInteger(xsArg(0));
 
