66ce79d78e9d00500140236b1ae8a45b79b30c0a
Thorsten von Eicken <tve@voneicken.com>
2023-06-14 14:54:50 -0700

    typings/global: add integer math function declarations

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -6,4 +6,14 @@ declare global {
   // NOTE: `System` is non-standard and temporary to support the IO examples. Breaking changes are possible.
   var System: typeof System_;
   var device: typeof Device;
+
+  interface Math {
+    idiv(a: number, b: number): number
+    imod(a: number, b: number): number // (x%y+y)%y (always pos results)
+    irem(a: number, b: number): number // x%y (neg x -> neg result)
+    idivmod(a: number, b: number): [number, number] // [x/y, (x%y+y)%y]
+    imuldiv(a: number, b: number, c: number): number // (a * b) / c
+    mod(a: number, b: number): number // (x%y+y)%y (floating point)
+  }
+
 }
