c5b073bdad1684ce2c2e33e86cf8d55614e8a853
Peter Hoddie <peter@moddable.tech>
2023-06-23 13:45:58 -0700

    use release mcsim

--- tools/mcmanifest.js
+++ tools/mcmanifest.js
@@ -1695,11 +1695,11 @@ export class Tool extends TOOL {
 		else if (!this.format)
 			this.format = "UNDEFINED";
 		if (this.platform == "mac")
-			this.environment.SIMULATOR = this.moddablePath + "/build/bin/mac/debug/mcsim.app";
+			this.environment.SIMULATOR = this.moddablePath + "/build/bin/mac/release/mcsim.app";
 		else if (this.platform == "win")
-			this.environment.SIMULATOR = this.moddablePath + "\\build\\bin\\win\\debug\\mcsim.exe";
+			this.environment.SIMULATOR = this.moddablePath + "\\build\\bin\\win\\release\\mcsim.exe";
 		else if (this.platform == "lin")
-			this.environment.SIMULATOR = this.moddablePath + "/build/bin/lin/debug/mcsim";
+			this.environment.SIMULATOR = this.moddablePath + "/build/bin/lin/release/mcsim";
 		this.environment.BUILD_SIMULATOR = this.moddablePath + this.slash + "build" + this.slash + "simulators";
 	}
 	concatProperties(object, properties, flag) {
