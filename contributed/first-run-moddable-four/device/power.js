4342c8629149a3f9e868e6bd8bfd61de33b3d036
Peter Hoddie <peter@moddable.tech>
2023-08-04 11:17:00 -0700

    add licenses

--- contributed/first-run-moddable-four/device/power.js
+++ contributed/first-run-moddable-four/device/power.js
@@ -1,6 +1,19 @@
+/*
+ * Copyright (c) 2023  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ * 
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+
 import Digital from "pins/digital";
 import {Sleep, ResetReason} from "sleep";
-import Timer from "sleep";
 import config from "mc/config";
 
 let wakenWith = "?";
