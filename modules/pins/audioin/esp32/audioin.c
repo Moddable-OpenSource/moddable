15278a28f04d2610487cdef8266745633ca2bbb1
Peter Hoddie <peter@moddable.tech>
2023-08-30 17:08:19 -0700

    analog audio in for ESP-IDF 5.1.1

--- modules/pins/audioin/esp32/audioin.c
+++ modules/pins/audioin/esp32/audioin.c
@@ -32,6 +32,8 @@
 	#include "freertos/FreeRTOS.h"
 	#include "driver/i2s_std.h"
 	#include "driver/i2s_pdm.h"
+	#include "esp_adc/adc_continuous.h"
+	#include "soc/soc_caps.h"
 #else
 	#error unsupported target
 #endif
@@ -59,19 +61,59 @@
 	#ifndef MODDEF_AUDIOIN_I2S_ADC
 		#define MODDEF_AUDIOIN_I2S_ADC (0)
 	#endif
+
+	#if MODDEF_AUDIOIN_SAMPLERATE < SOC_ADC_SAMPLE_FREQ_THRES_LOW
+		#error "sample rate too low"
+	#elif MODDEF_AUDIOIN_SAMPLERATE > SOC_ADC_SAMPLE_FREQ_THRES_HIGH
+		#error "sample rate too high"
+	#endif
 #endif
+
+#define kADCSampleLength (256)
 
 void xs_audioin_destructor(void *data)
 {
 	if (data) {
+#if MODDEF_AUDIOIN_I2S_ADC
+		adc_continuous_handle_t adcHandle = data;
+		adc_continuous_stop(adcHandle);
+		adc_continuous_deinit(adcHandle);
+#else
 		i2s_chan_handle_t rx_handle = data;
 		i2s_channel_disable(rx_handle);
 		i2s_del_channel(rx_handle);
+#endif
 	}
 }
 
 void xs_audioin(xsMachine *the)
 {
+#if MODDEF_AUDIOIN_I2S_ADC
+	adc_continuous_handle_t adcHandle;
+	adc_continuous_handle_cfg_t adc_config = {
+		.max_store_buf_size = 1024,
+		.conv_frame_size = kADCSampleLength,
+	};
+	ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &adcHandle));
+
+    adc_continuous_config_t dig_cfg = {
+        .sample_freq_hz = MODDEF_AUDIOIN_SAMPLERATE,
+        .conv_mode = ADC_CONV_SINGLE_UNIT_1,
+        .format = ADC_DIGI_OUTPUT_FORMAT_TYPE1,
+    };
+
+	adc_digi_pattern_config_t adc_pattern[1] = {0};
+	adc_pattern[0].atten = ADC_ATTEN_DB_11;		// if this is ADC_ATTEN_DB_0 M5Stack Fire is overdriven like crazy
+	adc_pattern[0].channel = ADC_CHANNEL_6;		//@@ config
+	adc_pattern[0].unit = ADC_UNIT_1;				//@@ config
+	adc_pattern[0].bit_width = ADC_BITWIDTH_12;
+	dig_cfg.pattern_num = 1;
+	dig_cfg.adc_pattern = adc_pattern;
+	ESP_ERROR_CHECK(adc_continuous_config(adcHandle, &dig_cfg));
+
+	ESP_ERROR_CHECK(adc_continuous_start(adcHandle));
+	xsmcSetHostData(xsThis, (void *)adcHandle);
+#else
 	esp_err_t err;
 	i2s_chan_handle_t rx_handle;
 
@@ -80,9 +122,7 @@ void xs_audioin(xsMachine *the)
     err = i2s_new_channel(&chan_cfg, NULL, &rx_handle);
 	if (err) xsUnknownError("i2s_new_channel failed");
 
-#if MODDEF_AUDIOIN_I2S_ADC
-	#error ADC umimplemented
-#elif MODDEF_AUDIOIN_I2S_PDM
+#if MODDEF_AUDIOIN_I2S_PDM
     i2s_pdm_rx_config_t pdm_rx_cfg = {
         .clk_cfg = I2S_PDM_RX_CLK_DEFAULT_CONFIG(MODDEF_AUDIOIN_SAMPLERATE),
         .slot_cfg = I2S_PDM_RX_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_MONO),
@@ -137,6 +177,7 @@ void xs_audioin(xsMachine *the)
 	}
 
 	xsmcSetHostData(xsThis, (void *)rx_handle);
+#endif
 }
 
 void xs_audioin_close(xsMachine *the)
@@ -151,7 +192,11 @@ void xs_audioin_close(xsMachine *the)
 
 void xs_audioin_read(xsMachine *the)
 {
+#if MODDEF_AUDIOIN_I2S_ADC
+	adc_continuous_handle_t adcHandle = xsmcGetHostData(xsThis);
+#else
 	i2s_chan_handle_t rx_handle = xsmcGetHostData(xsThis);
+#endif
 	esp_err_t err;
 	int argc = xsmcArgc;
 	size_t bytes_read;
@@ -178,7 +223,39 @@ void xs_audioin_read(xsMachine *the)
 		xsResult = xsArg(1);
 	}
 
-#if MODDEF_AUDIOIN_I2S_PDM && (8 == MODDEF_AUDIOIN_BITSPERSAMPLE)
+#if MODDEF_AUDIOIN_I2S_ADC
+	uint8_t input[kADCSampleLength];
+
+	while (sampleCount) {
+		uint32_t use = sizeof(input);
+		uint32_t needed = sampleCount * sizeof(adc_digi_output_data_t);
+		if (use > needed) use = needed;
+		uint32_t used;
+
+      err = adc_continuous_read(adcHandle, input, use, &used, 17);
+		if (err) break;
+
+		used /= sizeof(adc_digi_output_data_t);
+		sampleCount -= used;
+
+		adc_digi_output_data_t *in = (adc_digi_output_data_t *)input;
+#if 8 == MODDEF_AUDIOIN_BITSPERSAMPLE
+		uint8_t *out = (uint8_t *)samples;
+		while (used--) {
+			*out++ = in->type1.data >> 4;
+			in++;
+		}
+		samples = (void *)out;
+#else // 16 == MODDEF_AUDIOIN_BITSPERSAMPLE
+		uint16_t *out = (uint16_t *)samples;
+		while (used--) {
+			*out++ = (in->type1.data - 2048) * 8;
+			in++;
+		}
+		samples = (void *)out;
+#endif
+	}
+#elif MODDEF_AUDIOIN_I2S_PDM && (8 == MODDEF_AUDIOIN_BITSPERSAMPLE)
 	int8_t *out = samples;
 	int i;
 	while (sampleCount) {
