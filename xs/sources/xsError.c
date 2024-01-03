fc16cf86ac8e5efae7e4793a203f8b9511ef26e5
Patrick Soquet <ps@moddable.tech>
2024-01-03 09:08:40 +0100

    XS: esp32/moddable_two build

--- xs/sources/xsError.c
+++ xs/sources/xsError.c
@@ -39,7 +39,7 @@
 
 static txSlot* fx_Error_aux(txMachine* the, txError error, txInteger i);
 
-const txS4 gxErrorWhichPrototypeStackIndex[XS_ERROR_COUNT] ICACHE_FLASH_ATTR = {
+const int gxErrorWhichPrototypeStackIndex[XS_ERROR_COUNT] ICACHE_FLASH_ATTR = {
 	mxErrorPrototypeStackIndex,
 	mxErrorPrototypeStackIndex,
 	mxEvalErrorPrototypeStackIndex,
