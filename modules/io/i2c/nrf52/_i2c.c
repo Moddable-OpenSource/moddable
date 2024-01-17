a90f0aadb8abbf584688ed15a9a22db8ac809aad
Michael Kellner <mkellner@intlweb.com>
2024-01-16 17:31:22 -0800

    nRF52: verify i2c can activate in constructor

--- modules/io/i2c/nrf52/_i2c.c
+++ modules/io/i2c/nrf52/_i2c.c
@@ -200,9 +200,7 @@ void _xs_i2c_constructor(xsMachine *the)
 	if (!i2c)
 		xsRangeError("no memory");
 
-	xsmcSetHostData(xsThis, i2c);
 	i2c->obj = xsThis;
-	xsRemember(i2c->obj);
 	i2c->clock = clock;
 	i2c->data = data;
 	i2c->hz = hz;
@@ -212,8 +210,17 @@ void _xs_i2c_constructor(xsMachine *the)
 	i2c->pullup = pullup;
 	i2c->port = (uint8_t)port;
 
+	if (!i2cActivate(i2c)) {
+		c_free(i2c);
+		xsUnknownError("I2CErr init");
+	}
+
+	xsmcSetHostData(xsThis, i2c);
+
 	i2c->next = gI2C;
 	gI2C = i2c;
+
+	xsRemember(i2c->obj);
 
 	builtinUsePin(data);
 	builtinUsePin(clock);
