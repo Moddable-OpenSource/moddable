7c79846d03dc4cba3321727b6f6122e5ad4e4376
Patrick Soquet <ps@moddable.tech>
2025-08-01 17:05:05 +0200

    pebble piu inverter

---
+++ build/devices/pebble/modules/piu/inverter/piuInverter.js
@@ -0,0 +1,8 @@
+const inverter = {
+	__proto__: Content.prototype,
+	_create($, it) @ "PiuInverter_create",
+};
+export const Inverter = Template(inverter);
+Object.freeze(inverter);
+globalThis.Inverter = Inverter;
+export default Inverter;
