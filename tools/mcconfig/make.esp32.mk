b1e772eacb2ccb65f9fef3f4062905ebc3b081ff
Peter Hoddie <peter@moddable.com>
2025-08-17 21:25:17 -0700

    set EXPECTED_ESP_IDF in the esp32 manifest HipsterBrown/xs-dev#215

--- tools/mcconfig/make.esp32.mk
+++ tools/mcconfig/make.esp32.mk
@@ -39,8 +39,6 @@ endif
 PROGRAMMING_VID ?= 303a
 PROGRAMMING_PID ?= 1001
 
-EXPECTED_ESP_IDF ?= v5.5
-
 # ESP32_SUBCLASS is to find some include files in IDFv4
 # values include esp32, esp32s3 and esp32s2
 ESP32_SUBCLASS ?= esp32
