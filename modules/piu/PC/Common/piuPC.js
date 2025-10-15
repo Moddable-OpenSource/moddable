d65554d4705c1f46d28ca1d777c924103e2d80ff
Peter Hoddie <peter@moddable.com>
2025-10-14 19:54:31 -0700

    native

--- modules/piu/PC/Common/piuPC.js
+++ modules/piu/PC/Common/piuPC.js
@@ -20,7 +20,7 @@
 
 import {} from "piu/All";
 
-globalThis.__jsx__ = function(Tag, attributes) @ "Piu__jsx__"
+globalThis.__jsx__ = function(Tag, attributes) { return native("Piu__jsx__").call(this, Tag, attributes); }
 
 export class Component extends Behavior {
 	constructor($, it) {
@@ -62,10 +62,10 @@ globalThis.LinkComponent = LinkComponent;
 
 // PiuTexture.c
 
-export class Texture @ "PiuTextureDelete" {
-	constructor(it) @ "PiuTextureCreate"
-	get width() @ "PiuTexture_get_width"
-	get height() @ "PiuTexture_get_height"
+export class Texture extends Native("PiuTextureDelete") {
+	constructor(it) { super(); native("PiuTextureCreate").call(this, it); }
+	get width() { return native("PiuTexture_get_width").call(this); }
+	get height() { return native("PiuTexture_get_height").call(this); }
 	static template(it) {
 		return function() {
 			let map = globalThis.assetMap;
@@ -89,11 +89,11 @@ globalThis.Texture = Texture;
 
 var roundContent = {
 	__proto__: Content.prototype,
-	_create($, it) @ "PiuRoundContent_create",
+	_create($, it) { return native("PiuRoundContent_create").call(this, $, it); },
 
-	get radius() @ "PiuRoundContent_get_radius",
+	get radius() { return native("PiuRoundContent_get_radius").call(this); },
 	
-	set radius(it) @ "PiuRoundContent_set_radius",
+	set radius(it) { native("PiuRoundContent_set_radius").call(this, it); },
 };
 export var RoundContent = Template(roundContent);
 Object.freeze(roundContent);
@@ -103,15 +103,15 @@ globalThis.RoundContent = RoundContent;
 
 var field = {
 	__proto__: Content.prototype,
-	_create($, it) @ "PiuField_create",
+	_create($, it) { return native("PiuField_create").call(this, $, it); },
 
-	get placeholder() @ "PiuField_get_placeholder",
-	get string() @ "PiuField_get_string",
+	get placeholder() { return native("PiuField_get_placeholder").call(this); },
+	get string() { return native("PiuField_get_string").call(this); },
 	
-	set placeholder(it) @ "PiuField_set_placeholder",
-	set string(it) @ "PiuField_set_string",
+	set placeholder(it) { native("PiuField_set_placeholder").call(this, it); },
+	set string(it) { native("PiuField_set_string").call(this, it); },
 	
-	focus() @ "PiuField_focus",
+	focus() { return native("PiuField_focus").call(this); },
 };
 export var Field = Template(field);
 Object.freeze(field);
@@ -121,17 +121,17 @@ globalThis.Field = Field;
 
 var application = {
 	__proto__: Container.prototype,
-	_create($, it) @ "PiuApplication_create",
-	get cursor() @ "PiuApplication_get_cursor",
-	get title() @ "PiuApplication_get_title",
-	set cursor(it) @ "PiuApplication_set_cursor",
-	set title(it) @ "PiuApplication_set_title",
-	createMenus() @ "PiuApplication_createMenus",
-	gotoFront() @ "PiuApplication_gotoFront",
-	invalidateMenus() @ "PiuApplication_invalidateMenus",
-	purge() @ "PiuApplication_purge",
-	quit() @ "PiuApplication_quit",
-	updateMenus() @ "PiuApplication_updateMenus",
+	_create($, it) { return native("PiuApplication_create").call(this, $, it); },
+	get cursor() { return native("PiuApplication_get_cursor").call(this); },
+	get title() { return native("PiuApplication_get_title").call(this); },
+	set cursor(it) { native("PiuApplication_set_cursor").call(this, it); },
+	set title(it) { native("PiuApplication_set_title").call(this, it); },
+	createMenus() { return native("PiuApplication_createMenus").call(this); },
+	gotoFront() { return native("PiuApplication_gotoFront").call(this); },
+	invalidateMenus() { return native("PiuApplication_invalidateMenus").call(this); },
+	purge() { return native("PiuApplication_purge").call(this); },
+	quit() { return native("PiuApplication_quit").call(this); },
+	updateMenus() { return native("PiuApplication_updateMenus").call(this); },
 }
 
 export function Application($, it = {}) {
@@ -156,15 +156,15 @@ globalThis.Application = Application;
 
 // PiuView.c
 
-class View @ "PiuViewDelete" {
-	constructor(it) @ "PiuViewCreate"
+class View extends Native("PiuViewDelete") {
+	constructor(it) { super(); native("PiuViewCreate").call(this, it); }
 }
 Object.freeze(View.prototype);
 
 // PiuService.c
 
-export class Service @ "ServiceProxyDelete" {
-	constructor(thread, module) @ "ServiceProxyCreate"
+export class Service extends Native("ServiceProxyDelete") {
+	constructor(thread, module) { super(); native("ServiceProxyCreate").call(this, thread, module); }
 	get(target, key) {
 		let handler = this;
 		return function(...params) {
@@ -176,75 +176,71 @@ export class Service @ "ServiceProxyDelete" {
 };
 globalThis.Service = Service;
 
-
 globalThis.cursors = {
-	get arrow() @ "PiuCursors_get_arrow",
-	get cross() @ "PiuCursors_get_cross",
-	get iBeam() @ "PiuCursors_get_iBeam",
-	get link() @ "PiuCursors_get_link",
-	get notAllowed() @ "PiuCursors_get_notAllowed",
-	get resizeColumn() @ "PiuCursors_get_resizeColumn",
-	get resizeRow() @ "PiuCursors_get_resizeRow",
+	get arrow() { return native("PiuCursors_get_arrow").call(this); },
+	get cross() { return native("PiuCursors_get_cross").call(this); },
+	get iBeam() { return native("PiuCursors_get_iBeam").call(this); },
+	get link() { return native("PiuCursors_get_link").call(this); },
+	get notAllowed() { return native("PiuCursors_get_notAllowed").call(this); },
+	get resizeColumn() { return native("PiuCursors_get_resizeColumn").call(this); },
+	get resizeRow() { return native("PiuCursors_get_resizeRow").call(this); },
 };
 Object.freeze(globalThis.cursors);
 
 globalThis.system = {
-	get applicationPath() @ "PiuSystem_get_applicationPath",
-	get localDirectory() @ "PiuSystem_get_localDirectory",
-	get platform() @ "PiuSystem_get_platform",
+	get applicationPath() { return native("PiuSystem_get_applicationPath").call(this); },
+	get localDirectory() { return native("PiuSystem_get_localDirectory").call(this); },
+	get platform() { return native("PiuSystem_get_platform").call(this); },
 
-	launchPath(url) @ "PiuSystem_launchPath",
-	launchURL(url) @ "PiuSystem_launchURL",
+	launchPath(url) { return native("PiuSystem_launchPath").call(this, url); },
+	launchURL(url) { return native("PiuSystem_launchURL").call(this, url); },
 	
 	// clipboard
-	getClipboardString() @ "PiuSystem_getClipboardString",
-	setClipboardString(it) @ "PiuSystem_setClipboardString",
+	getClipboardString() { return native("PiuSystem_getClipboardString").call(this); },
+	setClipboardString(it) { return native("PiuSystem_setClipboardString").call(this, it); },
 
 	// dialogs
-	alert(dictionary, callback) @ "PiuSystem_alert",
-	openDirectory(dictionary, callback) @ "PiuSystem_openDirectory",
-	openFile(dictionary, callback) @ "PiuSystem_openFile",
-	saveDirectory(dictionary, callback) @ "PiuSystem_saveDirectory",
-	saveFile(dictionary, callback) @ "PiuSystem_saveFile",
+	alert(dictionary, callback) { return native("PiuSystem_alert").call(this, dictionary, callback); },
+	openDirectory(dictionary, callback) { return native("PiuSystem_openDirectory").call(this, dictionary, callback); },
+	openFile(dictionary, callback) { return native("PiuSystem_openFile").call(this, dictionary, callback); },
+	saveDirectory(dictionary, callback) { return native("PiuSystem_saveDirectory").call(this, dictionary, callback); },
+	saveFile(dictionary, callback) { return native("PiuSystem_saveFile").call(this, dictionary, callback); },
 
 	// files
-	copyFile(from, to) @ "PiuSystem_copyFile",
-	deleteDirectory(path) @ "PiuSystem_deleteDirectory",
-	deleteFile(path) @ "PiuSystem_deleteFile",
-	ensureDirectory(path) @ "PiuSystem_ensureDirectory",
+	copyFile(from, to) { return native("PiuSystem_copyFile").call(this, from, to); },
+	deleteDirectory(path) { return native("PiuSystem_deleteDirectory").call(this, path); },
+	deleteFile(path) { return native("PiuSystem_deleteFile").call(this, path); },
+	ensureDirectory(path) { return native("PiuSystem_ensureDirectory").call(this, path); },
 	
-	fileExists(path) @ "PiuSystem_fileExists",
-	getFileInfo(path) @ "PiuSystem_getFileInfo",
-	getSymbolicLinkInfo(path) @ "PiuSystem_getSymbolicLinkInfo",
+	fileExists(path) { return native("PiuSystem_fileExists").call(this, path); },
+	getFileInfo(path) { return native("PiuSystem_getFileInfo").call(this, path); },
+	getSymbolicLinkInfo(path) { return native("PiuSystem_getSymbolicLinkInfo").call(this, path); },
 	
-	readFileBuffer(path) @ "PiuSystem_readFileBuffer",
-	readFileString(path) @ "PiuSystem_readFileString",
-	readPreferenceString(key) @ "PiuSystem_readPreferenceString",
+	readFileBuffer(path) { return native("PiuSystem_readFileBuffer").call(this, path); },
+	readFileString(path) { return native("PiuSystem_readFileString").call(this, path); },
+	readPreferenceString(key) { return native("PiuSystem_readPreferenceString").call(this, key); },
 	
-	renameDirectory(path, name) @ "PiuSystem_renameDirectory",
-	renameFile(path, name) @ "PiuSystem_renameFile",
+	renameDirectory(path, name) { return native("PiuSystem_renameDirectory").call(this, path, name); },
+	renameFile(path, name) { return native("PiuSystem_renameFile").call(this, path, name); },
 	
-	writeFileBuffer(path, data) @ "PiuSystem_writeFileBuffer",
-	writeFileString(path, data) @ "PiuSystem_writeFileString",
-	writePreferenceString(key, data) @ "PiuSystem_writePreferenceString",
+	writeFileBuffer(path, data) { return native("PiuSystem_writeFileBuffer").call(this, path, data); },
+	writeFileString(path, data) { return native("PiuSystem_writeFileString").call(this, path, data); },
+	writePreferenceString(key, data) { return native("PiuSystem_writePreferenceString").call(this, key, data); },
 	
-	DirectoryIterator: class @ "PiuSystem_DirectoryIteratorDelete" {
-		constructor(path)  @ "PiuSystem_DirectoryIteratorCreate"
-		next() @ "PiuSystem_DirectoryIterator_next"
+	DirectoryIterator: class extends Native("PiuSystem_DirectoryIteratorDelete") {
+		constructor(path) { super(); native("PiuSystem_DirectoryIteratorCreate").call(this, path); }
+		next() { return native("PiuSystem_DirectoryIterator_next").call(this); }
 	},
-	DirectoryNotifier: class @ "PiuSystem_DirectoryNotifierDelete" {
-		constructor(path, callback)  @ "PiuSystem_DirectoryNotifierCreate"
-		close() @ "PiuSystem_DirectoryNotifier_close"
+	DirectoryNotifier: class extends Native("PiuSystem_DirectoryNotifierDelete") {
+		constructor(path, callback) { super(); native("PiuSystem_DirectoryNotifierCreate").call(this, path, callback); }
+		close() { return native("PiuSystem_DirectoryNotifier_close").call(this); }
 	},
 	
 	// paths
-	buildPath(directory, name, extension) @ "PiuSystem_buildPath",
-	getPathDirectory(path) @ "PiuSystem_getPathDirectory",
-	getPathExtension(path) @ "PiuSystem_getPathExtension",
-	getPathName(path) @ "PiuSystem_getPathName",
+	buildPath(directory, name, extension) { return native("PiuSystem_buildPath").call(this, directory, name, extension); },
+	getPathDirectory(path) { return native("PiuSystem_getPathDirectory").call(this, path); },
+	getPathExtension(path) { return native("PiuSystem_getPathExtension").call(this, path); },
+	getPathName(path) { return native("PiuSystem_getPathName").call(this, path); },
 }
 Object.freeze(globalThis.system);
-
-
-
 
