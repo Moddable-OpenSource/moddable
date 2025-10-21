6150e19e28ff5998370e89d6b078d1a55ce0948c
Peter Hoddie <peter@moddable.com>
2025-10-20 20:27:16 -0700

    udp - first time

---
+++ modules/io/socket/zephyr/udp.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2019-2025  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ *
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+class UDP extends Native("xs_udp_destructor") {
+	constructor(dictionary) { super(); native("xs_udp_constructor").call(this, dictionary); };
+	close() { return native("xs_udp_close").call(this); }
+	read() { return native("xs_udp_read").call(this); }
+	write() { return native("xs_udp_write").call(this); }
+	// add() { return native("xs_udp_add").call(this); }
+	// remove() { return native("xs_udp_remove").call(this); }
+
+	get format() {
+		return "buffer";
+	}
+	set format(value) {
+		if ("buffer" !== value)
+			throw new RangeError;
+	}
+}
+
+export default UDP;
