40806c9e83e09ed0b96850b787275d514243df6d
Joshua Jun <joshuajun@proton.me>
2025-12-18 12:00:06 -0800

    Fix up pebble math

--- xs/platforms/pebble/xsHost.h
+++ xs/platforms/pebble/xsHost.h
@@ -445,12 +445,20 @@ void *bsearch(const void *key, const void *base, size_t nel, size_t width, int (
 #define M_PI        3.14159265358979323846
 #endif
 
+#ifndef M_LN10
+#define M_LN10      2.30258509299404568402
+#endif
+
+#ifndef M_PI_2
+#define M_PI_2      1.57079632679489661923
+#endif
+
 #define _M_LN2        0.693147180559945309417
 #define M_LOG2E     1.4426950408889634074
 #define M_LOG10E    0.43429448190325182765
 #define M_LN2       _M_LN2
-#define M_LN10      2.30258509299404568402 
-#define M_PI_2      1.57079632679489661923
+//#define M_LN10      2.30258509299404568402 
+//#define M_PI_2      1.57079632679489661923
 #define M_PI_4      0.78539816339744830962
 #define M_1_PI      0.31830988618379067154
 #define M_2_PI      0.63661977236758134308
