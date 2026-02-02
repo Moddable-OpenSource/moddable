d64c4bbbc67a81355d5cae29e23f7db98c3f9f7f
Peter Hoddie <peter@moddable.com>
2026-02-02 13:01:01 -0800

    typo

--- xs/sources/xsSnapshot.c
+++ xs/sources/xsSnapshot.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018  Moddable Tech, Inc.
+ * Copyright (c) 2016-2026  Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK Runtime.
  * 
@@ -737,7 +737,7 @@ static txCallback gxCallbacks[mxCallbacksLength] = {
 	fx_Map_prototype_getOrInsertComputed,
 	fx_Math_sumPrecise,
 	fx_WeakMap_prototype_getOrInsert,
-	fx_Weak_prototype_getOrInsertComputed,
+	fx_WeakMap_prototype_getOrInsertComputed,
 #endif
 };
 extern const txTypeDispatch gxTypeDispatches[];
