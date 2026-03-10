ea5a1c678e6c6f27af0899dbe7f017170635cbf4
mkellner <mkellner@moddable.tech>
2026-03-10 01:16:04 -0700

    win tools: only build debug if requested

--- build/makefiles/win/build.bat
+++ build/makefiles/win/build.bat
@@ -42,13 +42,6 @@ IF "%1%"=="clean" (
 
 @echo on
 
-nmake GOAL=debug BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f %XS_DIR%\makefiles\win\xsc.mak /s
-nmake GOAL=debug BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f %XS_DIR%\makefiles\win\xsid.mak /s
-nmake GOAL=debug BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f %XS_DIR%\makefiles\win\xsl.mak /s
-
-nmake GOAL=debug BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f serial2xsbug.mak /s
-nmake GOAL=debug BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f tools.mak /s
-
 nmake GOAL=release BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f %XS_DIR%\makefiles\win\xsc.mak /s
 nmake GOAL=release BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f %XS_DIR%\makefiles\win\xsid.mak /s
 nmake GOAL=release BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f %XS_DIR%\makefiles\win\xsl.mak /s
@@ -56,8 +49,16 @@ nmake GOAL=release BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f %XS_DIR%\makefile
 nmake GOAL=release BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f serial2xsbug.mak /s
 nmake GOAL=release BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f tools.mak /s
 
-call %MODDABLE%\build\bin\win\debug\mcconfig -d -m -p x-win %MODDABLE%\tools\xsbug\manifest.json
-call %MODDABLE%\build\bin\win\debug\mcconfig -d -m -p x-win %MODDABLE%\tools\mcsim\manifest.json
-
 call %MODDABLE%\build\bin\win\release\mcconfig -m -p x-win %MODDABLE%\tools\xsbug\manifest.json
 call %MODDABLE%\build\bin\win\release\mcconfig -m -p x-win %MODDABLE%\tools\mcsim\manifest.json
+
+IF "%1%"=="debug" (
+nmake GOAL=debug BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f %XS_DIR%\makefiles\win\xsc.mak /s
+nmake GOAL=debug BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f %XS_DIR%\makefiles\win\xsid.mak /s
+nmake GOAL=debug BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f %XS_DIR%\makefiles\win\xsl.mak /s
+
+nmake GOAL=debug BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f serial2xsbug.mak /s
+nmake GOAL=debug BUILD_DIR=%BUILD_DIR% XS_DIR=%XS_DIR% /c /f tools.mak /s
+call %MODDABLE%\build\bin\win\debug\mcconfig -d -m -p x-win %MODDABLE%\tools\xsbug\manifest.json
+call %MODDABLE%\build\bin\win\debug\mcconfig -d -m -p x-win %MODDABLE%\tools\mcsim\manifest.json
+)
