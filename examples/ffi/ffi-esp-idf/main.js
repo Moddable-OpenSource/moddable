77db8a34fc89d9d3eefbf068318e4b9c64e82905
Peter Hoddie <peter@moddable.com>
2026-05-04 20:25:24 -0700

    copyrights

--- examples/ffi/ffi-esp-idf/main.js
+++ examples/ffi/ffi-esp-idf/main.js
@@ -1,44 +1,57 @@
+/*
+ * Copyright (c) 2026  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ * 
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+
 import FFI from "mc/ffi";
 import {gpio_io_config_t} from "gpio_t"
 import Timer from "timer"
 
-const gpio = new FFI;
+const Natives = new FFI;
 
 const targetGPIO = 0;
 
 // get / set drive capability
-
 let uint32 = new Uint32Array(1);
-gpio.gpio_get_drive_capability(targetGPIO, uint32.buffer);
+Natives.gpio_get_drive_capability(targetGPIO, uint32.buffer);
 trace(`GPIO drive ${uint32[0]}\n`);		// default - expect 2 (GPIO_DRIVE_CAP_DEFAULT)
 
-gpio.gpio_set_drive_capability(targetGPIO, 3 /* GPIO_DRIVE_CAP_3 - strongest */);
-gpio.gpio_get_drive_capability(targetGPIO, uint32.buffer);
+Natives.gpio_set_drive_capability(targetGPIO, 3 /* GPIO_DRIVE_CAP_3 - strongest */);
+Natives.gpio_get_drive_capability(targetGPIO, uint32.buffer);
 trace(`GPIO drive ${uint32[0]}\n`);
 
 // disable internal pull-up
-gpio.gpio_pullup_dis(targetGPIO);
+Natives.gpio_pullup_dis(targetGPIO);
 
 // dump configuration
 let config = new gpio_io_config_t;
-gpio.gpio_get_io_config(targetGPIO, config.buffer);
+Natives.gpio_get_io_config(targetGPIO, config.buffer);
 trace(JSON.stringify(config, undefined, "  "), "\n");
 
 // reset and dump configuration to see what changed
-gpio.gpio_reset_pin(targetGPIO);
+Natives.gpio_reset_pin(targetGPIO);
 
-gpio.gpio_get_io_config(targetGPIO, config.buffer);
+Natives.gpio_get_io_config(targetGPIO, config.buffer);
 trace(JSON.stringify(config, undefined, "  "), "\n");
 
 // configure as input with pull-up
-gpio.gpio_set_direction(targetGPIO, 1 /* GPIO_MODE_INPUT */);
-gpio.gpio_pullup_en(targetGPIO);
+Natives.gpio_set_direction(targetGPIO, 1 /* GPIO_MODE_INPUT */);
+Natives.gpio_pullup_en(targetGPIO);
 
 // poll to log button state changes
 trace(`Monitoring GPIO ${targetGPIO} for changes\n`);
 let last = undefined;
 Timer.repeat(() => {
-	const level = gpio.gpio_get_level(targetGPIO);
+	const level = Natives.gpio_get_level(targetGPIO);
 	if (level === last)
 		return;
 
--- examples/ffi/ffi-lib/integers.c
+++ examples/ffi/ffi-lib/integers.c
@@ -1,3 +1,16 @@
+* Copyright (c) 2026  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ * 
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+
 #include "stdint.h"
 
 extern int32_t add32_t(int32_t arg0, int32_t arg1);
