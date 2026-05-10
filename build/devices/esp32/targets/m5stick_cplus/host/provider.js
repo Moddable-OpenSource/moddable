e089ab6874b001d291e1f36a2f6eab6aecfe4d8c
STC <stc1988@users.noreply.github.com>
2026-05-10 18:06:45 +0900

    M5StickC and M5StickCplus migrate  drives from pins/i2c to Ecma-419

--- build/devices/esp32/targets/m5stick_cplus/host/provider.js
+++ build/devices/esp32/targets/m5stick_cplus/host/provider.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021  Moddable Tech, Inc.
+ * Copyright (c) 2021-2026  Moddable Tech, Inc.
  *
  *   This file is part of the Moddable SDK Runtime.
  *
@@ -28,6 +28,9 @@ import Serial from "embedded:io/serial";
 import SMBus from "embedded:io/smbus";
 import SPI from "embedded:io/spi";
 import PulseWidth from "embedded:io/pulsewidth";
+import AXP192 from "embedded:peripheral/Power/axp192";
+import MPU6886 from "embedded:sensor/Accelerometer-Gyroscope/MPU6886";
+import RTC from "embedded:RTC/BM8563";
 
 const device = {
 	I2C: {
@@ -75,6 +78,69 @@ const device = {
 		led: 10,
 		displayDC: 23,
 		displaySelect: 5
+	},
+	sensor: {
+		IMU: class extends MPU6886 {
+			constructor(options) {
+				super({
+					...options,
+					sensor: {
+						...device.I2C.internal,
+						io: device.io.SMBus
+					}
+				});
+			}
+			sample() {
+				const sample = super.sample();
+				sample.accelerometer.x *= -1;
+				sample.accelerometer.y *= -1;
+				sample.gyroscope.y *= -1;
+				return sample;
+			}
+		}
+	},
+	peripheral: {
+		Power: class extends AXP192 {
+			constructor(options) {
+				super({
+					...options,
+					peripheral:{
+						...device.I2C.internal,
+						io: device.io.SMBus
+					}
+				});
+				this.writeByte(0x10, 0xff); // OLED VPP Enable
+				this.writeByte(0x28, 0xff); // Enable LDO2&LDO3, LED&TFT 3.3V
+				this.writeByte(0x82, 0xff); // Enable all the ADCs
+				this.writeByte(0x33, 0xc0); // Enable Charging, 100mA, 4.2V End at 0.9
+				this.writeByte(0xb8, 0x80); // Enable Colume Counter
+				this.writeByte(0x12, 0x4d); // Enable DC-DC1, OLED VDD, 5B V EXT
+				this.writeByte(0x36, 0x5c); // PEK
+				this.writeByte(0x91, 0xA0); // Set MIC voltage to 2.8V
+				this.writeByte(0x90, 0x02); // gpio0
+			}
+			 // value 0 - 100 %
+			set brightness(value) {
+				if (value <= 0)
+				value = 7;
+				else if (value >= 100)
+				value = 15;
+				else
+				value = (value / 100) * 8 + 7;
+				this.writeByte(0x28, (this.readByte(0x28) & 0x0F) | (value << 4));
+			}
+		},
+		RTC: class {
+			  constructor(options) {
+				return new RTC({
+				  ...options,
+				  clock: {
+					...device.I2C.internal,
+					io: SMBus,
+				  },
+				});
+			}
+		},
 	}
 };
 
---
+++ modules/drivers/sensors/sh200q/sh200q.js
@@ -0,0 +1,184 @@
+/*
+ * Copyright (c) Wilberforce
+ 
+ * Copyright (c) 2019-2020 Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK Runtime.
+ *
+ *   The Moddable SDK Runtime is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   The Moddable SDK Runtime is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with the Moddable SDK Runtime.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+/*
+    sh200q Accelerometer + Gyro
+            Datasheet: 	http://senodia.com/Uploads/Product/5b2b6ef1216e8.pdf
+            Register Map:   https://github.com/m5stack/M5StickC/blob/master/src/utility/SH200Q.h#L7-L22
+            https://github.com/m5stack/M5StickC/blob/master/src/utility/SH200Q.cpp
+           
+*/
+
+import Timer from "timer";
+
+const REGISTERS = {
+    ACCEL_XOUT: 0x00,
+    TEMP_OUT: 0x0C,
+    GYRO_XOUT: 0x06,
+    WHO_AM_I: 0x30,
+
+    ACC_CONFIG: 0x0E,
+    GYRO_CONFIG: 0x0F,
+    GYRO_DLPF: 0x11,
+    FIFO_CONFIG: 0x12,
+    ACC_RANGE: 0x16,
+    GYRO_RANGE: 0x2B,
+    OUTPUT_ACC: 0x00,
+    OUTPUT_GYRO: 0x06,
+    OUTPUT_TEMP: 0x0C,
+    REG_SET1: 0xBA,
+    REG_SET2: 0xCA, //ADC reset
+    ADC_RESET: 0xC2, //drive reset
+    SOFT_RESET: 0x7F,
+    RESET: 0x75
+};
+Object.freeze(REGISTERS);
+
+const EXPECTED_WHO_AM_I = 0x18;
+
+const GYRO_SCALER = {
+    GFS_125DPS: (125.0 / 32768.0),
+    GFS_250DPS: (250.0 / 32768.0),
+    GFS_500DPS: (500.0 / 32768.0),
+    GFS_1000DPS: (1000.0 / 32768.0),
+    GFS_2000DPS: (2000.0 / 32768.0)
+};
+Object.freeze(GYRO_SCALER);
+
+const ACCEL_SCALER = {
+    AFS_2G: (2.0 / 32768.0),
+    AFS_4G: (4.0 / 32768.0),
+    AFS_8G: (8.0 / 32768.0),
+    AFS_16G: (16.0 / 32768.0)
+};
+Object.freeze(ACCEL_SCALER);
+
+class SH200Q {
+    #io;
+    #view;
+    #gyroScale = GYRO_SCALER.GFS_2000DPS;
+    #accelScale = ACCEL_SCALER.AFS_8G;
+
+    constructor(options) {
+        const io = this.#io = new options.sensor.io({
+            hz: 400_000,
+            address: 0x6C,
+            ...options.sensor
+        });
+
+        this.#view = new DataView(new ArrayBuffer(6));
+
+        if (io.readUint8(REGISTERS.WHO_AM_I) !== EXPECTED_WHO_AM_I)
+            throw new Error("unrecognized");
+
+        this.enable();
+    }
+
+    close() {
+        this.#io?.close();
+        this.#io = undefined;
+    }
+
+    configure(dictionary) {
+        for (const property in dictionary) {
+            switch (property) {
+                case "GYRO_SCALER":
+                    this.#gyroScale = dictionary.GYRO_SCALER;
+                    break;
+                case "ACCEL_SCALER":
+                    this.#accelScale = dictionary.ACCEL_SCALER;
+                    break;
+            }
+        }
+    }
+
+    enable() {
+        const io = this.#io;
+
+        Timer.delay(1);
+
+        // set acc odr 256hz
+        // 0x81 1024hz   //0x89 512hz    //0x91 256hz
+        io.writeUint8(REGISTERS.ACC_CONFIG, 0x91);
+
+        // set gyro odr 500hz
+        // 0x11 1000hz    //0x13 500hz   //0x15 256hz
+        io.writeUint8(REGISTERS.GYRO_CONFIG, 0x13);
+
+        // set gyro dlpf 50hz
+        // 0x00 250hz   //0x01 200hz   //0x02 100hz  //0x03 50hz  //0x04 25hz
+        io.writeUint8(REGISTERS.GYRO_DLPF, 0x03);
+
+        // set no buffer mode
+        io.writeUint8(REGISTERS.FIFO_CONFIG, 0x00);
+
+        // set acc range +-8G
+        io.writeUint8(REGISTERS.ACC_RANGE, 0x01);
+
+        // set gyro range +-2000/s
+        io.writeUint8(REGISTERS.GYRO_RANGE, 0x00);
+
+        io.writeUint8(REGISTERS.REG_SET1, 0xC0);
+
+        // ADC Reset
+        const temp = io.readUint8(REGISTERS.REG_SET2);
+        io.writeUint8(REGISTERS.REG_SET2, temp | 0x10);
+
+        Timer.delay(1);
+
+        io.writeUint8(REGISTERS.REG_SET2, temp & 0xEF);
+
+        Timer.delay(10);
+    }
+
+    sample() {
+        const io = this.#io;
+        const view = this.#view;
+
+        io.readBuffer(REGISTERS.ACCEL_XOUT, view);
+        const accelerometer = {
+            x: view.getInt16(0) * this.#accelScale,
+            y: view.getInt16(2) * this.#accelScale,
+            z: view.getInt16(4) * this.#accelScale
+        };
+
+        io.readBuffer(REGISTERS.GYRO_XOUT, view);
+        const gyroscope = {
+            x: view.getInt16(0) * this.#gyroScale,
+            y: view.getInt16(2) * this.#gyroScale,
+            z: view.getInt16(4) * this.#gyroScale
+        };
+
+        io.readBuffer(REGISTERS.TEMP_OUT, 2, view.buffer);
+        const thermometer = {
+            temperature: view.getInt16(0, true) / 333.87 + 21.0
+        };
+
+        return {
+            accelerometer,
+            gyroscope,
+            thermometer
+        };
+    }
+}
+Object.freeze(SH200Q.prototype);
+
+export default SH200Q;
