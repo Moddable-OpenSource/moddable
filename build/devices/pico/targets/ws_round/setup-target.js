ce82747be5d2fef456885496168be1e079239081
STC <stc1988@users.noreply.github.com>
2026-05-12 23:57:58 +0900

    use host's BackLight

--- build/devices/pico/targets/ws_round/setup-target.js
+++ build/devices/pico/targets/ws_round/setup-target.js
@@ -3,20 +3,7 @@ import Timer from "timer";
 
 export default function (done) {
 	const Digital = device.io.Digital;
-	const backlightPin = new Digital({ pin: config.backlight, mode: Digital.Output });
-	if ((undefined === config.brightness) || ("none" === config.brightness)) {
-		backlightPin.write(0);
-		backlightPin.close();
-	}
-	else if ("off" === config.backlight) {
-		backlightPin.write(1);
-		backlightPin.close();
-	}
-	else {
-		backlightPin.close();
-		globalThis.backlight = new device.peripheral.Backlight;
-		backlight.brightness = parseInt(config.brigtness) / 100;
-	}
+
 	const displayResetPin = new Digital({ pin: config.lcd_rst_pin, mode: Digital.Output });
 	const displayCSPin = new Digital({ pin: config.lcd_cs_pin, mode: Digital.Output });
 
