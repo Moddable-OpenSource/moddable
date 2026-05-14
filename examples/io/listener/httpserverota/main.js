5f9a38632707ad0ff724e9e9ae7cf7e0ab809eae
Peter Hoddie <peter@moddable.com>
2026-05-13 19:19:13 -0700

    ECMA-419 HTTP OTA server example

---
+++ examples/io/listener/httpserverota/main.js
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2022-2026  Moddable Tech, Inc.
+ *
+ *   This file is part of the Moddable SDK.
+ * 
+ *   This work is licensed under the
+ *       Creative Commons Attribution 4.0 International License.
+ *   To view a copy of this license, visit
+ *       <http://creativecommons.org/licenses/by/4.0>.
+ *   or send a letter to Creative Commons, PO Box 1866,
+ *   Mountain View, CA 94042, USA.
+ *
+ */
+
+import HTTPServer from "embedded:network/http/server"
+import Listener from "embedded:io/socket/listener";
+import OTA from "embedded:update";
+import flash from "embedded:storage/flash";
+
+import WebPage from "embedded:network/http/server/options/webpage";
+
+const port = 80;
+const router = new Map;
+const notFound = {
+	...WebPage,		// STATIC ROUTE
+	data: ArrayBuffer.fromString("Not found\n"),
+};
+
+new HTTPServer({
+	io: Listener,
+	port,
+	onConnect(connection) {
+		connection.accept({
+			onRequest(request) {
+				this.route = router.get(request.path) ?? notFound;
+			},
+		})
+	}
+});
+
+router.set("/ota", {
+	onRequest(request) {
+		this.status = 200;
+		if ("PUT" !== request.method) {
+			this.status = 405;
+			return;
+		}
+		try {
+			this.updater = OTA.open({partition: flash.open({path: "nextota"})});
+		}
+		catch {
+			this.status = 500;
+		}
+	},
+	onReadable(count) {
+		try {
+			const bytes = this.read();
+			this.updater?.write(bytes);
+		}
+		catch {
+			this.status = 500;
+		}
+	},
+	onResponse(response) {
+		try {
+			this.updater?.complete();
+		}
+		catch {
+			this.status = 500;
+		}
+		finally {
+			this.updater?.close();
+		}
+		response.status = this.status;
+		response.headers.set("content-length", 0);
+		this.respond(response);
+	},
+	onError() {
+		this.updater?.close();
+	}
+});
